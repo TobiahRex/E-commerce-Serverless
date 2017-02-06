f747b4b11f6cb3cb0cd87c942b895bbf6a3b92da
TobiahRex <tobiah.rex@gmail.com>
2017-02-07 00:43:20 +0900

    created NavbarOptionsLangauge comp.

--- src/Pages/Navbar/navbarComponents/navbarOptions.js
+++ src/Pages/Navbar/navbarComponents/navbarOptions.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, PureComponent } from 'react';
-import FontAwesome from 'react-fontawesome';
 
-import navbarOptionsLangauge from './navbarOptions_language';
+import NavbarOptionsLangauge from './navbarOptions_language';
+import NavbarOptionsCurrency from './navbarOptions_currency';
 
 /* TODO:
 1. Pass down via props the respective class methods shown below.
@@ -19,24 +19,8 @@ class NavbarOptions extends PureComponent {
   render() {
     return (
       <div className="navbar acitonSection upper options">
-        <navbarOptionsLangauge />
-        <div className="navbar actionSection upper currency">
-          <div className="navbar actionSection upper currency dollar">
-            <FontAwesome
-              name="usd"
-              className="navbar actionSection upper currency dollar icon"
-            />
-          </div>
-          <div className="navbar actionSection upper currency title">
-            <span>CURRENCY</span>
-          </div>
-          <div className="navbar actionSection upper currency chevron">
-            <FontAwesome
-              name="chevron-down"
-              className="navbar actionSection upper currency chevron icon"
-            />
-          </div>
-        </div>
+        <NavbarOptionsLangauge />
+        <NavbarOptionsCurrency />
       </div>
     );
   }
---
+++ src/Pages/Navbar/navbarComponents/navbarOptions_currency.js
@@ -0,0 +1,17 @@
+<div className="navbar actionSection upper currency">
+  <div className="navbar actionSection upper currency dollar">
+    <FontAwesome
+      name="usd"
+      className="navbar actionSection upper currency dollar icon"
+    />
+  </div>
+  <div className="navbar actionSection upper currency title">
+    <span>CURRENCY</span>
+  </div>
+  <div className="navbar actionSection upper currency chevron">
+    <FontAwesome
+      name="chevron-down"
+      className="navbar actionSection upper currency chevron icon"
+    />
+  </div>
+</div>
