92e1acb581f381f3cac1e7e2b016bc78b0238972
TobiahRex <tobiah.rex@gmail.com>
2017-02-07 14:47:46 +0900

    modularizing folder strucutre for navbar

---
+++ src/Pages/Navbar/navbarComponents/navbarUpper/navbarOptions_currency.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import NavbarOptionsCurrencyButton from './navbarOptions_currency_mainButton';
+import NavbarOptionsCurrencyDropdnContent from './navbarOptions_currency_dropdnContent';
+
+export default () => (
+  <div className="navbar actionSection upper currency">
+    <NavbarOptionsCurrencyButton />
+    <NavbarOptionsCurrencyDropdnContent />
+  </div>
+);
