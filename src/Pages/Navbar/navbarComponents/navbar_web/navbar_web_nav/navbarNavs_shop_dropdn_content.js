f22767cbfbe2d44eb70b9aa10037c78d2c047d14
TobiahRex <tobiah.rex@gmail.com>
2017-02-08 22:38:46 +0900

    building out navbar shop ddn content mid third.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop_dropdn_content.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop_dropdn_content.js
@@ -17,7 +17,40 @@ class NavbarNavsShopDropdnContent extends PureComponent {
               <h1>Switch Juice</h1>
             </div>
             <div className="shop-dropdown-content-midThird-juices">
-              
+              <div className="shop-dropdown-content-midThird-juices-fbb">
+                <div className="shop-dropdown-content-midThird-juices-fbb-title">
+                  <span>Fruity Bamm Bamm</span>
+                </div>
+                <div
+                  className="shop-dropdown-content-midThird-juices-fbb-image" />
+              </div>
+              <div className="shop-dropdown-content-midThird-juices-klp">
+                <div className="shop-dropdown-content-midThird-juices-klp-title">
+                  <span>Keylime Pie</span>
+                </div>
+                <div className="shop-dropdown-content-midThird-juices-klp-image" />
+              </div>
+              <div className="shop-dropdown-content-midThird-juices-pc">
+                <div className="shop-dropdown-content-midThird-juices-pc-title">
+                  <span>Pina Colada</span>
+                </div>
+                <div className="shop-dropdown-content-midThird-juices-pc-image" />
+              </div>
+              <div className="shop-dropdown-content-midThird-juices-fvm">
+                <div className="shop-dropdown-content-midThird-juices-fvm-title">
+                </div>
+                <div className="shop-dropdown-content-midThird-juices-fvm-image"></div>
+              </div>
+              <div className="shop-dropdown-content-midThird-juices-snc">
+                <div className="shop-dropdown-content-midThird-juices-snc-title">
+                </div>
+                <div className="shop-dropdown-content-midThird-juices-snc-image"></div>
+              </div>
+              <div className="shop-dropdown-content-midThird-juices-pb">
+                <div className="shop-dropdown-content-midThird-juices-pb-title">
+                </div>
+                <div className="shop-dropdown-content-midThird-juices-pb-image"></div>
+              </div>
             </div>
           </div>
           <div className="shop-dropdown-content-bottomThird"></div>
