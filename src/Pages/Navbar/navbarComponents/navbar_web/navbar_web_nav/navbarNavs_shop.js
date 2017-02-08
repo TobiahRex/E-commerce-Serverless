ca2b2b9965694670a003995c19e094789d586584
TobiahRex <tobiah.rex@gmail.com>
2017-02-08 17:36:40 +0900

    building out SHOP hover component.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop.js
@@ -3,19 +3,17 @@ import React, { PureComponent } from 'react';
 class NavbarNavsShop extends PureComponent {
   render() {
     return (
-      <div className="navbar actionSection lower shop">
-        <div className="navbar-actionSection-lower-shop-main">
-          <span className="lower-shop-main-button">
-            <div className="title">
-              <span>SHOP</span>
-            </div>
-          </span>
-          {/* <span className="lower-shop-dropdown-content">
-            <div className="title">
+      <div className="navbar-actionSection-lower-shop">
+        <span className="shop-main-button">
+          <div className="shop-main-button-title">
             <span>SHOP</span>
-            </div>
-          </span> */}
-        </div>
+          </div>
+        </span>
+        <span className="shop-dropdown-content">
+          <div className="title">
+            <span>SHOP</span>
+          </div>
+        </span>
       </div>
     );
   }
