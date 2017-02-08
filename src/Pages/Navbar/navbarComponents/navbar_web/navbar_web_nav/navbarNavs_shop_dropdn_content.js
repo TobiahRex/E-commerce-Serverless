3ffab0812881faf44829c77e716838f3b002dee7
TobiahRex <tobiah.rex@gmail.com>
2017-02-08 21:33:49 +0900

    added NavbarNavsShopDropdnContent.

---
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop_dropdn_content.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class NavbarNavsShopDropdnContent extends PureComponent {
+  render() {
+    return (
+      <span className="shop-dropdown-content">
+        <div className="title">
+          <span>SHOP</span>
+        </div>
+      </span>
+    );
+  }
+}
+
+export default NavbarNavsShopDropdnContent;
