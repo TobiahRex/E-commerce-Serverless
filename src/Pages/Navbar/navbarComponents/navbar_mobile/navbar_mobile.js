7cf2fe18b99b1935c2d22d9d051bd066162408be
TobiahRex <tobiah.rex@gmail.com>
2017-02-10 02:48:10 +0900

    building out navbar_mobile_userActions component structure.

--- src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile.js
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { Link } from 'react-router';
 
 import NavbarMobileOptions from './navbar_mobile_options/navbarOptions_mobile';
+import NavbarMobileActions from './navbar_mobile_actions/navbar_mobile_userActions';
 
 class NavbarMobile extends PureComponent {
   render() {
@@ -12,19 +13,7 @@ class NavbarMobile extends PureComponent {
             <img className="navbar-mobile-logo-src" alt="nj2jp logo" />
           </div>
           <NavbarMobileOptions />
-          <div className="navbar-mobile-actions">
-            <div className="navbar-mobile-actions-signedIn">
-              <Link>My Account</Link>
-              <Link>Checkout</Link>
-              <button>
-                Sign Out
-              </button>
-            </div>
-            <div className="navbar-mobile-actions-notSignedIn">
-              <Link>Sign In</Link>
-              <Link>Register</Link>
-            </div>
-          </div>
+          <NavbarMobileActions />
           <div className="navbar-mobile-navbar"></div>
         </div>
       </nav>
