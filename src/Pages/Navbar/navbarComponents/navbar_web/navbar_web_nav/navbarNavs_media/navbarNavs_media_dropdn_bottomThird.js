784fa761d3d380dc555b42489942eaf824356123
TobiahRex <tobiah.rex@gmail.com>
2017-02-09 12:48:47 +0900

    finished preliminary navbar media dropdown styling. Adding effets and individual hover props.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_bottomThird.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_bottomThird.js
@@ -7,9 +7,43 @@ class NavbarNavsMediaDropdnBottomthird extends PureComponent {
       <div className="media-dropdown-content-bottomThird">
         <div className="media-dropdown-content-bottomThird-outterContainer">
           <div className="media-dropdown-content-bottomThird-innerContainer">
-            <div className="media-dropdown-content-bottomThird-innerContainer-smedia">
+            <div
+              id="media-dropdown-facebook-icon"
+              className="media-dropdown-content-bottomThird-innerContainer-smedia">
               <FontAwesome
                 name="facebook"
+                className="media-dropdown-content-bottomThird-innerContainer-smedia-icon"
+              />
+            </div>
+            <div
+              id="media-dropdown-twitter-icon"
+              className="media-dropdown-content-bottomThird-innerContainer-smedia">
+              <FontAwesome
+                name="twitter"
+                className="media-dropdown-content-bottomThird-innerContainer-smedia-icon"
+              />
+            </div>
+            <div
+              id="media-dropdown-youtube-icon"
+              className="media-dropdown-content-bottomThird-innerContainer-smedia">
+              <FontAwesome
+                name="youtube"
+                className="media-dropdown-content-bottomThird-innerContainer-smedia-icon"
+              />
+            </div>
+            <div
+              id="media-dropdown-linkedin-icon"
+              className="media-dropdown-content-bottomThird-innerContainer-smedia">
+              <FontAwesome
+                name="linkedin"
+                className="media-dropdown-content-bottomThird-innerContainer-smedia-icon"
+              />
+            </div>
+            <div
+              id="media-dropdown-instagram-icon"
+              className="media-dropdown-content-bottomThird-innerContainer-smedia">
+              <FontAwesome
+                name="instagram"
                 className="media-dropdown-content-bottomThird-innerContainer-smedia-icon"
               />
             </div>
