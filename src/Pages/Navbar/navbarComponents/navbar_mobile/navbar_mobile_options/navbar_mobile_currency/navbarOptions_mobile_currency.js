e1c0e0b46cfe24920c03997f0999ff5ca470613b
TobiahRex <tobiah.rex@gmail.com>
2017-02-10 00:31:08 +0900

    completed ipp navbar mobile options currency parent component.

---
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_options/navbar_mobile_currency/navbarOptions_mobile_currency.js
@@ -0,0 +1,28 @@
+import React, { PropTypes, PureComponent } from 'react';
+
+import NavbarMobileOptionsCurrencyButton from './navbarOptions_mobile_currency_mainButton';
+import NavbarMobileOptionsCurrencyDropdnContent from './navbarOptions_mobile_currency_dropdnContent';
+
+/* TODO:
+1. Pass down via props the respective class methods shown below.
+- Currency change: Will set a flag in state that all SMART components will be receiving to determine how to render the currency info.
+
+*/
+
+class NavbarMobileOptionsCurrency extends PureComponent {
+  static propTypes = {
+    onCurrencyChange: PropTypes.func,
+  }
+
+  onCurrencyChange = () => console.info('changed currency');
+
+  render() {
+    return (
+      <div className="navbar-mobile-options-currency">
+        <NavbarMobileOptionsCurrencyButton />
+        <NavbarMobileOptionsCurrencyDropdnContent />
+      </div>
+    );
+  }
+}
+export default NavbarMobileOptionsCurrency;
