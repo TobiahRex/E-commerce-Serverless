a9b45ec64eba094d3888daf238cdac8c09e36ae4
TobiahRex <tobiah.rex@gmail.com>
2017-02-09 12:55:57 +0900

    finished navbar media dropdn bottom third styling.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
@@ -1,21 +1,30 @@
 import React, { PropTypes, PureComponent } from 'react';
+import { Link } from 'react-router';
 
 class NavbarNavsMediaDropdnMidthird extends PureComponent {
   render() {
     return (
       <div className="media-dropdown-content-midThird">
-        <div className="media-dropdown-content-midThird-title">
-          <h4>Contact Us</h4>
-        </div>
-        <div className="media-dropdown-content-midThird-title">
-          <h4>Juice Reviews</h4>
-        </div>
-        <div className="media-dropdown-content-midThird-title">
-          <h4>Vape News</h4>
-        </div>
-        <div className="media-dropdown-content-midThird-title">
-          <h4>User Stories</h4>
-        </div>
+        <Link to={'/conatct_us'}>
+          <div className="media-dropdown-content-midThird-title">
+            <h4>Contact Us</h4>
+          </div>
+        </Link>
+        <Link to={'/juice_reviews'}>
+          <div className="media-dropdown-content-midThird-title">
+            <h4>Juice Reviews</h4>
+          </div>
+        </Link>
+        <Link to={'/vape_news'}>
+          <div className="media-dropdown-content-midThird-title">
+            <h4>Vape News</h4>
+          </div>
+        </Link>
+        <Link to={'/user_stories'}>
+          <div className="media-dropdown-content-midThird-title">
+            <h4>User Stories</h4>
+          </div>
+        </Link>
       </div>
     );
   }
