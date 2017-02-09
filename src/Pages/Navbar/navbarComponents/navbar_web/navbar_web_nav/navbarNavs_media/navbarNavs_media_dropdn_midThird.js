ac8f67f9d57865706507a159453151d04e8ea1bf
TobiahRex <tobiah.rex@gmail.com>
2017-02-09 13:00:03 +0900

    adding cursor styling to navbar media dropdn.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
@@ -5,22 +5,33 @@ class NavbarNavsMediaDropdnMidthird extends PureComponent {
   render() {
     return (
       <div className="media-dropdown-content-midThird">
-        <Link to={'/conatct_us'}>
+        <Link
+          to={'/contact_us'} className="media-dropdown-content-midThird-links"
+        >
           <div className="media-dropdown-content-midThird-title">
             <h4>Contact Us</h4>
           </div>
         </Link>
-        <Link to={'/juice_reviews'}>
+        <Link
+          to={'/juice_reviews'}
+          className="media-dropdown-content-midThird-links"
+        >
           <div className="media-dropdown-content-midThird-title">
             <h4>Juice Reviews</h4>
           </div>
         </Link>
-        <Link to={'/vape_news'}>
+        <Link
+          to={'/vape_news'}
+          className="media-dropdown-content-midThird-links"
+        >
           <div className="media-dropdown-content-midThird-title">
             <h4>Vape News</h4>
           </div>
         </Link>
-        <Link to={'/user_stories'}>
+        <Link
+          to={'/user_stories'}
+          className="media-dropdown-content-midThird-links"
+        >
           <div className="media-dropdown-content-midThird-title">
             <h4>User Stories</h4>
           </div>
