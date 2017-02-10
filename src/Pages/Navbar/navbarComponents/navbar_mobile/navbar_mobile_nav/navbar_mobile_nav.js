77afb675087254acc1546c0946950e05a060ed91
TobiahRex <tobiah.rex@gmail.com>
2017-02-10 16:23:05 +0900

    ran new build.

--- src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav.js
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav.js
@@ -28,73 +28,81 @@ class NavbarMobileNav extends Component {
     return (
       <div className="navbar-mobile-nav">
         <div className="navbar-mobile-nav-main">
-          <div className="navbar-mobile-nav-hamburger">
-            <div className="navbar-mobile-nav-hamburger-icon">
-              <a
-                onClick={e => e.preventDefault()}
-                href=""
-                className="navbar-mobile-nav-hamburger-icon-button"
-              >
-                <span />
-              </a>
+          <span className="navbar-mobile-nav-main-left">
+            <div className="navbar-mobile-nav-hamburger">
+              <div className="navbar-mobile-nav-hamburger-icon">
+                <a
+                  onClick={e => e.preventDefault()}
+                  href=""
+                  className="navbar-mobile-nav-hamburger-icon-button"
+                >
+                  <span />
+                </a>
+              </div>
             </div>
             <div className="navbar-mobile-nav-title">
-              <h5>Title Here</h5>
+              <h5>My Account</h5>
             </div>
-            <div className="navbar-mobile-cart">
+          </span>
+          <span className="navbar-mobile-nav-main-right">
+            <div className="navbar-mobile-nav-cart">
               <div className="navbar-mobile-cart-icon">
-                <FontAwesome name="shopping-cart" />
+                <FontAwesome
+                  className="navbar-mobile-cart-icon-fa"
+                  name="shopping-cart"
+                />
               </div>
+              <span className="navbar-mobile-cart-icon-divider"></span>
               <div className="navbar-mobile-cart-qty">
                 <span className="navbar-mobile-cart-qty-value">0</span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="navbar-mobile-nav-dropdown-content">
-          <div className="navbar-mobile-nav-dropdown-shop">
-            <h3 className="navbar-mobile-nav-dropdown-shop-title">Shop</h3>
-            <div className="navbar-mobile-nav-dropdown-shop-expanded">
-              <ul>
-                <li>Fruity Bamm Bamm</li>
-                <li>Keylime Pie</li>
-                <li>Pina Colada</li>
-                <li>French Vanilla Mocha</li>
-                <li>Strawberries {('N\'')} Cream</li>
-                <li>Papple Berry</li>
-              </ul>
+            <div className="navbar-mobile-nav-dropdown-content hidden">
+              <div className="navbar-mobile-nav-dropdown-shop">
+                <h3 className="navbar-mobile-nav-dropdown-shop-title">Shop</h3>
+                <div className="navbar-mobile-nav-dropdown-shop-expanded">
+                  <ul>
+                    <li>Fruity Bamm Bamm</li>
+                    <li>Keylime Pie</li>
+                    <li>Pina Colada</li>
+                    <li>French Vanilla Mocha</li>
+                    <li>Strawberries {('N\'')} Cream</li>
+                    <li>Papple Berry</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="navbar-mobile-nav-dropdown-media">
+                <h3 className="navbar-mobile-nav-dropdown-media-title">Media</h3>
+                <div className="navbar-mobile-nav-dropdown-media-expanded">
+                  <ul>
+                    <li>Contact Us</li>
+                    <li>Juice Reviews</li>
+                    <li>Vape News</li>
+                    <li>User Stories</li>
+                    <li>(Social Media Icons)</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="navbar-mobile-nav-dropdown-info">
+                <h3 className="navbar-mobile-nav-dropdown-info-title">Info</h3>
+                <div className="navbar-mobile-nav-dropdown-info-expanded">
+                  <ul>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>{('FAQ\'s')}</li>
+                    <li>Wholesale</li>
+                    <li>Mission Statement</li>
+                    <li>Shipping Policy</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>Nicotine Disclaimer</li>
+                    <li>Return Policy</li>
+                    <li>Affiliate Program</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="navbar-mobile-nav-dropdown-media">
-            <h3 className="navbar-mobile-nav-dropdown-media-title">Media</h3>
-            <div className="navbar-mobile-nav-dropdown-media-expanded">
-              <ul>
-                <li>Contact Us</li>
-                <li>Juice Reviews</li>
-                <li>Vape News</li>
-                <li>User Stories</li>
-                <li>(Social Media Icons)</li>
-              </ul>
-            </div>
-          </div>
-          <div className="navbar-mobile-nav-dropdown-info">
-            <h3 className="navbar-mobile-nav-dropdown-info-title">Info</h3>
-            <div className="navbar-mobile-nav-dropdown-info-expanded">
-              <ul>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>{('FAQ\'s')}</li>
-                <li>Wholesale</li>
-                <li>Mission Statement</li>
-                <li>Shipping Policy</li>
-                <li>Privacy Policy</li>
-                <li>Terms & Conditions</li>
-                <li>Nicotine Disclaimer</li>
-                <li>Return Policy</li>
-                <li>Affiliate Program</li>
-              </ul>
-            </div>
-          </div>
+          </span>
         </div>
       </div>
     );
