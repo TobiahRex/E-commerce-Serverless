0ef1cb379aa1f913caadb0dddbd0b79d2afa8d58
TobiahRex <tobiah.rex@gmail.com>
2017-02-12 16:41:03 +0900

    finished creating children Components for NavbarMobileNavMainBar.

--- src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_mainBar/navbar_mobile_nav_mainBar_hamburger.js
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_mainBar/navbar_mobile_nav_mainBar_hamburger.js
@@ -1,11 +1,27 @@
-<div className="navbar-mobile-nav-hamburger">
-  <div className="navbar-mobile-nav-hamburger-icon">
-    <a
-      onClick={e => e.preventDefault()}
-      href=""
-      className="navbar-mobile-nav-hamburger-icon-button"
-    >
-      <span />
-    </a>
-  </div>
-</div>
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  mobileNavbarExpanded: PropTypes.bool,
+};
+NavbarMobileNavTitle.propTypes = propTypes;
+
+export default function NavbarMobileNavTitle({ mobileNavbarExpanded }) {
+  return (
+    <div className="navbar-mobile-nav-hamburger">
+      <div className="navbar-mobile-nav-hamburger-icon">
+        <a
+          onClick={event.preventDefault}
+          href=""
+          className="navbar-mobile-nav-hamburger-icon-button"
+        >
+          <span />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+/* NOTE
+- I'm not sure this prop is necessary.  First see if you can build the hamburger transition only with SASS.
+
+*/
--- src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_mainBar/navbar_mobile_nav_mainBar_title.js
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_mainBar/navbar_mobile_nav_mainBar_title.js
@@ -1,3 +1,14 @@
-<div className="navbar-mobile-nav-title">
-  <h5>My Account</h5>
-</div>
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  activePage: PropTypes.string,
+};
+NavbarMobileNavTitle.propTypes = propTypes;
+
+export default function NavbarMobileNavTitle({ activePage }) {
+  return (
+    <div className="navbar-mobile-nav-title">
+      <h5>{activePage}</h5>
+    </div>
+  );
+}
