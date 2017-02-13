11222a96cb701e8a019ff90ca7fae653e8048e7e
TobiahRex <tobiah.rex@gmail.com>
2017-02-14 00:50:32 +0900

    building out homepage-header.

--- src/Pages/Home/homePage.js
+++ src/Pages/Home/homePage.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function HomePage() {
   return (
     <div className="homepage">
-      <h1>Home Page</h1>
+      <header className="homepage-header-img-src">
+        <div className="homepage-header-innner-container">
+          <img
+            alt="NicJuice2Japan" className="homepage-header-inner-container-img-src"
+          />
+          <h1>Fastest Nicotine e-Juice Delivery In Japan</h1>
+          <h1>Guaranteed</h1>
+        </div>
+      </header>
     </div>
   );
 }
