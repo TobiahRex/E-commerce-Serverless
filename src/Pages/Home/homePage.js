ce29f9665ad223265d3bb449ce5d0f69e7f00710
TobiahRex <tobiah.rex@gmail.com>
2017-02-14 00:56:29 +0900

    finished heading component structure.

--- src/Pages/Home/homePage.js
+++ src/Pages/Home/homePage.js
@@ -8,8 +8,8 @@ export default function HomePage() {
           <img
             alt="NicJuice2Japan" className="homepage-header-inner-container-img-src"
           />
-          <h1>Fastest Nicotine e-Juice Delivery In Japan</h1>
-          <h1>Guaranteed</h1>
+          <h1 className="hompage-header-title-1">Fastest Nicotine e-Juice Delivery In Japan</h1>
+          <h1 className="hompage-header-title-2">Guaranteed!</h1>
         </div>
       </header>
     </div>
