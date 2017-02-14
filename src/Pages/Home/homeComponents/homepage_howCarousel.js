c338b877ccd4297c33885ed117bc3142caa856b7
TobiahRex <tobiah.rex@gmail.com>
2017-02-14 23:33:16 +0900

    still building out carousel for homepage.

--- src/Pages/Home/homeComponents/homepage_howCarousel.js
+++ src/Pages/Home/homeComponents/homepage_howCarousel.js
@@ -25,9 +25,9 @@ export default class HomepageHowCarousel extends Component {
 
       distro: 'Moments later, we receive your order at our Distribution Center in California.',
 
-      warehouse: 'Our distribution warehouse then quickly prepares your Nictoine Juice parcel per your order invoice.',
+      warehouse: 'Our distribution warehouse then quickly prepares your Nicotine Juice parcel per your order invoice.',
 
-      flights: 'Shortly thereafter, your parcel is put on the soonest direct flight from California to Japan.',
+      flight: 'Shortly thereafter, your parcel is put on the soonest direct flight from California to Japan.',
 
       truck: 'Soon after landing, your parcel is placed on a delivery truck and on its way to your address in Japan.',
 
@@ -38,48 +38,49 @@ export default class HomepageHowCarousel extends Component {
       <div className="homepage-how">
         <h1 className="homepage-how-title">How?</h1>
         <div className="homepage-how-carousel-container">
+          <<div className="homepage-how-carousel-container-slides">
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="couple"
+              description={slideDescs.couple}
+              alt="Buy NJ2JP Juices"
+            />
 
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="couple"
-            description={slideDescs.couple}
-            alt="Buy NJ2JP Juices"
-          />
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="distro"
+              description={slideDescs.distro}
+              alt="Distribution Center"
+            />
 
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="distro"
-            description={slideDescs.distro}
-            alt="Distribution Center"
-          />
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="warehouse"
+              description={slideDescs.warehouse}
+              alt="Warehouse"
+            />
 
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="warehouse"
-            description={slideDescs.warehouse}
-            alt="Warehouse"
-          />
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="flight"
+              description={slideDescs.flight}
+              alt="Shipping Flight"
+            />
 
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="flight"
-            description={slideDescs.flight}
-            alt="Shipping Flight"
-          />
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="truck"
+              description={slideDescs.truck}
+              alt="Delivery Truck"
+            />
 
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="truck"
-            description={slideDescs.truck}
-            alt="Delivery Truck"
-          />
-
-          <CarouselSlide
-            className="homepage-how-carousel"
-            name="delivery"
-            description={slideDescs.delivery}
-            alt="Delivery Man"
-          />
+            <CarouselSlide
+              className="homepage-how-carousel"
+              name="delivery"
+              description={slideDescs.delivery}
+              alt="Delivery Man"
+            />
+          </div>
           {/* NAVS */}
           <CarouselNav
             className="homepage-how-carousel"
