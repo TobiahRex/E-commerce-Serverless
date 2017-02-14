0d2cd20a9c318b7c46b40295f5f3c596c3a3a96a
TobiahRex <tobiah.rex@gmail.com>
2017-02-14 20:47:23 +0900

    building out pre-lim styles for homepage_how.js

--- src/Pages/Home/homePage.js
+++ src/Pages/Home/homePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import HomepageHeader from './homeComponents/homepage_header';
 import HomepageFastestDelivery from './homeComponents/homepage_fastestDelivery';
@@ -9,51 +10,62 @@ export default function HomePage() {
       <HomepageHeader />
       <HomepageFastestDelivery />
       <div className="homepage-how">
-        <h1>How?</h1>
-      </div>
-      <div className="homepage-how-carousel-container">
-        <div className="homepage-how-carousel-couple">
-          <img alt="Buy NJ2JP Juices" className="homepage-how-carousel-couple-image" />
-          <div className="homepage-how-carousel-couple-description-container">
-            <p>
-              Start by choosing from our delicious Juice Flavors
-              with 4 different Nicotine Strengths and place your order.
-            </p>
+        <h1 className="homepage-how-title">How?</h1>
+        <div className="homepage-how-carousel-container">
+          <div className="homepage-how-carousel-couple">
+            <img alt="Buy NJ2JP Juices" className="homepage-how-carousel-couple-image" />
+            <div className="homepage-how-carousel-couple-description-container">
+              <p>
+                Start by choosing from our delicious Juice Flavors
+                with 4 different Nicotine Strengths and place your order.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="homepage-how-carousel-distro">
-          <img alt="Distribution Center" className="homepage-how-carousel-distro-image" />
-          <div className="homepage-how-carousel-distro-description-container">
-            <p>
-              Moments later, we receive your order at our
-              Distribution Center in California.
-            </p>
+          <div className="homepage-how-carousel-distro">
+            <img alt="Distribution Center" className="homepage-how-carousel-distro-image" />
+            <div className="homepage-how-carousel-distro-description-container">
+              <p>
+                Moments later, we receive your order at our
+                Distribution Center in California.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="homepage-how-carousel-warehouse">
-          <img alt="Warehouse" className="homepage-how-carousel-warehouse-image" />
-          <div className="homepage-how-carousel-warehouse-description-container">
-            <p>
-              Our distribution warehouse then quickly prepares your
-              Nictoine Juice parcel per your order invoice.
-            </p>
+          <div className="homepage-how-carousel-warehouse">
+            <img alt="Warehouse" className="homepage-how-carousel-warehouse-image" />
+            <div className="homepage-how-carousel-warehouse-description-container">
+              <p>
+                Our distribution warehouse then quickly prepares your
+                Nictoine Juice parcel per your order invoice.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="homepage-how-carousel-flight">
-          <img alt="Shipping Plane" className="homepage-how-carousel-flight-image" />
-          <div className="homepage-how-carousel-flight-description-container">
-            <p>
-              Shortly thereafter, your parcel is put on the soonest
-              direct flight from California to Japan.
-            </p>
+          <div className="homepage-how-carousel-flight">
+            <img alt="Shipping Plane" className="homepage-how-carousel-flight-image" />
+            <div className="homepage-how-carousel-flight-description-container">
+              <p>
+                Shortly thereafter, your parcel is put on the soonest
+                direct flight from California to Japan.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="homepage-how-carousel-truck">
-          <img alt="Shipping Plane" className="homepage-how-carousel-truck-image" />
-          <div className="homepage-how-carousel-truck-description-container">
-            <p>
-              Soon after landing, your parcel is placed on a delivery truck and on its way to your address in Japan.
-            </p>
+          <div className="homepage-how-carousel-truck">
+            <img alt="Shipping Plane" className="homepage-how-carousel-truck-image" />
+            <div className="homepage-how-carousel-truck-description-container">
+              <p>
+                Soon after landing, your parcel is placed on a delivery truck and on its way to your address in Japan.
+              </p>
+            </div>
+          </div>
+          <div className="homepage-how-carousel-delivery">
+            <img alt="Shipping Plane" className="homepage-how-carousel-delivery-image" />
+            <div className="homepage-how-carousel-delivery-description-container">
+              <p>
+                Finally, you receive your Nicotine Juice from NicJuice2Japan faster than ever before.  Don’t believe us?
+              </p>
+              <Link className="homepage-how-carousel-delivery-buy-btn" to="/products">
+                Buy Now
+              </Link>
+            </div>
           </div>
         </div>
       </div>
