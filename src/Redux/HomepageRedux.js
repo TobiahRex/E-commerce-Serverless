6098d527e918d4b40be9277d848eb75126f70450
TobiahRex <tobiah.rex@gmail.com>
2017-02-16 00:45:32 +0900

    refactoring component.

--- src/Redux/HomepageRedux.js
+++ src/Redux/HomepageRedux.js
@@ -1,8 +1,9 @@
 import { createReducer, createActions } from 'reduxsauce';
 
 const { Types, Creators } = createActions({
-  startTimer: ['index'],
+  startTimer: null,
   stopTimer: null,
+  restartTimer: ['index'],
 });
 
 export const HomepageTypes = Types;
@@ -10,36 +11,95 @@ export default Creators;
 
 export const INITIAL_STATE = {
   reviews: {
-    slideIndex: null,
-    slideAdjust: null,
+    slideIndex: 0,
+    slideAdjust: '0em',
   },
 };
 
+let globalTimer;
+const startTimer = (index) => {
+  // function startTimer(index) {
+    if (index === 4) {
+      return startTimer(0);
+    }
+    console.log(index);
+    return globalTimer = setTimeout(() => {
+      startTimer(++index);
+    }, 2000);
+  }
 
-let reduxTimer;
-function asyncTimer() {
-  let index = -1;
-  reduxTimer = setInterval(() => {
-    index += 1;
+  startTimer(0);
+
+  setTimeout(() => {
+    clearTimeout(globalTimer);
+    startTimer(1);
+  }, 5000);
+
+  if (index === 3) {
+    index = 0;
+    reduxTimer = setInterval(() => {
+      index += 1;
+      return ({
+        reviews: {
+          slideIndex: index,
+          slideAdjust: `${(-1000 * index) / 10}em`,
+        },
+      });
+    }, 3000);
+
     return ({
       reviews: {
         slideIndex: index,
         slideAdjust: `${(-1000 * index) / 10}em`,
       },
     });
-  }, 3000);
-}
+  } else {
+    reduxTimer = setInterval(() => {
+      index += 1;
+      return ({
+        reviews: {
+          slideIndex: index,
+          slideAdjust: `${(-1000 * index) / 10}em`,
+        },
+      });
+    }, 3000);
 
-const startTimer = ({ reviews }, { index }) =>
-asyncTimer(index, reviews.slideIndex);
+    return ({
+      reviews: {
+        slideIndex: 0,
+        slideAdjust: `0em`,
+      },
+    });
+  }
+};
 
-const stopTimer = () => {
+
+const stopTimer = (index) => {
   reduxTimer.clearInterval();
   return ({
     reviews: {
-      timer: null,
-      slideIndex: null,
-      slideAdjust: null,
+      slideIndex: 1,
+      slideAdjust: `${(-1000 * index) / 10}em`,
+    },
+  });
+};
+
+const restartTimer = (state, { index }) => {
+  let customIndex = index;
+  reduxTimer = setInterval(() => {
+    customIndex += 1;
+    return ({
+      reviews: {
+        slideIndex: customIndex,
+        slideAdjust: `${(-1000 * customIndex) / 10}em`,
+      },
+    });
+  }, 3000);
+
+  return ({
+    reviews: {
+      slideIndex: customIndex,
+      slideAdjust: `${(-1000 * customIndex) / 10}em`,
     },
   });
 };
@@ -47,4 +107,5 @@ const stopTimer = () => {
 export const homepageReducer = createReducer(INITIAL_STATE, {
   [Types.START_TIMER]: startTimer,
   [Types.STOP_TIMER]: stopTimer,
+  [Types.RESTART_TIMER]: restartTimer,
 });
