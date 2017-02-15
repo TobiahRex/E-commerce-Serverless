c84ab387ae58831461c18044c54f894547676e86
TobiahRex <tobiah.rex@gmail.com>
2017-02-15 19:25:31 +0900

    building out functionality for homepageReviewSlides.

--- src/Pages/Home/homeComponents/homepage_reviewsCarousel.js
+++ src/Pages/Home/homeComponents/homepage_reviewsCarousel.js
@@ -9,36 +9,37 @@ class HomepageReviewsCarousel extends Component {
 
     this.state = {
       numSlides: 4,
-      showIndex: 0,
+      showDot: 0,
     };
   }
 
   render() {
+    const showIndex = this.state.showDot;
     return (
       <div className="homepage-reviews">
         <h1 className="homepage-reviews-title">Reviews</h1>
         <div className="homepage-reviews-carousel-parent">
           <div className="homepage-reviews-carousel-container">
             <div className="homepage-reviews-carousel-slides">
-              <HomepageReviewSlide
+              <HomepageReviewsSlide
                 className={'homepage-reviews-carousel'}
                 name={'alpha'}
                 review={'Well, NJ2JP wasn’t lying. 5 days to Fukuoka. Way faster than all of my previous online choices. I’m sold.'}
                 author={'Matt Shipmen'}
               />
-              <HomepageReviewSlide
+              <HomepageReviewsSlide
                 className={'homepage-reviews-carousel'}
                 name={'beta'}
                 review={'Wow! Fruity Bamm-Bamm = Delicious.  4 Day Delivery = Fast. My New Juice Source = NJ2JP.'}
                 author={'Gene Smith, Okinawa'}
               />
-              <HomepageReviewSlide
+              <HomepageReviewsSlide
                 className={'homepage-reviews-carousel'}
                 name={'gamma'}
                 review={'“NicJuice2Japan (NJ2JP) are killing it with these delivery speeds. Not to mention the juice line is delicious.  Looking forward to them carrying more juices flavors.”'}
                 author={'Robert McNair, Sasebo'}
               />
-              <HomepageReviewSlide
+              <HomepageReviewsSlide
                 className={'homepage-reviews-carousel'}
                 name={'delta'}
                 review={'I placed my order on Monday, by Thursday morning, I was vaping Nicotine e-juice. Nj2jp is blazing fast!'}
@@ -47,7 +48,10 @@ class HomepageReviewsCarousel extends Component {
             </div>
           </div>
         </div>
-        <HomepageReviewsCarourselDots />
+        <HomepageReviewsCarourselDots
+          show={activeDot}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
