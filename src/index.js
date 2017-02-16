eaecca5c94f79fababe74ec440a2fc0bb2def5be
TobiahRex <tobiah.rex@gmail.com>
2017-02-17 01:54:44 +0900

    testing new build.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MobileDetect from 'mobile-detect';
 
 import './Services/socket-init';
 import createStore from './Redux/index';
@@ -13,7 +14,10 @@ import asynchServices from './Services/asynchDispatchServices';
 injectTapEventPlugin();
 const store = createStore();
 
-alert(/Android 6.0| Android Android 5.1|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent));
+// alert(/Android 6.0|Android|Android 5.1|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent));
+
+const md = new MobileDetect(window.navigator.userAgent);
+alert(JSON.stringify(md, null, 2));
 
 render(
   <Provider store={store} >
