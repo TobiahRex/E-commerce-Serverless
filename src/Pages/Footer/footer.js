34ee01473ddcfdaa0738f30137e06adc62dda068
TobiahRex <tobiah.rex@gmail.com>
2017-02-17 20:53:48 +0900

    creating footer.

--- src/Pages/Footer/footer.js
+++ src/Pages/Footer/footer.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-main">
+      <div className="footer-main-sections-container">
+        <ul className="footer-main-sections-list">
+          <li className="footer-main-sections-list-contactus"></li>
+          <li className="footer-main-sections-list-myaccount"></li>
+          <li className="footer-main-sections-list-customercare"></li>
+        </ul>
+      </div>
     </div>
   );
 }
