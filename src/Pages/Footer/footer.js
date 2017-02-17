0c7b7f6c018d15e173684ecd7d14f964d86d24b2
TobiahRex <tobiah.rex@gmail.com>
2017-02-17 20:56:15 +0900

    building out scaffolding for footer.

--- src/Pages/Footer/footer.js
+++ src/Pages/Footer/footer.js
@@ -5,9 +5,25 @@ export default function Footer() {
     <div className="footer-main">
       <div className="footer-main-sections-container">
         <ul className="footer-main-sections-list">
-          <li className="footer-main-sections-list-contactus"></li>
-          <li className="footer-main-sections-list-myaccount"></li>
-          <li className="footer-main-sections-list-customercare"></li>
+          <li className="footer-main-sections-list-contactus">
+            <div className="footer-main-sections-contactus-container">
+              <h1 className="footer-main-sections-contactus-title">
+                Contact Us
+              </h1>
+              <ul className="footer-main-sections-contactus-list">
+                <li className="footer-main-sections-contactus-email">Email</li>
+                <li className="footer-main-sections-contactus-phone">Phone</li>
+              </ul>
+            </div>
+          </li>
+          <li className="footer-main-sections-list-myaccount">
+            <div className="footer-main-sections-myaccount-container">
+              <h1 className="footer-main-sections-myaccount-title">My Account</h1>
+            </div>
+          </li>
+          <li className="footer-main-sections-list-customercare">
+
+          </li>
         </ul>
       </div>
     </div>
