3b250fd7db5c39d1281d3b4f4d62a0d54a2bd43c
TobiahRex <tobiah.rex@gmail.com>
2017-02-21 23:09:34 +0900

    building out scaffolding for empty cart page.;

--- src/Pages/Cart/EmptyCart/emptyCart.js
+++ src/Pages/Cart/EmptyCart/emptyCart.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 export default function EmptyCart() {
   return (
-    <div>
-      <h1>Empty Cart</h1>
+    <div className="empty-cart-main">
+      <div className="empty-cart-main-title">
+        <h1>Your Shopping Cart Is Empty</h1>
+      </div>
+      <div className="empty-cart-main-icon">
+        <FontAwesome name="shopping-basket" size="5x" />
+      </div>
+      <div className="empty-cart-oops-msg">
+        <h1>Oops :(</h1>
+      </div>
+      <div className="empty-cart-sub-msg">
+        <p>You have no items in your shopping cart.</p>
+      </div>
+      <div className="empty-cart-shopping-btn">
+        <button>Start Shopping</button>
+      </div>
     </div>
   );
 }
