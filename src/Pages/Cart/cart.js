0c83aeb8cebdbf3b88ea12c60b986f5487aacf8b
TobiahRex <tobiah.rex@gmail.com>
2017-02-22 18:21:46 +0900

    creating dynamic render function for juices in cart.

--- src/Pages/Cart/cart.js
+++ src/Pages/Cart/cart.js
@@ -3,8 +3,26 @@ import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router';
 
 export default function CartTable() {
-  const juiceName = 'Fruity Bamm-Bamm';
-  const
+  const juice1 = {
+    imgSrc: '',
+    name: 'Fruity Bamm-Bamm',
+    sku: '123123123',
+    nicotine: '6mg',
+  }
+  const juice2 = {
+    imgSrc: '',
+    name: 'Fruity Bamm-Bamm',
+    sku: '123123123',
+    nicotine: '6mg',
+  }
+  const juices = [juiceObj1, juiceObj2];
+
+  const renderJuices = () => {
+    jucies.map((juiceObj) => {
+
+    })
+  }
+
   return (
     <div className="shopping-cart-main">
       <div className="shopping-cart-breadcrumb-container">
@@ -32,25 +50,43 @@ export default function CartTable() {
           <tr className="shopping-cart-table-body-container">
             <td className="shopping-cart-table-body-infobox">
               <div className="shopping-cart-table-body-infobox-img">
-                <img className="shopping-cart-table-body-infobox-img-src" alt={juiceName} />
+                <img className="shopping-cart-table-body-infobox-img-src" alt={juiceObj1.imgSrc} />
               </div>
               <ul className="shopping-cart-table-body-infobox-list">
                 <li className="shopping-cart-table-body-infobox-title">
-                  {juiceTitle}
+                  {juiceObj1.name}
                 </li>
                 <li className="shopping-cart-table-body-infobox-nicotine">
-                  {juiceNicotine}
+                  {juiceObj1.nicotine}
                 </li>
                 <li className="shopping-cart-table-body-infobox-sku">
-                  {juiceSku}
+                  {juiceObj1.sku}
                 </li>
                 <li className="shopping-cart-table-body-infobox-trash">
                   <FontAwesome name="trash-o" />
                 </li>
               </ul>
             </td>
-            <td className="shopping-cart-table-body-product1">
-
+          </tr>
+          <tr className="shopping-cart-table-body-container">
+            <td className="shopping-cart-table-body-infobox">
+              <div className="shopping-cart-table-body-infobox-img">
+                <img className="shopping-cart-table-body-infobox-img-src" alt={juiceObj2.imgSrc} />
+              </div>
+              <ul className="shopping-cart-table-body-infobox-list">
+                <li className="shopping-cart-table-body-infobox-title">
+                  {juiceObj2.name}
+                </li>
+                <li className="shopping-cart-table-body-infobox-nicotine">
+                  {juiceObj2.nicotine}
+                </li>
+                <li className="shopping-cart-table-body-infobox-sku">
+                  {juiceObj2.sku}
+                </li>
+                <li className="shopping-cart-table-body-infobox-trash">
+                  <FontAwesome name="trash-o" />
+                </li>
+              </ul>
             </td>
           </tr>
         </table>
