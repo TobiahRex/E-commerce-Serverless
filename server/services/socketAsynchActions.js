f3ec21c3a3dcf08513ca700b6bf1ae165aa75053
TobiahRex <tobiah.rex@gmail.com>
2017-02-24 15:38:19 +0900

    successfully rendering dynamic tax rates from Avalara API inside cart.

--- server/services/socketAsynchActions.js
+++ server/services/socketAsynchActions.js
@@ -5,9 +5,9 @@ export default function socketActions(socket) {
   request('https://taxrates.api.avalara.com/postal?country=usa&postal=98101&apikey=fK%2BVk61LSB4Lg7piAan%2FgQudgwiqt4I2UbaFcrCgyqpnntXVWP%2FS1hZBQ6TLwq7Vr9lAeCW54bB0x%2B7J%2FxGbLQ%3D%3D&referrer=&lastReferrer=taxratesapi.avalara.com', (err, res, body) => {
     const response = JSON.parse(body);
     const taxRate = {
-      stateRate: response.rates[0].rate,
-      cityRate: response.rates[1].rate,
-      totalRate: response.totalRate,
+      stateRate: response.rates[0].rate / 100,
+      cityRate: response.rates[1].rate / 100,
+      totalRate: response.totalRate / 100,
     };
     socket.emit('tax_rate', taxRate);
   });
