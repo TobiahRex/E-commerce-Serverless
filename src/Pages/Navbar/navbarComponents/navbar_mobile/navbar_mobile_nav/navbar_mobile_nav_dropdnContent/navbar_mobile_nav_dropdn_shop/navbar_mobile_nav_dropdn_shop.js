e32377de451b8a7cd780d61f29e5443159b18086
TobiahRex <tobiah.rex@gmail.com>
2017-02-24 19:46:12 +0900

    setting alert for onClick @ navbar-nav-dropdown-shop-dropdown

--- src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_dropdnContent/navbar_mobile_nav_dropdn_shop/navbar_mobile_nav_dropdn_shop.js
+++ src/Pages/Navbar/navbarComponents/navbar_mobile/navbar_mobile_nav/navbar_mobile_nav_dropdnContent/navbar_mobile_nav_dropdn_shop/navbar_mobile_nav_dropdn_shop.js
@@ -10,7 +10,7 @@ const propTypes = {
 function NavbarMobileNavDropdnShop({ toggleDropdown }) {
   return (
     <li className="navbar-mobile-nav-dropdown-shop">
-      <div className="navbar-mobile-nav-dropdown-shop-dropdown">
+      <div className="navbar-mobile-nav-dropdown-shop-dropdown" onClick={(e) => alert(e.target)}>
         <NavbarMobileNavDropdnShopTitle />
         <NavbarMobileNavDropdnShopDropdnContent toggleDropdown={toggleDropdown} />
       </div>
