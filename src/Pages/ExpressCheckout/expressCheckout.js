409770fe17a7f609ba840b8477e41f6d854c17e3
TobiahRex <tobiah.rex@gmail.com>
2017-02-28 12:45:12 +0900

    building style sheets for checkout breadcrumb & titles.

--- src/Pages/ExpressCheckout/expressCheckout.js
+++ src/Pages/ExpressCheckout/expressCheckout.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router';
+import FontAwesome from 'react-fontawesome';
+
 
 export default function ExpressCheckout() {
   return (
-    <div>
-      <h1>Express Checkout</h1>
+    <div className="checkout__main">
+      <div className="checkout__breadcrumb--container">
+        <ul className="list">
+          <li className="path">
+            <Link className="path__link" to="/">Home</Link>
+            <FontAwesome
+              className="path__link--rightChevron"
+              name="angle-right"
+            />
+          </li>
+          <li className="path">
+            Shopping Cart
+          </li>
+        </ul>
+      </div>
+      <div className="checkout__title">
+        <h1>Shopping Cart</h1>
+      </div>
     </div>
   );
 }
