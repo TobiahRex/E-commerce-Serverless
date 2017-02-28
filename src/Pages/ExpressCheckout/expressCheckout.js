47a59eaf5d910ca02574108156877a23feb8f2b2
TobiahRex <tobiah.rex@gmail.com>
2017-02-28 15:03:37 +0900

    new build with media query changes & beginning of express checkout styling.

--- src/Pages/ExpressCheckout/expressCheckout.js
+++ src/Pages/ExpressCheckout/expressCheckout.js
@@ -45,6 +45,7 @@ export default function ExpressCheckout() {
                 />
               </div>
             </div>
+
             <div className="input__row">
               <div className="input__row--email">
                 <p>Email <span className="required">*</span></p>
@@ -54,6 +55,87 @@ export default function ExpressCheckout() {
                 />
               </div>
             </div>
+
+            <div className="input__row">
+              <div className="input__row--address-line-1">
+                <p>Address Line 1 <span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--address-line-2">
+                <p>Address Line 2 <span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--country">
+                <p>Country<span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--prefecture">
+                <p>Prefecture<span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--postal-code">
+                <p>Postal Code<span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--city">
+                <p>City<span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--phone">
+                <p>Phone / Cell<span className="required">*</span></p>
+                <input
+                  type="text"
+                  onChange={e => console.log(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="input__row">
+              <div className="input__row--same-as-billing">
+                <input
+                  type="checkbox"
+                  onChange={e => console.log(e.target.value)}
+                />
+                <p>Use same address for shipping.<span className="required">*</span></p>
+              </div>
+            </div>
+            
           </div>
           <div className="checkout__shipping">
             <h3>Shipping Address</h3>
