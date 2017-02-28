331fbbbe8a373f4055e284ad1df177527a36a43f
TobiahRex <tobiah.rex@gmail.com>
2017-02-28 18:04:50 +0900

    finished scaffolding for Order Review table @ Checkout.

--- src/Pages/ExpressCheckout/expressCheckout.js
+++ src/Pages/ExpressCheckout/expressCheckout.js
@@ -409,8 +409,52 @@ export default function ExpressCheckout() {
                   <th className="header__row--subtotal">Subtotal</th>
                 </tr>
               </thead>
+              <tbody className="table__body">
+                <tr className="body__row">
+                  <td className="body__row--product-name">
+                    <div className="image__container">
+                      <img alt="Juice" className="image--source" />
+                    </div>
+                    <div className="description__container">
+                      <p>Fruity Bamm-Bamm</p>
+                      <p>Nicotine Strength: <i>6mg</i></p>
+                    </div>
+                  </td>
+                  <td className="body__row--product-qty">
+                    <div className="qty--container">
+                      <p>3</p>
+                    </div>
+                  </td>
+                  <td className="body__row--product-subtotal">
+                    <div className="product-subtotal-container">
+                      <p><FontAwesome name="usd" />{'\u00A0'}90.00</p>
+                    </div>
+                  </td>
+                </tr>
+                <tr className="body__row">
+                  <td className="body__row--product-name">
+                    <div className="image__container">
+                      <img alt="Juice" className="image--source" />
+                    </div>
+                    <div className="description__container">
+                      <p>French Vanilla Mocha</p>
+                      <p>Nicotine Strength: <i>8mg</i></p>
+                    </div>
+                  </td>
+                  <td className="body__row--product-qty">
+                    <div className="qty--container">
+                      <p>1</p>
+                    </div>
+                  </td>
+                  <td className="body__row--product-subtotal">
+                    <div className="product-subtotal-container">
+                      <p><FontAwesome name="usd" />{'\u00A0'}30.00</p>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
             </table>
-
+            
           </div>
         </div>
         <div className="checkout__grid">
