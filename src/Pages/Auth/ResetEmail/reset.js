541f8c39710548be176c494c6db80ec859de1884
TobiahRex <tobiah.rex@gmail.com>
2017-03-06 23:58:40 +0900

    styling: Auth > Reset = GTG.

--- src/Pages/Auth/ResetEmail/reset.js
+++ src/Pages/Auth/ResetEmail/reset.js
@@ -1,43 +1,47 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-// import { browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 
 export default function Forgot() {
   return (
     <div className="reset--main">
       <div className="reset--container">
         <div className="reset__verify">
-          <div className="verify--title">
-            <div className="verify__icon">
-              <FontAwesome name="question-circle" />
-            </div>
-            <div className="verify__title">
-              <h1>Do you want to reset your password?</h1>
-              <h4>Please confirm if you want to reset your password or,{'\u00A0'}
-                <br />
-              press “Cancel” to return to the homepage.</h4>
+          <div className="verify" style={{ display: 'none' }}>
+            <div className="verify__top">
+              <div className="verify__icon">
+                <FontAwesome name="question-circle" />
+              </div>
+              <div className="verify__title">
+                <h1>Do you want to reset your password?</h1>
+                <h4>Please confirm if you want to reset your password or,{'\u00A0'}
+                  <br />
+                press “Cancel” to return to the homepage.</h4>
+              </div>
             </div>
             <div className="verify__action-btns">
-              <button className="yes-btn">
+              <button className="yes-btn" onClick={() => console.info('Reset Password!')}>
                 Yes, Reset my password
               </button>
-              <button className="cancel-btn">
+              <button className="cancel-btn" onClick={() => browserHistory.push('/')}>
                 Cancel
               </button>
             </div>
           </div>
         </div>
         <div className="reset__success">
-          <div className="success--title">
-            <div className="success__icon">
-              <FontAwesome name="exclamation-circle" />
-            </div>
-            <div className="success__title">
-              <h1>Ok, {'we\'ve'} sent you an email!</h1>
-              <h4>Please follow the instructions in the email to reset your password.</h4>
+          <div className="success">
+            <div className="success__top">
+              <div className="success__icon">
+                <FontAwesome name="exclamation-circle" />
+              </div>
+              <div className="success__title">
+                <h1>Ok, {'we\'ve'} sent you an email!</h1>
+                <h4>Please follow the instructions in the email to reset your password.</h4>
+              </div>
             </div>
             <div className="success__btn">
-              <button className="go-back-home">
+              <button className="go-back-home" onClick={() => browserHistory.push('/')}>
                 <span className="flex-btn-parent">
                   <FontAwesome name="double-angled-left" />
                   Back To Homepage
