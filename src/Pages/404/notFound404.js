e972f63f9dfc77df2025f780a34eb90f11a914fb
TobiahRex <tobiah.rex@gmail.com>
2017-03-06 19:45:06 +0900

    scaffolding: Pages > 404 = GTG

--- src/Pages/404/notFound404.js
+++ src/Pages/404/notFound404.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { browserHistory } from 'react-router';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="404--main">
+      <div className="404--container">
+        <div className="404__title">
+          <div className="title--icon">
+            <FontAwesome name="eye-slash" />
+          </div>
+          <h1>404 - Page Not Found</h1>
+        </div>
+        <div className="404__body">
+          <h5>The page you requested was not found.</h5>
+
+          <h5>If you types the URL directly, make sure the spelling is correct.</h5>
+
+          <h5>If you clicked a link to get here, then the link is outdated.  We apologize.</h5>
+
+        </div>
+        <div className="404_home-btn">
+          <button
+            className="sweep-right"
+            onClick={() => browserHistory.push('/')}
+          >Home</button>
+        </div>
+      </div>
     </div>
   );
 }
