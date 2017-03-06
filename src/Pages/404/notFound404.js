7f8d524fbe11f7a2cd2fd4b330412358e5de2949
TobiahRex <tobiah.rex@gmail.com>
2017-03-06 20:04:31 +0900

    styling: Pages > 404 = GTG

--- src/Pages/404/notFound404.js
+++ src/Pages/404/notFound404.js
@@ -4,23 +4,23 @@ import { browserHistory } from 'react-router';
 
 export default function NotFound() {
   return (
-    <div className="404--main">
-      <div className="404--container">
-        <div className="404__title">
+    <div className="not-found--main">
+      <div className="not-found--container">
+        <div className="not-found__title">
           <div className="title--icon">
             <FontAwesome name="eye-slash" />
           </div>
           <h1>404 - Page Not Found</h1>
         </div>
-        <div className="404__body">
-          <h5>The page you requested was not found.</h5>
-
-          <h5>If you types the URL directly, make sure the spelling is correct.</h5>
-
-          <h5>If you clicked a link to get here, then the link is outdated.  We apologize.</h5>
-
+        <div className="not-found__body">
+          <h4>The page you requested was not found.</h4>
+          <br />
+          <h4>If you typed the URL directly, make sure the spelling is correct.</h4>
+          <br />
+          <h4>If you clicked a link to get here, then the link is outdated.  We apologize.</h4>
+          <br />
         </div>
-        <div className="404_home-btn">
+        <div className="not-found_home-btn">
           <button
             className="sweep-right"
             onClick={() => browserHistory.push('/')}
