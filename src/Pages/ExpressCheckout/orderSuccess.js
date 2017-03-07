a4b411e327365f56ba497fb4c7b293c8eb0f903e
TobiahRex <tobiah.rex@gmail.com>
2017-03-07 19:42:00 +0900

    scaffolding: Express Checkout > orderSuccess.js > header

--- src/Pages/ExpressCheckout/orderSuccess.js
+++ src/Pages/ExpressCheckout/orderSuccess.js
@@ -1,18 +1,40 @@
 import React from 'react';
 // import { Link, browserHistory } from 'react-router';
 import FontAwesome from 'react-fontawesome';
+import moment from 'moment';
+import uuid from 'uuid';
 
 
 export default function OrderSuccess() {
   return (
     <div className="ordered--main">
       <div className="ordered--container">
-        <div className="ordered__title--icon">
-          <FontAwesome name="question-circle" />
+        <div className="ordered__title">
+          <div className="title--icon">
+            <FontAwesome name="question-circle" />
+          </div>
+          <div className="title--msg">
+            <h1>Your order has been successfully placed!</h1>
+            <h4>The invoice shown below has been sent to your email.</h4>
+          </div>
         </div>
-        <div className="ordered__title--msg">
-          <h1>Your order has been successfully placed!</h1>
-          <h4>The invoice shown below has been sent to your email.</h4>
+        <div className="ordered__header">
+          <div className="header--date">
+            <p>Date: {moment().format('LLLL')}</p>
+            <p>Invoice #: {uuid()}</p>
+          </div>
+          <div className="header--status">
+            {'<Shipping Status>'}
+          </div>
+          <div className="header--info">
+            <div className="header__tracking">
+              <p>Tracking #: {uuid()}</p>
+              <p>Order #: {uuid()}</p>
+            </div>
+            <div className="header__price">
+              Price: <FontAwesome name="usd" />{'\u00A0'}32.14
+            </div>
+          </div>
         </div>
       </div>
     </div>
