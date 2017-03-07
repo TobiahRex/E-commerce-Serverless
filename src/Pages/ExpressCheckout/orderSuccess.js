cf9810fbf057d055ed02e5852e5a15f53780efdf
TobiahRex <tobiah.rex@gmail.com>
2017-03-07 19:34:41 +0900

    scaffolding: Express Checkout > Successfully sent page.

--- src/Pages/ExpressCheckout/orderSuccess.js
+++ src/Pages/ExpressCheckout/orderSuccess.js
@@ -1,12 +1,20 @@
 import React from 'react';
 // import { Link, browserHistory } from 'react-router';
-// import FontAwesome from 'react-fontawesome';
+import FontAwesome from 'react-fontawesome';
 
 
 export default function OrderSuccess() {
   return (
-    <div className="">
-      <h1>Order Success</h1>
+    <div className="ordered--main">
+      <div className="ordered--container">
+        <div className="ordered__title--icon">
+          <FontAwesome name="question-circle" />
+        </div>
+        <div className="ordered__title--msg">
+          <h1>Your order has been successfully placed!</h1>
+          <h4>The invoice shown below has been sent to your email.</h4>
+        </div>
+      </div>
     </div>
   );
 }
