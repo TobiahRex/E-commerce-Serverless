c8b77579cac6986983b9c1c4748ba0d707894a6c
TobiahRex <tobiah.rex@gmail.com>
2017-03-07 19:30:00 +0900

    created orderSuccess: js & style files.

---
+++ src/Pages/ExpressCheckout/orderSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link, browserHistory } from 'react-router';
+// import FontAwesome from 'react-fontawesome';
+
+
+export default function OrderSuccess() {
+  return (
+    <div className="">
+      <h1>Order Success</h1>
+    </div>
+  );
+}
