5227678c94463e1fc2f727d005be1f66c85ee3aa
TobiahRex <tobiah.rex@gmail.com>
2017-03-14 21:11:04 +0900

    dynamically rendering UserDashboard > userHomeDash & userAddressBook.

---
+++ src/Pages/UserDashboard/userDashComponents/userDashboard_welcomeMsg/userWelcomeMsg.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UserWelcomeMsg() {
+  return (
+    <div className="user-dash__welcome-msg">
+      <h1>Hello, {'<Bruce Wayne>'}</h1>
+    </div>
+  );
+}
