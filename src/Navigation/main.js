6e3d40bea5d10b71fed4ff684e1caf405da291b1
TobiahRex <tobiah.rex@gmail.com>
2017-03-15 23:35:59 +0900

    created mediaRoutes.js

--- src/Navigation/main.js
+++ src/Navigation/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, IndexRoute, Switch } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 // import Navbar from '../Pages/Navbar/navbarComponents/navbarMain';
 
 /* NOTE:
@@ -10,22 +10,13 @@ import { Route, IndexRoute, Switch } from 'react-router';
 import App from '../App';
 import NotFound from '../Pages/404/notFound404';
 // ----------------------------- Media ------------------------------
-import About from '../Pages/Legal/about';
-import ContactUs from '../Pages/Legal/contactUs';
-import Phone from '../Pages/Legal/phone';
-import UserStories from '../Pages/Media/userStories';
-import VapeNews from '../Pages/Media/vapeNews';
-import Faqs from '../Pages/Legal/faqs';
-import MissionStatement from '../Pages/Legal/missionStatement';
-import SocialMedia from '../Pages/Legal/socialMedia';
-import Wholesale from '../Pages/Legal/wholesale';
-import Affiliates from '../Pages/Legal/affiliateProgram';
 
 import {
   Legal,
   Auth,
   UserDashboard,
   Checkout,
+  Media,
 } from './';
 
 // ----------------------------- Shopping ---------------------------
@@ -42,16 +33,7 @@ export default (
     <IndexRoute path="/" component={Homepage} />
     <Route path="home" component={Homepage} />
     {/* MEDIA */}
-    <Route path="about" component={About} />
-    <Route path="contact_us" component={ContactUs} />
-    <Route path="phone" component={Phone} />
-    <Route path="vape_news" component={VapeNews} />
-    <Route path="stories" component={UserStories} />
-    <Route path="social_media" component={SocialMedia} />
-    <Route path="mission_statement" component={MissionStatement} />
-    <Route path="faqs" component={Faqs} />
-    <Route path="wholesale" component={Wholesale} />
-    <Route path="affiliate_program" component={Affiliates} />
+    <Media />
     <Legal />
     <Auth />
     <Checkout />
---
+++ src/Navigation/mediaRoutes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+
+// ----------------------------- Media -------------------------------
+import About from '../Pages/Legal/about';
+import ContactUs from '../Pages/Legal/contactUs';
+import Phone from '../Pages/Legal/phone';
+import UserStories from '../Pages/Media/userStories';
+import VapeNews from '../Pages/Media/vapeNews';
+import Faqs from '../Pages/Legal/faqs';
+import MissionStatement from '../Pages/Legal/missionStatement';
+import SocialMedia from '../Pages/Legal/socialMedia';
+import Wholesale from '../Pages/Legal/wholesale';
+import Affiliates from '../Pages/Legal/affiliateProgram';
+
+const authRoutes = () => (
+  <Switch>
+    <Route path="about" component={About} />
+    <Route path="contact_us" component={ContactUs} />
+    <Route path="phone" component={Phone} />
+    <Route path="vape_news" component={VapeNews} />
+    <Route path="stories" component={UserStories} />
+    <Route path="social_media" component={SocialMedia} />
+    <Route path="mission_statement" component={MissionStatement} />
+    <Route path="faqs" component={Faqs} />
+    <Route path="wholesale" component={Wholesale} />
+    <Route path="affiliate_program" component={Affiliates} />
+  </Switch>
+);
+
+export default authRoutes;
