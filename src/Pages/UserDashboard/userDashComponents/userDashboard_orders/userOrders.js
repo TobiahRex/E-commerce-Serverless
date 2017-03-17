360aee46f1f7391fec0ca8c7437fc5c10a832931
TobiahRex <tobiah.rex@gmail.com>
2017-03-17 22:24:29 +0900

    scaffolding: UserDashboard > Orders > Filter Section

--- src/Pages/UserDashboard/userDashComponents/userDashboard_orders/userOrders.js
+++ src/Pages/UserDashboard/userDashComponents/userDashboard_orders/userOrders.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import 'react-router';
+import FontAwesome from 'react-fontawesome';
 import Breadcrumb from '../../../../Components/breadcrumbs';
 import UserSideBar from '../userDashboard_sidebar/userSideBar';
 import UserWelcomeMsg from '../userDashboard_welcomeMsg/userWelcomeMsg';
@@ -35,6 +36,26 @@ function UserOrders({ location }) {
                 </div>
                 <div className="tabs--open-orders">
                   <p>Open Orders</p>
+                </div>
+              </div>
+
+              <div className="dashboard__filter">
+                {/* TODO: These 3 element will be rendered dynamically. */}
+                <div className="filter__results-msg">
+                  <p className="result-msg--number">99</p>
+                  <p className="results-msg--periodcity">Open Order(s)</p>
+                  <p className="results-msg--rest">placed in the last</p>
+                </div>
+                <div className="filter__periodcity-ddn--container">
+                  <div className="periodicity__ddn--readout">
+                    <input type="text" className="readout--msg" disabled value="6 Months" />
+                    <button className="readout--btn">
+                      <FontAwesome name="angle-down" />
+                    </button>
+                  </div>
+                  <div className="periodicity__ddn--content">
+
+                  </div>
                 </div>
               </div>
             </div>
