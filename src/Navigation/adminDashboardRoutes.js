3ac41f89e10b789fc3b2883e9315e6727e1cac04
TobiahRex <tobiah.rex@gmail.com>
2017-03-27 18:09:24 +0900

    building: AdminDashboard folder structure.

--- src/Navigation/adminDashboardRoutes.js
+++ src/Navigation/adminDashboardRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import AdminDashboard from '../Pages/AdminDashboard/adminDashboard';
+import AdminDashboard from '../Pages/AdminDashboard/adminDashComponents/adminDashboard';
 
 const AdminDashboardRoutes = () => (
   <Route path="/admin_:id" component={AdminDashboard} />
