94a46e360bf2d9eefbf978e5b9327d33eff1c04f
TobiahRex <tobiah.rex@gmail.com>
2017-03-27 23:58:10 +0900

    scaffolding: WEB > AdminDashboard > Home > Web Traffic.

--- src/Pages/AdminDashboard/adminDashComponents/adminDashboard_home/adminHomeDash.js
+++ src/Pages/AdminDashboard/adminDashComponents/adminDashboard_home/adminHomeDash.js
@@ -270,7 +270,7 @@ function AdminHomeDash({ location }) {
                     </div>
                     <div className="msg__text">
                       <div className="email__input--container">
-                        <p>This report will be sent via Text Message to the following number...</p>
+                        <p>This report will be sent via SMS Message (Text) to the following number...</p>
                         <div className="emai__input">
                           <label htmlFor="cell-input">Cell Number</label>
                           <div className="cell-input__flag--container">
@@ -288,13 +288,13 @@ function AdminHomeDash({ location }) {
                         </div>
                         <div className="email__action-btns">
                           <button className="primary-button sweep-right">Cancel</button>
-                          <button className="primary-button sweep-right">Send Email</button>
+                          <button className="primary-button sweep-right">Send SMS</button>
                         </div>
                       </div>
-                      <div className="email__sent">
-                        <p>Report sent via email attaachment to...</p>
+                      <div className="text__sent">
+                        <p>Report sent via SMS to...</p>
                         <br />
-                        <p>{'<email address here>'}</p>
+                        <p>{'<cell number here>'}</p>
                         <div className="email__action-btns">
                           <button className="primary-button sweep-right">OK</button>
                         </div>
@@ -304,6 +304,111 @@ function AdminHomeDash({ location }) {
                 </div>
               </div>
 
+              <div className="dashboard__web-traffic">
+                <div className="web-traffic__title admin-dash__small-title">
+                  <h3>Web Traffic</h3>
+                </div>
+
+                <div className="web-traffic__body">
+                  <div className="body__header">
+                    <ul className="header--list">
+                      <li className="list--option">
+                        <div className="option--periodicty">
+                          <div className="periodicty__ddn--container">
+                            <div className="periodicty__ddn--readout">
+                              <input type="text" className="readout--msg" disabled value="Today (24hrs)" />
+                              <button className="readout--btn sweep-right">
+                                <span className="flex-btn-parent">
+                                  <FontAwesome name="angle-down" />
+                                </span>
+                              </button>
+                            </div>
+                            <div className="periodicity__ddn--content">
+                              <ul className="ddn--content__list">
+                                {/* NOTE: These need to be rendered dynamically, and the option selected, should absent from the available choices. */}
+                                <li className="list--option sweep-right">
+                                  <p>Today (24hrs)</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Week</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Month</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Quarter</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Semi Annual</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Annual</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Lifetime</p>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+
+
+                        </div>
+                      </li>
+                      <li className="list--option">
+                        <div className="option--periodicty">
+                          <div className="periodicty__ddn--container">
+                            <div className="periodicty__ddn--readout">
+                              <input type="text" className="readout--msg" disabled value="Today (24hrs)" />
+                              <button className="readout--btn sweep-right">
+                                <span className="flex-btn-parent">
+                                  <FontAwesome name="angle-down" />
+                                </span>
+                              </button>
+                            </div>
+                            <div className="periodicity__ddn--content">
+                              <ul className="ddn--content__list">
+                                {/* NOTE: These need to be rendered dynamically, and the option selected, should absent from the available choices. */}
+                                <li className="list--option sweep-right">
+                                  <p>Today (24hrs)</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Week</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Month</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Quarter</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Semi-Annual</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Annual</p>
+                                </li>
+                                <li className="list--option sweep-right">
+                                  <p>Lifetime</p>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+
+
+                        </div>
+                      </li>
+                      <li className="list--option">
+                        <div className="option--file-output">
+                          <FontAwesome name="file-o" />
+                          {'\u00A0'}
+                          <span className="file-output__file-name">
+                            20161225_Today_SalesReport.pdf
+                          </span>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
 
             </div>
           </div>
