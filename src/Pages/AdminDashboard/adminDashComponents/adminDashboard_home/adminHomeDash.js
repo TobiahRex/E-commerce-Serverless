a5ec2174eff6d231d0e8ac02087886c6406f0e20
TobiahRex <tobiah.rex@gmail.com>
2017-03-28 13:59:23 +0900

    scaffolding: WEB > AdminDashboard > Home > Web Traffic = GTG

--- src/Pages/AdminDashboard/adminDashComponents/adminDashboard_home/adminHomeDash.js
+++ src/Pages/AdminDashboard/adminDashComponents/adminDashboard_home/adminHomeDash.js
@@ -478,31 +478,193 @@ function AdminHomeDash({ location }) {
                         </div>
                       </li>
                       <li className="list--option">
-                        <div className="option--">
-                          <h4></h4>
-                          <p></p>
+                        <div className="option--new-visitors">
+                          <h4>55%</h4>
+                          <p>New Visitors</p>
                         </div>
                       </li>
                       <li className="list--option">
-                        <div className="option--">
-                          <h4></h4>
-                          <p></p>
+                        <div className="option--avg-visit-time">
+                          <h4>5 min</h4>
+                          <p>Avg. Visit Time</p>
                         </div>
                       </li>
                       <li className="list--option">
-                        <div className="option--">
-                          <h4></h4>
-                          <p></p>
+                        <div className="option--page-views">
+                          <h4>40,000</h4>
+                          <p>Page Views</p>
                         </div>
                       </li>
                       <li className="list--option">
-                        <div className="option--">
-                          <h4></h4>
-                          <p></p>
+                        <div className="option--mobile-views">
+                          <h4>60%</h4>
+                          <p>Mobile Views</p>
                         </div>
                       </li>
                     </ul>
                   </div>
+
+                  <div className="body__map">
+                    <div className="map__google-heat-map">
+                      <span>{'<Google Heat Map>'}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="dashboard__latest-orders">
+                <div className="latest-orders__header">
+                  <ul className="header--list">
+                    <li className="list--option">
+                      <div className="option--periodicty">
+                        <div className="last-order__ddn--container">
+                          <div className="last-order__ddn--readout">
+                            <input type="text" className="readout--msg" disabled value="12 mins" />
+                            <button className="readout--btn sweep-right">
+                              <span className="flex-btn-parent">
+                                <FontAwesome name="angle-down" />
+                              </span>
+                            </button>
+                          </div>
+                          <div className="last-order__ddn--content">
+                            <ul className="ddn--content__list">
+                              {/* NOTE: These need to be rendered dynamically, and the option selected, should absent from the available choices. */}
+                              <li className="list--option sweep-right">
+                                <p>Today (24hrs)</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Week</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Month</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Quarter</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Semi Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Lifetime</p>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+
+                      </div>
+                    </li>
+                    <li className="list--option">
+                      <div className="option--visitors">
+                        <div className="visitors__ddn--container">
+                          <div className="visitors__ddn--readout">
+                            <div className="ddn__options">
+                              <label htmlFor="ddn__btn">Visitors Per</label>
+                              <input type="text" id="ddn__btn" className="readout--msg" value="Today (24hrs)" disabled />
+                              <button className="readout--btn sweep-right">
+                                <span className="flex-btn-parent">
+                                  <FontAwesome name="angle-down" />
+                                </span>
+                              </button>
+                            </div>
+                            <div className="ddn__result">
+                              <h3>99</h3>
+                            </div>
+                          </div>
+                          <div className="visitors__ddn--content">
+                            <ul className="ddn--content__list">
+                              {/* NOTE: These need to be rendered dynamically, and the option selected, should absent from the available choices. */}
+                              <li className="list--option sweep-right">
+                                <p>Minute</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Hour</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Today (24hrs)</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Week</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Month</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Quarter</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Semi-Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Lifetime</p>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+
+
+                      </div>
+                    </li>
+                    <li className="list--option">
+                      <div className="option--purchases">
+                        <div className="purchases__ddn--container">
+                          <div className="purchases__ddn--readout">
+                            <div className="ddn__options">
+                              <label htmlFor="ddn__btn">Purchases Per</label>
+                              <input type="text" id="ddn__btn" className="readout--msg" value="Today (24hrs)" disabled />
+                              <button className="readout--btn sweep-right">
+                                <span className="flex-btn-parent">
+                                  <FontAwesome name="angle-down" />
+                                </span>
+                              </button>
+                            </div>
+                            <div className="ddn__result">
+                              <h3>99</h3>
+                            </div>
+                          </div>
+                          <div className="purchases__ddn--content">
+                            <ul className="ddn--content__list">
+                              {/* NOTE: These need to be rendered dynamically, and the option selected, should absent from the available choices. */}
+                              <li className="list--option sweep-right">
+                                <p>Minute</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Hour</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Today (24hrs)</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Week</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Month</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Quarter</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Semi-Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Annual</p>
+                              </li>
+                              <li className="list--option sweep-right">
+                                <p>Lifetime</p>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+
+
+                      </div>
+                    </li>
+                  </ul>
                 </div>
               </div>
 
