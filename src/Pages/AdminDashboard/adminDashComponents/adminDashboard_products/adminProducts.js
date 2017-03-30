ff905335f709c1149e9bc3dbac53d335b808d237
TobiahRex <tobiah.rex@gmail.com>
2017-03-31 01:56:11 +0900

    scaffolding: AdminDash > Products > Pending Products

--- src/Pages/AdminDashboard/adminDashComponents/adminDashboard_products/adminProducts.js
+++ src/Pages/AdminDashboard/adminDashComponents/adminDashboard_products/adminProducts.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import FontAwesome from 'react-fontawesome';
 import { LineChart } from 'react-svg-chart';
-
+import uuid from 'uuid';
 
 import Breadcrumb from '../../../../Components/breadcrumbs';
 import AdminSideBar from '../adminDashboard_sidebar/adminSideBar';
@@ -177,24 +177,284 @@ function AdminProducts({ location }) {
                                   Qty
                                 </p>
                               </th>
-                              <th className="header--qty">
-                                <p>Colors</p>
+                            </tr>
+                          </thead>
+                          <tbody className="table__body">
+                            <tr className="body__row">
+                              <td className="row__image">
+                                <img className="image--primary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                              </td>
+                              <td className="row--desc">
+                                <ul className="desc--list">
+                                  <li className="list--desc">
+                                    <label htmlFor="title">Title</label>
+                                    <p id="title">French Vanilla Mocha</p>
+                                  </li>
+                                  <li className="list--desc">
+                                    <label htmlFor="sub-title">Nicotine</label>
+                                    <p id="sub-title">6mg</p>
+                                  </li>
+                                  <li className="list-sku">
+                                    <label htmlFor="sku">SKU</label>
+                                    <p id="sku">{uuid()}</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--nicotine">
+                                <div className="nicotine--option">
+                                  <span>2mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>4mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>6mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>8mg</span>
+                                </div>
+                              </td>
+                              <td className="row--prices">
+                                <div className="row__japanese">
+                                  <p>{'\u00A5'} {'35'}.00</p>
+                                </div>
+                                <div className="row__usd">
+                                  <p>$ {'30'}.00</p>
+                                </div>
+                              </td>
+                              <td className="row--qty">
+                                <p>35</p>
+                              </td>
+                            </tr>
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="main__product-stats">
+                    <div className="product-stats__header">
+                      <div className="header__title admin-dash__small-title">
+                        <h3>Product Stats</h3>
+                      </div>
+                    </div>
+
+                    <div className="product-stats__body ">
+                      <div className="body__table">
+                        <table className="table--container">
+                          <thead className="table__header">
+                            <tr className="header--row">
+                              <th className="header--images">
+                                <p>Image(s)</p>
+                              </th>
+                              <th className="header--desc">
+                                <p>Description</p>
+                              </th>
+                              <th className="header--page-views">
+                                <p>Page Views</p>
+                              </th>
+                              <th className="header--clicks">
+                                <p>Clicks</p>
+                              </th>
+                              <th className="header--added-to-cart">
+                                <p>Cart Adds</p>
+                              </th>
+                              <th className="header--purchased">
+                                <p>Purchased</p>
+                              </th>
+                              <th className="header--rating">
+                                <p>Rating</p>
                               </th>
                             </tr>
                           </thead>
                           <tbody className="table__body">
                             <tr className="body__row">
-                              <td className="row--image">
-                                <img src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                              <td className="row__image">
+                                <img className="image--primary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
                               </td>
                               <td className="row--desc">
-                                <label htmlFor="title">Title</label>
-                                <p id="title">French Vanilla Mocha</p>
+                                <ul className="desc--list">
+                                  <li className="list--desc">
+                                    <label htmlFor="title">Title</label>
+                                    <p id="title">French Vanilla Mocha</p>
+                                  </li>
+                                  <li className="list--desc">
+                                    <label htmlFor="sub-title">Nicotine</label>
+                                    <p id="sub-title">6mg</p>
+                                  </li>
+                                  <li className="list-sku">
+                                    <label htmlFor="sku">SKU</label>
+                                    <p id="sku">{uuid()}</p>
+                                  </li>
+                                </ul>
                               </td>
-                              <td className="row--nicotine"></td>
-                              <td className="row--prices"></td>
-                              <td className="row--qty"></td>
+                              <td className="row--page-views">
+                                <ul className="page-views--list">
+                                  <li className="list--visitors">
+                                    <label htmlFor="title">Visitors</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                  <li className="list--members">
+                                    <label htmlFor="title">Members</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                  <li className="list--time">
+                                    <p>00:00:12</p>
+                                    <p>per view</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--clicks">
+                                <ul className="clicks--list">
+                                  <li className="list--visitors">
+                                    <label htmlFor="title">Visitors</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                  <li className="list--members">
+                                    <label htmlFor="title">Members</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--added-to-cart">
+                                <ul className="added-to-cart--list">
+                                  <li className="list--visitors">
+                                    <label htmlFor="title">Visitors</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                  <li className="list--members">
+                                    <label htmlFor="title">Members</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--purchased">
+                                <ul className="purchased--list">
+                                  <li className="list--once">
+                                    <label htmlFor="title">Once</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                  <li className="list--repeat">
+                                    <label htmlFor="title">Repeat</label>
+                                    <p id="title">123123123</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--rating">
+                                <ul className="rating--list">
+                                  <li className="list--entries">
+                                    <label htmlFor="title">Entries</label>
+                                    <p id="title">789 - {0.3 * 100}%</p>
+                                  </li>
+                                  <li className="list--grade">
+                                    <label htmlFor="title">Grade</label>
+                                    <p id="title">+ {0.86 * 100}%</p>
+                                  </li>
+                                </ul>
+                              </td>
                             </tr>
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="main__pending-products">
+                    <div className="pending-products__header">
+                      <div className="header__title admin-dash__small-title">
+                        <h3>Pending Products(s)</h3>
+                      </div>
+                      <div className="header__msg">
+                        <p>* Double click row to modify information</p>
+                      </div>
+                    </div>
+                    <div className="pending-products__sub-header">
+                      <div className="sub-header__icon">
+                        <FontAwesome name="info-circle" />
+                        <p>These are new product entries that are incomplete.  You must fill out all required fields before they will be added to the inventory.</p>
+                      </div>
+                    </div>
+
+                    <div className="product-table__body ">
+                      <div className="body__table">
+                        <table className="table--container">
+                          <thead className="table__header">
+                            <tr className="header--row">
+                              <th className="header--images">
+                                <p>Image(s)</p>
+                              </th>
+                              <th className="header--desc">
+                                <p>Description</p>
+                              </th>
+                              <th className="header--nicotine">
+                                <p>Nicotine</p>
+                              </th>
+                              <th className="header--prices">
+                                <p>Prices</p>
+                              </th>
+                              <th className="header--qty">
+                                <p>
+                                  Qty
+                                </p>
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody className="table__body">
+                            <tr className="body__row">
+                              <td className="row__image">
+                                <img className="image--primary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                                <img className="image--secondary" src="../Images/nj2jp_juice_card_fvm.png" alt="Juice" />
+                              </td>
+                              <td className="row--desc">
+                                <ul className="desc--list">
+                                  <li className="list--desc">
+                                    <label htmlFor="title">Title</label>
+                                    <p id="title">French Vanilla Mocha</p>
+                                  </li>
+                                  <li className="list--desc">
+                                    <label htmlFor="sub-title">Nicotine</label>
+                                    <p id="sub-title">6mg</p>
+                                  </li>
+                                  <li className="list-sku">
+                                    <label htmlFor="sku">SKU</label>
+                                    <p id="sku">{uuid()}</p>
+                                  </li>
+                                </ul>
+                              </td>
+                              <td className="row--nicotine">
+                                <div className="nicotine--option">
+                                  <span>2mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>4mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>6mg</span>
+                                </div>
+                                <div className="nicotine--option">
+                                  <span>8mg</span>
+                                </div>
+                              </td>
+                              <td className="row--prices">
+                                <div className="row__japanese">
+                                  <p>{'\u00A5'} {'35'}.00</p>
+                                </div>
+                                <div className="row__usd">
+                                  <p>$ {'30'}.00</p>
+                                </div>
+                              </td>
+                              <td className="row--qty">
+                                <p>35</p>
+                              </td>
+                            </tr>
+
                           </tbody>
                         </table>
                       </div>
