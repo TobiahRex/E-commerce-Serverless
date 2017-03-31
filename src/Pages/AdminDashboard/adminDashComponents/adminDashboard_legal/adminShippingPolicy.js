c3aad7808c363b0bc4cb46d43637a445855652b3
TobiahRex <tobiah.rex@gmail.com>
2017-03-31 19:46:22 +0900

    scaffolding: AdminDashboard > Shipping Policy > Added Edit Section = GTG

--- src/Pages/AdminDashboard/adminDashComponents/adminDashboard_legal/adminShippingPolicy.js
+++ src/Pages/AdminDashboard/adminDashComponents/adminDashboard_legal/adminShippingPolicy.js
@@ -39,42 +39,54 @@ export default function AdminShippingPolicy() {
                   <br /><br />
                   We offer priority handling and delivery on most items upon request. For special instructions, PLEASE ORDER BY PHONE.
                 </p>
-              </div>
-
-              <div className="legal__action-section--container">
-                <div className="action-section__back-btn">
-                  <button className="back-btn primary-flex-button sweep-right">
-                    <span className="flex-parent-btn">
-                      <FontAwesome name="angle-double-left" />
-                      {'\u00A0'}
-                      Back
-                    </span>
-                  </button>
-                  <button className="save-btn primary-flex-button sweep-right">
-                    Edit
-                  </button>
+                <div className="legal__action-section--container">
+                  <div className="action-section__back-btn">
+                    <button className="back-btn primary-flex-button sweep-right">
+                      <span className="flex-parent-btn">
+                        <FontAwesome name="angle-double-left" />
+                        {'\u00A0'}
+                        Back
+                      </span>
+                    </button>
+                    <button className="save-btn primary-flex-button sweep-right">
+                      Edit
+                    </button>
+                  </div>
                 </div>
-
+              </div>
+              <div className="legal__body--edit">
+                <textarea cols="30" rows="100">
+                  PROCESSING AND SHIPPING
+                  <br /><br />
+                  Orders will be processed within 1-3 business days after your full payment has been received. Shipment time will be between 2-7 business days depending on your physical address. The total wait time after you place your order will be between 1-7 business days. Shipping time and cost vary depending on location, and product ordered.
+                  <br /><br />
+                  IN A HURRY?
+                  <br /><br />
+                  We offer priority handling and delivery on most items upon request. For special instructions, PLEASE ORDER BY PHONE.
+                </textarea>
                 <div className="action-section__save-btn">
-                  <button className="save-btn primary-flex-button sweep-right">
-                    Save
-                  </button>
-                </div>
-                <div>
-                  <button className="save-btn primary-flex-button-saving sweep-right">
-                    <FontAwesome name="refresh" spin />
-                    {'\u00A0'} Saving...
-                  </button>
-                </div>
-                <div>
-                  <button className="save-btn primary-flex-button-saved sweep-right">
-                    <span className="flex-parent-btn">
-                      <FontAwesome name="check-circle" />
-                      {'\u00A0'} Saved!
-                    </span>
-                  </button>
+                  <div className="action-section__save-btn">
+                    <button className="save-btn primary-flex-button sweep-right">
+                      Save
+                    </button>
+                  </div>
+                  <div>
+                    <button className="save-btn primary-flex-button-saving sweep-right">
+                      <FontAwesome name="refresh" spin />
+                      {'\u00A0'} Saving...
+                    </button>
+                  </div>
+                  <div>
+                    <button className="save-btn primary-flex-button-saved sweep-right">
+                      <span className="flex-parent-btn">
+                        <FontAwesome name="check-circle" />
+                        {'\u00A0'} Saved!
+                      </span>
+                    </button>
+                  </div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
