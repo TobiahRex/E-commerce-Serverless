7f7a706ed16c6aaac561afa98a7235a3f33dbaa0
TobiahRex <tobiah.rex@gmail.com>
2017-04-05 18:13:47 +0900

    refactored: SHOP dropdown.

--- src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop/navbarNavs_shop.js
+++ src/Pages/Navbar/navbarComponents/navbar_web/navbar_web_nav/navbarNavs_shop/navbarNavs_shop.js
@@ -5,8 +5,8 @@ import NavbarNavsShopDropdnContent from './navbarNavs_shop_dropdn_content';
 class NavbarNavsShop extends PureComponent {
   render() {
     return (
-      <div className="navbar-actionSection-lower-shop sweep-right-red">
-        <span className="shop-main-button">
+      <div className="navbar-actionSection-lower-shop">
+        <span className="shop-main-button ">
           <div className="shop-main-button-title">
             <span>SHOP</span>
           </div>
