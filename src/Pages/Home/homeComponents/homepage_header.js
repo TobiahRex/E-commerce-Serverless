9c941777ce4122f6ef377cf478c29f47302222b6
TobiahRex <tobiah.rex@gmail.com>
2017-04-07 16:14:11 +0900

    refactored: Web Header > Styles.

--- src/Pages/Home/homeComponents/homepage_header.js
+++ src/Pages/Home/homeComponents/homepage_header.js
@@ -2,14 +2,36 @@ import React from 'react';
 
 export default function HomepageHeader() {
   return (
-    <header className="homepage-header-img-src">
-      <div className="homepage-header-innner-container">
-        <img
-          alt="NicJuice2Japan" className="homepage-header-inner-container-img-src"
-        />
-        <h1 className="hompage-header-title-1">Fastest Nicotine e-Juice Delivery In Japan</h1>
-        <h1 className="hompage-header-title-2">Guaranteed!</h1>
+    <header className="homepage__header-img-src">
+      <div className="homepage__header--inner-container">
+        <img alt="NicJuice2Japan" className="header__img-src" />
+        <div className="header__msg--container">
+          <h1 className="msg__title">Fastest Nicotine e-Juice Delivery In Japan</h1>
+          <h1 className="msg__subtitle">Guaranteed!</h1>
+        </div>
+        <button className="header__cta primary-button shutter-out-horizontal">
+          Shop Now
+        </button>
       </div>
     </header>
   );
 }
+
+/*
+TODO
+1. Add "Shutter Out Horizontal" CTA to Splash Header.
+
+2. Add arrow down pulse to Home pages.
+
+3. Rename Homepage style sheets classes to BEM syntax.
+
+4. Add sectional scrolling to Homepage.
+
+5. Create Mockups & Wireframes for Homepage bottom section.
+
+6. Add single parallax for Homepage image.
+http://jsfiddle.net/X7UwG/
+
+7. Add fade in transition css for Homepage Header title
+http://jsfiddle.net/SO_AMK/VV2ek/
+*/
