5c6592e4c2295bc049f1dbdf1f8d1f3edb8f23cf
TobiahRex <tobiah.rex@gmail.com>
2017-04-08 17:59:08 +0900

    creatd NavBob component.

--- src/Pages/Home/homeComponents/homepage_header.js
+++ src/Pages/Home/homeComponents/homepage_header.js
@@ -1,14 +1,12 @@
 import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
-import FontAwesome from 'react-fontawesome';
-import Scroll from 'react-scroll';
+import NavBob from './homepage_navBob';
 
 const propTypes = {
   height: PropTypes.number.isRequired,
 };
 
 function HomepageHeader({ height }) {
-  const scroll = Scroll.animateScroll;
   return (
     <header className="homepage__header--img-src" style={{ height }} >
       <div className="header__upper">
@@ -23,22 +21,7 @@ function HomepageHeader({ height }) {
           </button>
         </div>
       </div>
-      <div className="header__nav-down--container hover-bob-active">
-        <button
-          className="nav-down__button"
-          onClick={() => scroll.scrollTo(886, {
-            duration: 1500,
-            delay: 100,
-            smooth: 'easeInOutQuint',
-          })}
-        >
-          <FontAwesome
-            className="button__icon"
-            name="angle-double-down"
-            size="5x"
-          />
-        </button>
-      </div>
+      <NavBob className={'header__nav-down--container'} height={height} />
     </header>
   );
 }
---
+++ src/Pages/Home/homeComponents/homepage_navBob.js
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react';
+import FontAwesome from 'react-fontawesome';
+import Scroll from 'react-scroll';
+
+const propTypes = {
+  height: PropTypes.number.isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+function NavBob({ height, className }) {
+  const scroll = Scroll.animateScroll;
+  return (<div className={`${className} hover-bob-active`}>
+    <button
+      className="nav-down__button"
+      onClick={() => scroll.scrollTo(height, {
+        duration: 1500,
+        delay: 100,
+        smooth: 'easeInOutQuint',
+      })}
+    >
+      <FontAwesome
+        className="button__icon"
+        name="angle-double-down"
+        size="5x"
+      />
+    </button>
+  </div>
+  );
+}
+
+NavBob.propTypes = propTypes;
+export default NavBob;
