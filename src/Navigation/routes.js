6d87b247db82154152523f21bbf0b5667bd713d0
TobiahRex <tobiah.rex@gmail.com>
2017-04-11 15:06:04 +0900

    js: added requireAuth to dashboard routes.

--- src/Navigation/routes.js
+++ src/Navigation/routes.js
@@ -6,7 +6,11 @@ import Routes from './index';
 import AuthService from '../services/utils/authService';
 
 const auth = new AuthService(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN);
-console.warn('auth: ', auth);
+
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) replace({ pathname: '/login' });
+};
+
 export default (
   <Route path="/" component={App} auth={auth}>
     <IndexRoute component={Homepage} />
@@ -15,8 +19,8 @@ export default (
     {Routes.LegalRoutes()}
     {Routes.AuthRoutes()}
     {Routes.CheckoutRoutes()}
-    {Routes.UserDashboardRoutes()}
-    {Routes.AdminDashboardRoutes()}
+    {Routes.UserDashboardRoutes(requireAuth)}
+    {Routes.AdminDashboardRoutes(requireAuth)}
     {Routes.NotFoundRoute()}
   </Route>
 );
