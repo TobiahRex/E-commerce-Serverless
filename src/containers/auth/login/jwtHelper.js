3f907d1282a332d0cacf2d8b663acdcff05cc858
TobiahRex <tobiah.rex@gmail.com>
2017-04-12 18:51:53 +0900

    js: jwtHelper.js > created getTokenexpirationDate && isTokenExpired  = UNTESTED

--- src/containers/auth/login/jwtHelper.js
+++ src/containers/auth/login/jwtHelper.js
@@ -1,5 +1,16 @@
 import decode from 'jwt-decode';
 
 export function getTokenExpirationDate(token) {
-  const decoded
+  const decoded = decode(token);
+  if (!decoded.exp) return null;
+
+  const date = new Date(0);
+  date.setUTCSeconds(decoded.exp);
+  return date;
+}
+
+export function isTokenExpired(token) {
+  const date = getTokenExpirationDate(token);
+  if (date === null) return false;
+  return !(date.valueOf() > new Date().valueOf());
 }
