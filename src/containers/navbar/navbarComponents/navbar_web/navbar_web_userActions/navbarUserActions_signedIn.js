8aa15488448f829b89ca64ccbecde70afa59b8a9
TobiahRex <tobiah.rex@gmail.com>
2017-04-13 16:17:49 +0900

    refactored navbarUserActions_signedIn.js for routerActions.

--- src/containers/navbar/navbarComponents/navbar_web/navbar_web_userActions/navbarUserActions_signedIn.js
+++ src/containers/navbar/navbarComponents/navbar_web/navbar_web_userActions/navbarUserActions_signedIn.js
@@ -1,8 +1,14 @@
-import React, { PureComponent } from 'react';
-import { Link, browserHistory } from 'react-router';
+import React, { PureComponent, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { routerActions } from 'react-router-redux';
+import { Link } from 'react-router';
 import uuid from 'uuid'; // TODO Remove this once you have user ID's from BE.
 
 class NavbarUserActionsSignin extends PureComponent {
+  static propTypes = {
+    push: PropTypes.func.isRequired,
+  }
   static styles = {
     hidden: {
       display: 'none',
@@ -13,6 +19,7 @@ class NavbarUserActionsSignin extends PureComponent {
   signOut = () => console.info('user has signed out.');
 
   render() {
+    const { push } = this.props;
     return (
       <ul
         // style={NavbarUserActionsSignin.styles.hidden}
@@ -27,12 +34,12 @@ class NavbarUserActionsSignin extends PureComponent {
             }}
           >Logout</a>
         </li>
-        <li className="checkout-title" onClick={() => browserHistory.push('/express_checkout')}>
+        <li className="checkout-title" onClick={() => push('/express_checkout')}>
           <Link to={'/express_checkout'} className="navbar-userActions-checkout-title-link">
             Checkout
           </Link>
         </li>
-        <li className="my-account-title" onClick={() => browserHistory.push(`/user_${uuid()}`)}>
+        <li className="my-account-title" onClick={() => push(`/user_${uuid()}`)}>
           <Link to={`/user_${uuid()}`} className="navbar-userActions-myaccount-title-link">
             <img src="../Images/mock_profile-pic.png" alt="My Account" className="signedIn--profile-pic" />
           </Link>
@@ -42,5 +49,6 @@ class NavbarUserActionsSignin extends PureComponent {
     );
   }
 }
-
-export default NavbarUserActionsSignin;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ ...routerActions }, dispatch);
+export default connect(null, mapDispatchToProps)(NavbarUserActionsSignin);
