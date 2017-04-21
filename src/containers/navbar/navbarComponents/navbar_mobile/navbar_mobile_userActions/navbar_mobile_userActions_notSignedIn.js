cd1c51d621c2570ce0f543bea737e3e76aa17069
TobiahRex <tobiah.rex@gmail.com>
2017-04-21 21:24:18 +0900

    refactoring navbar_mobile_userActions_notSignedIn.js

--- src/containers/navbar/navbarComponents/navbar_mobile/navbar_mobile_userActions/navbar_mobile_userActions_notSignedIn.js
+++ src/containers/navbar/navbarComponents/navbar_mobile/navbar_mobile_userActions/navbar_mobile_userActions_notSignedIn.js
@@ -1,32 +1,14 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router';
 
-const { objectOf, any } = React.PropTypes;
-
-class NavbarMobileUserActionsNotSignedIn extends PureComponent {
-  static propTypes = {
-    profile: objectOf(any),
-  }
-  static defaultProps = {
-    profile: {
-      picture: '../images/default-user.png',
-    },
-  }
-  render() {
-    return (
-      <ul className="actions__notSignedIn--list">
-        <li className="list--login sweep-right-red">
-          <Link
-            to={'/login'}
-            className="login__link"
-          >Login</Link>
-        </li>
-      </ul>
-    );
-  }
+export default function NavbarMobileUserActionsNotSignedIn() {
+  return (
+    <ul className="actions__notSignedIn--list">
+      <li className="list--login sweep-right-red">
+        <Link to={'/login'} className="login__link">
+          Login
+        </Link>
+      </li>
+    </ul>
+  );
 }
-const mapStateToProps = ({ user }) => ({
-  profile: user.profile,
-});
-export default connect(mapStateToProps, null)(NavbarMobileUserActionsNotSignedIn);
