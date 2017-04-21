5c535e3045815faddd657a4b5a9acd5d8b06323e
TobiahRex <tobiah.rex@gmail.com>
2017-04-21 18:50:36 +0900

    modified startup saga.

--- src/sagas/startup/index.js
+++ src/sagas/startup/index.js
@@ -1,22 +1,20 @@
 /* eslint-disable no-constant-condition */
-import { call, take, put, fork } from 'redux-saga/effects';
+import { call, take } from 'redux-saga/effects';
 import cleanS3RouteSaga from './cleanS3Route';
 import getTaxRate from './getTaxRate';
 import getGeoLocation from './getGeoLocation';
 import mobileDetection from './mobileDetection';
 
 function* startupActions() {
-  const results = yield [
+  yield [
     call(cleanS3RouteSaga),
     call(getTaxRate),
     call(getGeoLocation),
     call(mobileDetection),
   ];
-  console.warn('finished: ', results);
 }
 
 export default function* startup() {
   yield take('APP_STARTUP');
   yield call(startupActions);
-  yield put({ type: 'APP_STARTUP_COMPLETE' });
 }
