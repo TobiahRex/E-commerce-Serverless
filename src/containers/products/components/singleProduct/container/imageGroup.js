9c3c1f8c79873886ea2aaf0d160f6a394b59b18c
TobiahRex <tobiah.rex@gmail.com>
2017-04-25 16:55:02 +0900

    passed down toggleModal() to <ImageGroup /> from <SingleProduct/>.

--- src/containers/products/components/singleProduct/container/imageGroup.js
+++ src/containers/products/components/singleProduct/container/imageGroup.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export default function ImageGroup() {
+const { func } = React.PropTypes;
+
+function ImageGroup({ toggleModal }) {
   return (
     <div className="main__info--image">
       <img className="image__src" alt="Switch Juice" />
-      <button className="image__promotion sweep-right-red">
+      <button
+        className="image__promotion sweep-right-red"
+        onClick={toggleModal}
+      >
         <p>Buy 4 Bottles</p>
         <br />
         <p>Get 25% Off Your Order</p>
@@ -12,3 +17,7 @@ export default function ImageGroup() {
     </div>
   );
 }
+ImageGroup.propTypes = {
+  toggleModal: func.isRequired,
+};
+export default ImageGroup;
