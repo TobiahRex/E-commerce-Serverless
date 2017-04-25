982ddcbd9a8aceab8a0c53ef0494a64135f369cd
TobiahRex <tobiah.rex@gmail.com>
2017-04-25 16:53:46 +0900

    modified <SingleProduct /> into React.COmponent.

--- src/containers/products/components/singleProduct/container/index.js
+++ src/containers/products/components/singleProduct/container/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ImageGroup from './imageGroup';
 import ProductDescription from './productDescription/';
 
-export default function ProductContainer() {
+const { func } = React.PropTypes;
+
+function Container({ toggleModal }) {
   return (
     <div className="main__parent">
-      <ImageGroup />
+      <ImageGroup toggleModal={toggleModal} />
       <ProductDescription />
     </div>
   );
 }
+Container.propTypes = {
+  toggleModal: func.isRequired,
+};
+export default Container;
--- src/containers/products/components/singleProduct/index.js
+++ src/containers/products/components/singleProduct/index.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import BreadCrumb from '../../../../components/breadcrumbs';
 import Title from './title';
 import Container from './container/';
 import ActionBtns from './actionBtns';
 import Modal from './modal';
 
-export default function SingleProduct() {
-  return (
-    <div className="juice-page__main">
-      <BreadCrumb
-        paths={['Home']}
-        classes={['home']}
-        destination={['']}
-        lastCrumb="Juice Page"
-      />
-      <Title />
-      <Container />
-      <ActionBtns />
-      <Modal />
-    </div>
-  );
+class SingleProduct extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="juice-page__main">
+        <BreadCrumb
+          paths={['Home']}
+          classes={['home']}
+          destination={['']}
+          lastCrumb="Juice Page"
+        />
+        <Title />
+        <Container toggleModal={this.toggleModal} />
+        <ActionBtns />
+        <Modal showModal={this.state.showModal} />
+      </div>
+    );
+  }
 }
+const mapDispatchToProps = dispatch => ({
+  saveProductToCart: () => console.log('savingProductToCart', dispatch),
+});
+export default connect(null, mapDispatchToProps)(SingleProduct);
