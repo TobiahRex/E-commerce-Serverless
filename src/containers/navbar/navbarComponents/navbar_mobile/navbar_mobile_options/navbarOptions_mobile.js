3778c541553128b2b124368383e86b8c237c7065
TobiahRex <tobiah.rex@gmail.com>
2017-04-25 11:10:30 +0900

    converted MOBILE > navbarOptions into func. component.

--- src/containers/navbar/navbarComponents/navbar_mobile/navbar_mobile_options/navbarOptions_mobile.js
+++ src/containers/navbar/navbarComponents/navbar_mobile/navbar_mobile_options/navbarOptions_mobile.js
@@ -2,20 +2,10 @@ import React from 'react';
 
 import NavbarMobileOptionsLanguage from './navbar_mobile_language/navbarOptions_mobile_language';
 
-class NavbarMobileOptions extends React.PureComponent {
-  onLanguageChange = () => {
-
-  };
-
-  render() {
-    return (
-      <div className="navbar__mobile--options">
-        <NavbarMobileOptionsLanguage />
-      </div>
-    );
-  }
+export default function NavbarMobileOptions() {
+  return (
+    <div className="navbar__mobile--options">
+      <NavbarMobileOptionsLanguage />
+    </div>
+  );
 }
-// const mapDispatchToProps = (dispatch) => ({
-//   selectLanguage: laguage => dispatch()
-// })
-export default NavbarMobileOptions;
