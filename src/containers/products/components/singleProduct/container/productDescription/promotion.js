7c71ae6ed5d289e74343f7af82cfc777bc9bdeeb
TobiahRex <tobiah.rex@gmail.com>
2017-04-26 12:26:49 +0900

    modified new members button @ <SingleProduct />.

--- src/containers/products/components/singleProduct/container/productDescription/promotion.js
+++ src/containers/products/components/singleProduct/container/productDescription/promotion.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 
-function Promotion() {
+function Promotion({ loggedIn, modalHandler }) {
+  let style;
+  if (!loggedIn) {
+    style = { display: 'flex' };
+  } else {
+    style = { display: 'none' };
+  }
   return (
-    <div className="desc__promotion">
+    <div className="desc__promotion" style={style}>
       <button
+        data-parent="promotion-register"
+        data-tag=""
         className="sweep-right"
-        onClick={() => browserHistory.push('/register')}
+        onClick={modalHandler}
       >New members get 10% off their first order</button>
     </div>
   );
 }
+const { bool, func } = React.PropTypes;
+Promotion.propTypes = {
+  loggedIn: bool.isRequired,
+  modalHandler: func.isRequired,
+};
 const mapStateToProps = ({ auth }) => ({
   loggedIn: auth.loggedIn,
 });
