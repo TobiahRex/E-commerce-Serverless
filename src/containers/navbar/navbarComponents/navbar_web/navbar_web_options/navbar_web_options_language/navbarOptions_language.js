26798c22175076faa5ca259a43a171ed21c31ef7
TobiahRex <tobiah.rex@gmail.com>
2017-04-27 15:56:47 +0900

    moved all dynamic rendering logic from NavbarOptionsLanguageButton into NavbarOptionsLanguage.

--- src/containers/navbar/navbarComponents/navbar_web/navbar_web_options/navbar_web_options_language/navbarOptions_language.js
+++ src/containers/navbar/navbarComponents/navbar_web/navbar_web_options/navbar_web_options_language/navbarOptions_language.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import NavbarOptionsLanguageButton from './navbarOptions_language_mainButton';
+import NavbarOptionsLanguageButtonEnglish from './navbarOptions_language_button_english';
+import NavbarOptionsLanguageButtonNihongo from './navbarOptions_language_button_nihongo';
+
 import NavbarOptionsLanguageDropdnContent from './navbarOptions_language_dropdnContent';
 import NavbarOptionsLanguageDropdnEnglish from './navbarOptions_language_dropdn_english';
 import NavbarOptionsLanguageDropdnNihongo from './navbarOptions_language_dropdn_nihongo';
 
+
 function NavbarOptionsLanguage({ activeLanguage, onLanguageChange }) {
   return (
     <div className="navbar-options-language">
-      <NavbarOptionsLanguageButton activeLanguage={activeLanguage} />
+      <NavbarOptionsLanguageButton activeLanguage={activeLanguage}>
+        {
+          activeLanguage === 'english' ? <NavbarOptionsLanguageButtonEnglish /> : <NavbarOptionsLanguageButtonNihongo />
+        }
+      </NavbarOptionsLanguageButton>
       <NavbarOptionsLanguageDropdnContent>
         {
           activeLanguage === 'english' ? <NavbarOptionsLanguageDropdnNihongo onLanguageChange={onLanguageChange} /> : <NavbarOptionsLanguageDropdnEnglish onLanguageChange={onLanguageChange} />
--- src/containers/navbar/navbarComponents/navbar_web/navbar_web_options/navbar_web_options_language/navbarOptions_language_mainButton.js
+++ src/containers/navbar/navbarComponents/navbar_web/navbar_web_options/navbar_web_options_language/navbarOptions_language_mainButton.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NavbarOptionsLanguageButtonEnglish from './navbarOptions_language_button_english';
-import NavbarOptionsLanguageButtonNihongo from './navbarOptions_language_button_nihongo';
 
-/* TODO
-1. This component receives a State property of the "active_language"  that will change display of the language button details.
-*/
-
-function NavbarOptionsLanguageButton({ activeLanguage }) {
+function NavbarOptionsLanguageButton({ children }) {
   return (
     <span className="language-main-button">
-      {
-        activeLanguage === 'english' ? <NavbarOptionsLanguageButtonEnglish /> : <NavbarOptionsLanguageButtonNihongo />
-      }
+      {children}
     </span>
   );
 }
+const { objectOf, any } = PropTypes;
 NavbarOptionsLanguageButton.propTypes = {
-  activeLanguage: PropTypes.string.isRequired,
+  children: objectOf(any).isRequired,
 };
 export default NavbarOptionsLanguageButton;
