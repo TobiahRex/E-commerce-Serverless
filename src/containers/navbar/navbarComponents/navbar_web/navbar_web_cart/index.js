4cf7028f50b406f8b60692fbd2bbb6d5ce7bb059
TobiahRex <tobiah.rex@gmail.com>
2017-04-27 17:21:04 +0900

    modified <NavbarCartMainButton/> into a functional component.

--- src/containers/navbar/navbarComponents/navbar_web/navbar_web_cart/index.js
+++ src/containers/navbar/navbarComponents/navbar_web/navbar_web_cart/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import NavbarCartDropdnContent from './navbar_web_cart_dropdn/navbarCart_dropdn_content';
 import NavbarCartMainButton from './navbarCart_mainButton';
 
-export default function NavbarCart() {
-  return (
-    <div className="mycart-main">
-      <NavbarCartMainButton qty={this.state.qty} />
-      <NavbarCartDropdnContent />
-    </div>
-  );
+class NavbarCart extends PureComponent {
+  static propTypes = {
+    qty: PropTypes.number.isRequired,
+  }
+  render() {
+    return (
+      <div className="mycart-main">
+        <NavbarCartMainButton qty={this.state.qty} />
+        <NavbarCartDropdnContent />
+      </div>
+    );
+  }
 }
+export default NavbarCart;
--- src/containers/navbar/navbarComponents/navbar_web/navbar_web_cart/navbarCart_mainButton.js
+++ src/containers/navbar/navbarComponents/navbar_web/navbar_web_cart/navbarCart_mainButton.js
@@ -1,28 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-class NavbarCartMainButton extends PureComponent {
-  static propTypes = {
-    qty: PropTypes.number.isRequired,
-  }
-  render() {
-    return (
-      <span className="mainbtn">
-        <Link to="/cart" className="mainbtn-mycart-link">
-          <div className="mainbtn-mycart-link-title">
-            <span>
-              My Cart
-            </span>
-          </div>
-          <div className="mainbtn-mycart-link-cartqty">
-            <span>
-              {this.props.qty}
-            </span>
-          </div>
-        </Link>
-      </span>
-    );
-  }
+function NavbarCartMainButton({ qty }) {
+  return (
+    <span className="mainbtn">
+      <Link to="/cart" className="mainbtn-mycart-link">
+        <div className="mainbtn-mycart-link-title">
+          <span>
+            My Cart
+          </span>
+        </div>
+        <div className="mainbtn-mycart-link-cartqty">
+          <span>
+            {qty}
+          </span>
+        </div>
+      </Link>
+    </span>
+  );
 }
+NavbarCartMainButton.propTypes = {
+  qty: PropTypes.number.isRequired,
+};
 export default NavbarCartMainButton;
