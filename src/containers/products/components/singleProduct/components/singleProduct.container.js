6ed24e397dbbd51b2ec8219837e37006572eb9b2
TobiahRex <tobiah.rex@gmail.com>
2017-04-28 19:15:15 +0900

    completely modified SingleProductContainer.

--- src/containers/products/components/singleProduct/components/singleProduct.container.js
+++ src/containers/products/components/singleProduct/components/singleProduct.container.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
   ImageGroup,
@@ -11,71 +11,111 @@ import {
   SocialMediaBtns,
 } from './imports';
 
-function SingleProductContainer({
-  productObj,
-  loggedIn,
-  modalHandler,
-}) {
-  return (
-    <div className="main__parent">
-      <ImageGroup
-        modalHandler={modalHandler}
-        imageUrl={
-          productObj.images
-          .reduce((accumObj, nextObj) => (
-            Object.prototype.hasOwnProperty.call(accumObj, 'large') ? accumObj.large : nextObj.large))
-        }
-      />
-
-      <div className="main__info--desc">
-        <JuiceTitle />
-        <PriceInfo />
-        <ProductBlurb />
-        <NewMemberPromotionBtn
-          modalHandler={modalHandler}
-          loggedIn={loggedIn}
-        />
-        <NicotineBtns
-          nicStrength={this.state.nicStrength}
-          nicotineHandler={this.nicotineHandler}
-        />
-        <ProductActions
-          quantity={this.state.qty}
-          nicStrength={this.state.nicStrength}
-          error={this.state.error}
-          addToCartHandler={this.addToCartHandler}
-          qtyHandler={this.qtyHandler}
-        />
-        <SocialMediaBtns
-          fbLike={this.fbLike}
-          location={`${process.env.BASE_URL}/juice/fruity_bamm_bamm`}
-        />
-      </div>
-    </div>
-  );
-}
 const {
-  func,
-  bool,
-  objectOf,
   any,
-  string,
-  arrayOf,
+  bool,
+  func,
   shape,
+  string,
+  number,
+  arrayOf,
+  objectOf,
 } = PropTypes;
 
-SingleProductContainer.propTypes = {
-  loggedIn: bool.isRequired,
-  modalHandler: func.isRequired,
-  productObj: objectOf(any).isRequired,
-  juiceObj: shape({
-    id: string,
-    title: string,
-    nicotine_strengths: arrayOf(string),
-    imageUrl: string,
-    routeTag: string,
-  }).isRequired,
-  addToMemberCart: func.isRequired,
-  addToGuestCart: func.isRequired,
-};
+class SingleProductContainer extends PureComponent {
+  static propTypes = {
+    qty: number.isRequired,
+    qtyHandler: func.isRequired,
+    nicStrength: number.isRequired,
+    nicotineHandler: func.isRequired,
+    addToCartHandler: func.isRequired,
+    fbLike: func.isRequired,
+    loggedIn: bool.isRequired,
+    modalHandler: func.isRequired,
+    productObj: objectOf(any).isRequired,
+    juiceObj: shape({
+      id: string,
+      title: string,
+      nicotine_strengths: arrayOf(string),
+      imageUrl: string,
+      routeTag: string,
+    }).isRequired,
+    addToMemberCart: func.isRequired,
+    addToGuestCart: func.isRequired,
+    error: bool,
+  }
+  static defaultProps = {
+    error: false,
+  }
+  render() {
+    const {
+      qty,
+      fbLike,
+      loggedIn,
+      productObj,
+      qtyHandler,
+      nicStrength,
+      modalHandler,
+      nicotineHandler,
+      addToCartHandler,
+      error,
+    } = this.props;
+    const {
+      description,
+      price,
+      sku,
+      id,
+      title,
+      images,
+    } = productObj;
+    return (
+      <div className="main__parent">
+        <ImageGroup
+          modalHandler={modalHandler}
+          imageUrl={images
+            .reduce((accumObj, nextObj) => (
+              Object.prototype.hasOwnProperty.call(accumObj, 'large') ? accumObj.large : nextObj.large))
+          }
+        />
+
+        <div className="main__info--desc">
+          <JuiceTitle
+            title={title}
+          />
+          <PriceInfo
+            price={price}
+            id={id}
+            sku={sku}
+          />
+          <ProductBlurb
+            description={description}
+          />
+
+          <NewMemberPromotionBtn
+            modalHandler={modalHandler}
+            loggedIn={loggedIn}
+          />
+
+          <NicotineBtns
+            nicStrength={nicStrength}
+            nicotineHandler={nicotineHandler}
+          />
+
+          <ProductActions
+            quantity={qty}
+            nicStrength={nicStrength}
+            error={error}
+            addToCartHandler={addToCartHandler}
+            qtyHandler={qtyHandler}
+          />
+
+          <SocialMediaBtns
+            fbLike={fbLike}
+            location={`${process.env.BASE_URL}/juice/${title}?id=${id}`}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 export default SingleProductContainer;
