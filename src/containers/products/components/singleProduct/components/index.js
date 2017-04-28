13941c74a072f377fae83fbc9caa313fe32fa732
TobiahRex <tobiah.rex@gmail.com>
2017-04-28 17:46:32 +0900

    added componentWillReceiveProps * shouldComponentUpdate to <SingleProduct/>.

--- src/containers/products/components/singleProduct/components/index.js
+++ src/containers/products/components/singleProduct/components/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import _ from 'lodash';
 import BreadCrumb from '../../../../../components/breadcrumbs';
 import productActions from '../../../../../redux/products/';
 import {
@@ -51,6 +52,17 @@ class SingleProduct extends Component {
   componentDidMount() {
     const { fetchProductById, productId } = this.props;
     fetchProductById(productId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!_.isEqual(nextProps, this.props)) {
+      this.setState({ ...nextProps });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (!_.isEqual(nextState, this.state)) return true;
+    return false;
   }
 
   modalHandler = (e) => {
