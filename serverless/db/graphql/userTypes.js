8cd0f9afc64bfecf3ea860a5fea686a5a6d6b28e
TobiahRex <tobiah.rex@gmail.com>
2017-05-04 21:11:12 +0900

    building: serverless/db/graphql/userTypes > authentication object.

--- serverless/db/graphql/userTypes.js
+++ serverless/db/graphql/userTypes.js
@@ -5,13 +5,23 @@ import {
 } from 'graphql';
 
 
-export const createUserType = {
+export const createUserInput = {
   name: new InputObject({
     name: 'Users name object.',
     fields: () => ({
       first: { type: NonNull(StringType) },
       last: { type: NonNull(StringType) },
       display: { type: NonNull(StringType) },
+    }),
+  }),
+  authentication: new InputObject({
+    name: 'Authentication information for user.',
+    fields: () => ({
+      lastLogin: { type: NonNull(StringType) },
+      signedUp: { type: NonNull(StringType) },
+      registered: { type: NonNull(StringType) },
+      password: { type: StringType },
+      avatar: 
     }),
   }),
 };
