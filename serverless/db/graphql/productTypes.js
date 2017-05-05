740508467024add0b5b630d797b46420be415b03
TobiahRex <tobiah.rex@gmail.com>
2017-05-05 19:26:14 +0900

    correcting userTypes object.

--- serverless/db/graphql/productTypes.js
+++ serverless/db/graphql/productTypes.js
@@ -2,38 +2,45 @@ import {
   GraphQLList as ListType,
   GraphQLString as StringType,
   GraphQLNonNull as NonNull,
+  GraphQLObjectType as ObjectType,
   GraphQLInputObjectType as InputObject,
 } from 'graphql';
 
 const ProductTypes = {
   // rootType:
-  popularProducts: {
-    title: {
-      type: new NonNull(StringType),
-      description: 'Name of the product.',
-    },
-    flavor: {
-      type: new NonNull(StringType),
-      description: 'The flavor for the product (if applicable).',
-    },
-    price: {
-      type: new NonNull(StringType),
-      description: 'The price of the product.',
-    },
-    routeTag: {
-      type: new NonNull(StringType),
-      description: 'The name of the unique route for the product',
-    },
-    images: new NonNull(new ListType(
-      new InputObject({
-        name: 'PopularProductImageObject',
-        description: 'Images for the product.',
-        fields: () => ({
-          purpose: { type: StringType },
-          url: { type: StringType },
-        }),
-      }),
-    )),
-  },
+  popularProducts: new ObjectType({
+    name: 'PopularProductsType',
+    description: 'The (qty) most popular products.',
+    fields: () => ({
+      title: {
+        type: new NonNull(StringType),
+        description: 'Name of the product.',
+      },
+      flavor: {
+        type: new NonNull(StringType),
+        description: 'The flavor for the product (if applicable).',
+      },
+      price: {
+        type: new NonNull(StringType),
+        description: 'The price of the product.',
+      },
+      routeTag: {
+        type: new NonNull(StringType),
+        description: 'The name of the unique route for the product',
+      },
+      images: new NonNull(
+        new ListType(
+          new InputObject({
+            name: 'PopularProductImageObject',
+            description: 'Images for the product.',
+            fields: () => ({
+              purpose: { type: StringType },
+              url: { type: StringType },
+            }),
+          }),
+        ),
+      ),
+    }),
+  }),
 };
 export default ProductTypes;
