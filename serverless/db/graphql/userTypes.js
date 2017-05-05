e0e9009318b39285892f6cad0fef49ff211b3e2c
TobiahRex <tobiah.rex@gmail.com>
2017-05-05 19:35:53 +0900

    modified usage of NonNull (incorrect) for UserTypes.createUser.

--- serverless/db/graphql/userTypes.js
+++ serverless/db/graphql/userTypes.js
@@ -60,58 +60,70 @@ const UserTypes = {
     }),
   },
   createUser: {
-    name: new InputObject({
-      name: 'UserInputNameObject',
-      description: 'Object: Users name.',
-      fields: () => ({
-        first: { type: NonNull(StringType) },
-        last: { type: NonNull(StringType) },
-        display: { type: NonNull(StringType) },
-      }),
-    }),
-    authentication: new InputObject({
-      name: 'UserInputAuthenticationObject',
-      description: 'Object: Auth info for user.',
-      fields: () => ({
-        lastLogin: { type: NonNull(StringType) },
-        signedUp: { type: NonNull(StringType) },
-        registered: { type: NonNull(StringType) },
-        password: { type: StringType },
-        avatar: { type: StringType },
-      }),
-    }),
-    contactInfo: new InputObject({
-      name: 'UserInputContactInfoObject',
-      description: 'Object: Contanct info for user.',
-      email: { type: StringType },
-      phone: { type: StringType },
-      location: new InputObject({
-        name: 'UserInputGeolocationObject',
-        description: 'Object: Geolocation information for user.',
+    name: new NonNull(
+      new InputObject({
+        name: 'UserInputNameObject',
+        description: 'Object: Users name.',
         fields: () => ({
-          ipAddress: { type: StringType },
-          lat: { type: StringType },
-          long: { type: StringType },
-          country: { type: StringType },
+          first: { type: StringType },
+          last: { type: StringType },
+          display: { type: StringType },
         }),
       }),
-    }),
-    permissions: new InputObject({
-      name: 'UserInputPermissionsObject',
-      description: 'Permissions granted for user.',
-      fields: () => ({
-        role: { type: StringType },
+    ),
+    authentication: new NonNull(
+      new InputObject({
+        name: 'UserInputAuthenticationObject',
+        description: 'Object: Auth info for user.',
+        fields: () => ({
+          lastLogin: { type: StringType },
+          signedUp: { type: StringType },
+          registered: { type: StringType },
+          password: { type: StringType },
+          avatar: { type: StringType },
+        }),
       }),
-    }),
-    userStory: new InputObject({
-      name: 'UserInputStoryObject',
-      description: 'Bio information for user.',
-      fields: () => ({
-        age: { type: IntType },
-        birthday: { type: StringType },
-        bio: { type: StringType },
+    ),
+    contactInfo: new NonNull(
+      new InputObject({
+        name: 'UserInputContactInfoObject',
+        description: 'Object: Contanct info for user.',
+        email: { type: StringType },
+        phone: { type: StringType },
+        location: new NonNull(
+          new InputObject({
+            name: 'UserInputGeolocationObject',
+            description: 'Object: Geolocation information for user.',
+            fields: () => ({
+              ipAddress: { type: StringType },
+              lat: { type: StringType },
+              long: { type: StringType },
+              country: { type: StringType },
+            }),
+          }),
+        ),
       }),
-    }),
+    ),
+    permissions: new NonNull(
+      new InputObject({
+        name: 'UserInputPermissionsObject',
+        description: 'Object: Permissions granted for user.',
+        fields: () => ({
+          role: { type: StringType },
+        }),
+      }),
+    ),
+    userStory: new NonNull(
+      new InputObject({
+        name: 'UserInputStoryObject',
+        description: 'Object: Bio information for user.',
+        fields: () => ({
+          age: { type: IntType },
+          birthday: { type: StringType },
+          bio: { type: StringType },
+        }),
+      }),
+    ),
   },
 };
 export default UserTypes;
