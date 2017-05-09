cc0b116fa917176bf9d1a2a553a65f1328e0f966
TobiahRex <tobiah.rex@gmail.com>
2017-05-09 20:43:42 +0900

    created mongo connect promise.

--- serverless/handler.js
+++ serverless/handler.js
@@ -2,13 +2,27 @@
 import mongoose from 'mongoose';
 import runGraphQL from './db/graphql/runGraphQL';
 
-module.exports.graphql = (event, context, cb) => {
-  require('dotenv').load({ silent: true });
-  const MONGO_DB = process.env.AWS_MONGO_URI_DEV;
-  mongoose.connect(MONGO_DB, () => process.stdout.write(`Connected to MONGO_DB: ${MONGO_DB}`));
+require('dotenv').load({ silent: true });
 
-  runGraphQL(event, (err, res) => {
-    if (err) return cb({ err });
-    return cb(null, res);
+const MONGO_DB = process.env.AWS_MONGO_URI_DEV;
+const connectToMongo = () => new Promise((res, rej) => {
+  mongoose.connect(MONGO_DB, (err) => {
+    if (!err) {
+      res(`Connected to MONGO_DB: ${MONGO_DB}`);
+    } else {
+      rej(err);
+    }
   });
+});
+
+module.exports.graphql = (event, context, cb) => {
+  connectToMongo()
+  .then((successMsg) => {
+    console.log(successMsg);
+    runGraphQL(event, (err, res) => {
+      if (err) return cb({ err });
+      return cb(null, res);
+    });
+  })
+  .catch(cb);
 };
