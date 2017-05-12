4f2e11ce17a3bfc507bdef5f352a5627f6a4b99e
TobiahRex <tobiah.rex@gmail.com>
2017-05-12 21:31:03 +0900

    finished building: graphQL Mutation > NewProduct > dates.

--- serverless/db/graphql/types/productTypes.js
+++ serverless/db/graphql/types/productTypes.js
@@ -170,6 +170,23 @@ const ProductTypes = {
                     }),
                   ),
                 },
+                quantities: {
+                  name: 'NewProductStockQuantities',
+                  description: 'Availability stats for this product.',
+                  type: new InputObject({
+                    name: 'NewProductQuantityInfo',
+                    fields: {
+                      available: {
+                        description: 'The available quanitty for this product.',
+                        type: IntType,
+                      },
+                      in_cart: {
+                        description: 'The quantity for products currently in customers\' carts.',
+                        type: IntType,
+                      },
+                    },
+                  }),
+                },
               }),
             }),
           ),
