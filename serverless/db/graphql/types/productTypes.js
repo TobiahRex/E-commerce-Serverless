51e68319b9c148aad0b0895b25dd9517717be4f4
TobiahRex <tobiah.rex@gmail.com>
2017-05-12 20:34:29 +0900

    building: graphQL Mutation > NewProduct > nicotineStrenghts.

--- serverless/db/graphql/types/productTypes.js
+++ serverless/db/graphql/types/productTypes.js
@@ -87,6 +87,38 @@ const ProductTypes = {
                           value: 2,
                           description: '2mg of Nicotine.',
                         },
+                        4: {
+                          value: 4,
+                          description: '4mg of Nicotine.',
+                        },
+                        6: {
+                          value: 6,
+                          description: '6mg of Nicotine',
+                        },
+                        8: {
+                          value: 8,
+                          description: '8mg of Nicotine.',
+                        },
+                        10: {
+                          value: 10,
+                          description: '8mg of Nicotine.',
+                        },
+                        12: {
+                          value: 12,
+                          description: '8mg of Nicotine.',
+                        },
+                        14: {
+                          value: 14,
+                          description: '8mg of Nicotine.',
+                        },
+                        16: {
+                          value: 16,
+                          description: '8mg of Nicotine.',
+                        },
+                        18: {
+                          value: 18,
+                          description: '8mg of Nicotine.',
+                        },
                       },
                     }),
                   ),
