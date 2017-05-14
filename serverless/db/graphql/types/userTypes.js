86b20eab88a177b766cedf067489203cbefa0f58
TobiahRex <tobiah.rex@gmail.com>
2017-05-14 15:34:16 +0900

    USER: adding descriptings for fields. Removing unnec. "names" for other fields > Root Types

--- serverless/db/graphql/types/userTypes.js
+++ serverless/db/graphql/types/userTypes.js
@@ -17,26 +17,50 @@ const UserTypes = {
         type: new NonNull(MongoID),
       },
       name: {
+        description: 'Object: The First & Last name for the User.',
         type: new ObjectType({
           name: 'UserNameObject',
-          description: 'Users name object.',
-          fields: {
-            first: { type: StringType },
-            last: { type: StringType },
-            display: { type: StringType },
-          },
+          fields: () => ({
+            first: {
+              description: 'The first name of the user.',
+              type: StringType,
+            },
+            last: {
+              description: 'The last name of the user.',
+              type: StringType,
+            },
+            display: {
+              description: 'The display name of the user.',
+              type: StringType,
+            },
+          }),
         }),
       },
       authentication: {
+        description: 'Authentication information for user.',
         type: new ObjectType({
           name: 'UserAuthenticationObject',
-          description: 'Authentication information for user.',
           fields: () => ({
-            lastLogin: { type: StringType },
-            signedUp: { type: StringType },
-            registered: { type: StringType },
-            password: { type: StringType },
-            avatar: { type: StringType },
+            lastLogin: {
+              description: 'The last time this user logged in.',
+              type: StringType,
+            },
+            signedUp: {
+              description: 'The date this user first signed up for newsletters - Typically coincides with users first purchase.',
+              type: StringType,
+            },
+            registered: {
+              description: 'The date this user first became a member.',
+              type: StringType,
+            },
+            password: {
+              description: 'This user\'s password if using email signup.',
+              type: StringType,
+            },
+            avatar: {
+              description: 'This user\'s profile avatar.',
+              type: StringType,
+            },
           }),
         }),
       },
