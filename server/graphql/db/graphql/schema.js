093f7b842d6ca4b7d99930ed5090661d62fa3603
TobiahRex <tobiah.rex@gmail.com>
2017-05-15 14:50:49 +0900

    updated description on RootMutationType.

--- server/graphql/db/graphql/schema.js
+++ server/graphql/db/graphql/schema.js
@@ -2,15 +2,12 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
 } from 'graphql';
-
 import ProductTypes from './types/productTypes';
 import UserTypes from './types/userTypes';
-import ProductModel from '../mongo/models/product';
-import UserModel from '../mongo/models/user';
 
 const query = new GraphQLObjectType({
   name: 'RootQueryType',
-  description: 'The primary query object type.',
+  description: 'The primary query object.',
   fields: () => ({
     findProductById: ProductTypes.queries.findProductById,
     popularProducts: ProductTypes.queries.popularProducts,
@@ -19,13 +16,9 @@ const query = new GraphQLObjectType({
 
 const mutation = new GraphQLObjectType({
   name: 'RootMutationType',
+  description: 'The primary mutation object.',
   fields: () => ({
-    createUser: {
-      type: UserTypes.rootType,
-      description: 'Create new user.',
-      args: UserTypes.mutations.createUser.args,
-      resolve: (_, args) => UserModel.createUser(args),
-    },
+    createUser: UserTypes.mutations.createUser,
     createProduct: ProductTypes.mutations.createProduct,
   }),
 });
