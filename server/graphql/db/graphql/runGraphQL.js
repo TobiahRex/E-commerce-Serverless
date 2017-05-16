897fc9bfe7c9edb8f4f8f1498123ff6bbcd643f3
TobiahRex <tobiah.rex@gmail.com>
2017-05-16 19:59:31 +0900

    added console.logs to all Product model methods for Cloud Watch.

--- server/graphql/db/graphql/runGraphQL.js
+++ server/graphql/db/graphql/runGraphQL.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-console */
 import { graphql } from 'graphql';
 import schema from './schema';
 
 const runGraphQL = (event, cb) => {
   const { query, variables } = event.body;
   graphql(schema, query, null, {}, variables)
-  .then(result => cb(null, result))
-  .catch(error => cb(error));
+  .then((result) => {
+    console.log('\n//runGraphQL.js @ RESOLVE');
+    cb(null, result);
+  })
+  .catch((error) => {
+    console.log('\n//runGraphQL.js @ REJECT');
+    cb(error);
+  });
 };
 export default runGraphQL;
--- serverless/handler.js
+++ serverless/handler.js
@@ -1,19 +1,19 @@
-/* eslint-disable global-require, import/first, no-unused-expressions */
+/* eslint-disable global-require, import/first, no-unused-expressions, no-console */
 if (!global._babelPolyfill) require('babel-polyfill');
 
 import runGraphQL from './db/graphql/runGraphQL';
 import { closeDB } from './db/mongo/connection';
 
 module.exports.graphql = (event, context, cb) => {
-  console.log('EVENT: ', event);
+  console.log('\nEVENT: ', event);
   runGraphQL(event, (error, response) =>
     closeDB(() => {
       if (error) {
-        console.log('\nERROR:', error);
+        console.log('\n//handler.js @ if(error):\nERROR: ', error);
         context.error && context.error(error);
         cb(null, error);
       }
-      console.log('\nRESULT: ', response);
+      console.log('\n//handler.js @ if(error) else:\nRESULT: ', response);
       context.succeed && context.succeed(response);
       cb(null, response);
     }),
