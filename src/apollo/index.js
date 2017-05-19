ff01df2d9f0c40e5d31baaa9eac472d0ada0dd19
TobiahRex <tobiah.rex@gmail.com>
2017-05-19 15:29:17 +0900

    fixed src/apollo/index.js env vars.

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -5,10 +5,11 @@ import {
 
 const {
   NODE_ENV,
-  AWS_GRAPHQL_PROD,
+  GRAPHQL_PORT,
+  LAMBDA_GRAPHQL,
 } = process.env;
 
-const uri = NODE_ENV === 'production' ? AWS_GRAPHQL_PROD : 'http://localhost:3001/graphql';
+const uri = NODE_ENV === 'production' ? LAMBDA_GRAPHQL : `http://localhost:${GRAPHQL_PORT}/graphql`;
 
 if (!uri) throw new Error('Apollo Client network interface is not defined.');
 console.info('Apollo Netowrk URI = ', uri); //eslint-disable-line
