03f5e363eeb9d17ca9a26806c63ab3c1d280e9b4
TobiahRex <tobiah.rex@gmail.com>
2017-05-26 23:39:23 +0900

    made NavbarMobile a Container component.

--- src/containers/navbar/components/navbar_mobile/navbar_mobile.js
+++ src/containers/navbar/components/navbar_mobile/navbar_mobile.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import NavbarLogo from './navbarLogo';
 import NavbarMobileOptionsLanguage from './navbar_mobile_options/navbar_mobile_language/navbarOptions_mobile_language';
 import NavbarMobileUserActions from './navbar_mobile_userActions/navbar_mobile_userActions';
 import NavbarMobileNav from './navbar_mobile_nav/navbar_mobile_nav';
 
-export default function NavbarMobile() {
+const { objectOf, any } = PropTypes;
+
+class NavbarMobile extends React.PureComponent {
   static propTypes = {
     activeUser: objectOf(any),
   }
   static defaultProps = {
     activeUser: false,
   }
-  return (
-    <nav className="navbar navbar__mobile">
-      <div className="navbar__mobile--container">
-        <NavbarLogo />
-        <NavbarMobileOptionsLanguage />
-        <NavbarMobileUserActions />
-        <NavbarMobileNav />
-      </div>
-    </nav>
-  );
+  render() {
+    return (
+      <nav className="navbar navbar__mobile">
+        <div className="navbar__mobile--container">
+          <NavbarLogo />
+          <NavbarMobileOptionsLanguage />
+          <NavbarMobileUserActions />
+          <NavbarMobileNav />
+        </div>
+      </nav>
+    );
+  }
 }
 const mapStateToProps = ({ user }) => ({
   activeUser: user.profile,
 });
-export default connect(mapStateToProps)(NavbarMobileUserActions);
+export default connect(mapStateToProps)(NavbarMobile);
