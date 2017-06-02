2fb4b57b99f0fdd5b2afab7ab1216d36036bba4a
TobiahRex <tobiah.rex@gmail.com>
2017-06-02 19:36:44 +0900

    created errorLogging middleware/afterWare for apollo client.

--- src/graphQL/errorLogging.js
+++ src/graphQL/errorLogging.js
@@ -1,66 +1,74 @@
+/* eslint-disable no-console, no-alert */
 import consoleGroup from 'console-group';
 
 consoleGroup.install();
 
-var processedRequests = 0
-  const requests = {}
-  const requestIdHeader = 'x-req-id'
+let processedRequests = 0;
+const requests = {};
+const requestIdHeader = 'x-req-id';
 
-  const storeQueriesMiddleware = {
-    applyMiddleware({ request, options }, next) {
-      const requestId = `r${(processedRequests++)}-${request.operationName}`
-      if (!options.headers)  options.headers = {};
-      options.headers[requestIdHeader] = requestId
-      requests[requestId] = request
-      next()
-    },
-  };
-  const processGqlResponseErrors = (request, requestId) => {
-    const params = []
+export const storeQueriesMiddleware = {
+  applyMiddleware({ request, options }, next) {
+    const requestId = `r${(processedRequests += 1)}-${request.operationName}`;
+    if (!options.headers) options.headers = {};
+    options.headers[requestIdHeader] = requestId;
+    requests[requestId] = request;
+    next();
+  },
+};
+const processGqlResponseErrors = (request, requestId) => {
+  const params = [];
 
-    console.group(`processGqlResponseErrors(${requestId})`)
+  console.group(`processGqlResponseErrors(${requestId})`);
+  let value = '';
 
-    let value = 'Pood my pants again'
-    Object.keys(request).forEach((key) => {
-      if (key == 'query') {
-        value = print(request[key])
-      }
-      else {
-        const jsonResponseElement = JSON.stringify(request[key])
-        value = (jsonResponseElement.length > 300) ? jsonResponseElement.substr(0, 300) + ' .... ' : jsonResponseElement
-      }
-      console.log(key);
-      console.log(value)
-      params.push(`${key}=${encodeURIComponent(value)}`)
-    })
-    console.groupEnd(`processGqlResponseErrors(${requestId})`)
-    if (typeof confirm == "function") {
-      if (confirm(`Open failed GQL request: ${request.operationName} query in GraphiQL?`)) window.open(`${window.location.origin}/graphiql?${params.join('&')}`);
+  Object.keys(request).forEach((key) => {
+    if (key === 'query') {
+      value = print(request[key]);
+    } else {
+      const jsonResponseElement = JSON.stringify(request[key], null, 2);
+
+      value = (jsonResponseElement.length > 300) ? `${jsonResponseElement.substr(0, 300)} ... ` : jsonResponseElement;
     }
-  }
-  const processGQLErrors = {
-    applyAfterware({ response, options }, next) {
-      const requestId = options.headers[requestIdHeader]
-      const request = requests[requestId]
-      delete requests[requestId]
-      if (!response.ok) {
-        response.clone().text().then(bodyText => {
-          const theBodyText = (bodyText.length > 500) ? bodyText.substr(0, 500) + ' .... ' : bodyText
-          console.error(`Network Error: ${response.status} (${response.statusText}) - ${theBodyText}`);
-          processGqlResponseErrors(request, requestId)
-          next();
-        });
-      } else {
-        response.clone().json().then(({ errors }) => {
-          if (errors) {
-            console.error('GraphQL Errors:', errors.map(e => e.message));
-            processGqlResponseErrors(request, requestId)
-          }
-          next();
-        });
-      }
-    },
-  };
+    console.log('key: ', key);
+    console.log('value: ', value);
+    params.push(`${key}=${encodeURIComponent(value)}`);
+  });
+  console.groupEnd(`processGqlResponseErrors(${requestId})`);
 
-  networkInterface.use([storeQueriesMiddleware]);
-  networkInterface.useAfter([processGQLErrors])
+  if (typeof confirm === 'function') {
+    if (
+      confirm(`
+        Open failed GQL request: ${request.operationName} query in GraphiQL?
+        `)
+    ) window.open(`${window.location.origin}/graphiql?${params.join('&')}`);
+  }
+};
+
+export const processGQLErrors = {
+  applyAfterware({ response, options }, next) {
+    const requestId = options.headers[requestIdHeader];
+    const request = requests[requestId];
+    delete requests[requestId];
+
+    if (!response.ok) {
+      response
+      .clone()
+      .text()
+      .then((bodyText) => {
+        const theBodyText = (bodyText.length > 500) ? `${bodyText.slice(0, bodyText.length)} ...` : bodyText;
+        console.error(`Network Error: ${response.status} (${response.statusText}) - ${theBodyText}`);
+        processGqlResponseErrors(request, requestId);
+        next();
+      });
+    } else {
+      response.clone().json().then(({ errors }) => {
+        if (errors) {
+          console.error('GraphQL Errors:', errors.map(e => e.message));
+          processGqlResponseErrors(request, requestId);
+        }
+        next();
+      });
+    }
+  },
+};
--- src/graphQL/index.js
+++ src/graphQL/index.js
@@ -3,6 +3,11 @@ import {
   createNetworkInterface,
 } from 'react-apollo';
 
+import {
+  processGQLErrors,
+  storeQueriesMiddleware,
+} from './errorLogging';
+
 const {
   NODE_ENV,
   GRAPHQL_PORT,
@@ -14,9 +19,28 @@ const uri = NODE_ENV === 'production' ? LAMBDA_GRAPHQL : `http://localhost:${GRA
 if (!uri) throw new Error('Apollo Client network interface is not defined.');
 console.info('Apollo Netowrk URI = ', uri); //eslint-disable-line
 
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri,
-  }),
-});
+const networkInterface = createNetworkInterface({ uri });
+networkInterface.use([storeQueriesMiddleware]);
+networkInterface.useAfter([processGQLErrors]);
+const client = new ApolloClient(networkInterface);
+// const client = new ApolloClient({
+//   networkInterface: createNetworkInterface({
+//     uri,
+//   }),
+// });
 export default client;
+//
+// import ApolloClient, { createNetworkInterface } from 'apollo-client';
+// import {logout} from './logout';
+// const networkInterface = createNetworkInterface({ uri: '/graphql' });
+// networkInterface.useAfter([{
+//   applyAfterware({ response }, next) {
+//     if (response.status === 401) {
+//       logout();
+//     }
+//     next();
+//   }
+// }]);
+// const client = new ApolloClient({
+//   networkInterface,
+// });
