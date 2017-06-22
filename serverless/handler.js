4fb7d7c41c3ab2e602011a0e32f908551757f47f
TobiahRex <tobiah.rex@gmail.com>
2017-06-22 18:29:27 +0700

    modified serverless/handler.js formatting.

--- serverless/handler.js
+++ serverless/handler.js
@@ -11,12 +11,12 @@ module.exports.graphql = (event, context, cb) => {
   startDB()
   .then(dbResults => runGraphQL({ event, ...dbResults }))
   .then((GraphQLResponse) => {
-    console.log('\n//handler.js @ \nRESOLVE: ', GraphQLResponse);
+    console.log('\n//Final Lambda SUCCESS Response: ', GraphQLResponse);
     context.succeed && context.succeed(GraphQLResponse);
     cb(null, GraphQLResponse);
   })
   .catch((error) => {
-    console.log('\n//handler.js @ CATCH\nERROR: ', error);
+    console.log('\n//Final Lambda ERROR: ', error);
     context.error && context.error(error);
   });
 };
@@ -24,10 +24,12 @@ module.exports.graphql = (event, context, cb) => {
 module.exports.wakeup = (event, context, callback) => {
   console.log('Calling Wakeup Lambda.');
   const options = {
-    "query":"query{FindProductById(_id: \"Test\") {_id, product { mainTitle title flavor price sku sizes nicotine_strengths routeTag vendor blurb images{ purpose url } dates {added_to_store  removed_from_store }}}}"
-  }
+    query: 'query{FindProductById(_id: \"Test\") {_id, product { mainTitle title flavor price sku sizes nicotine_strengths routeTag vendor blurb images{ purpose url } dates {added_to_store  removed_from_store }}}}',
+  };
 
-  request.post(process.env.WAKE_UP_URL, { body: JSON.stringify(options) },
+  request.post(process.env.WAKE_UP_URL, {
+    body: JSON.stringify(options),
+  },
   (err, res) => {
     if (err) return console.error('waking up failed:', err);
     console.log('woke up the sleeping lambda\nJSON response: \n', res);
