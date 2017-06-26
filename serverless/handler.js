5c5ce2e8ef907084e4ed0243cf07bca7e4279884
TobiahRex <tobiah.rex@gmail.com>
2017-06-26 13:05:29 +0700

    fixed Product.CreateProduct method error.

--- serverless/handler.js
+++ serverless/handler.js
@@ -3,7 +3,6 @@ if (!global._babelPolyfill) require('babel-polyfill');
 
 import runGraphQL from './db/graphql/runGraphQL';
 import { startDB } from './db/mongo/connection';
-import request from 'request';
 
 module.exports.graphql = (event, context, cb) => {
   console.log('\nEVENT: ', event);
@@ -18,21 +17,5 @@ module.exports.graphql = (event, context, cb) => {
   .catch((error) => {
     console.log('\n//Final Lambda ERROR: ', error);
     context.error && context.error(error);
-  });
-};
-
-module.exports.wakeup = (event, context, callback) => {
-  console.log('Calling Wakeup Lambda.');
-  const options = {
-    query: 'query{FindProductById(_id: \"Test\") {_id, product { mainTitle title flavor price sku sizes nicotine_strengths routeTag vendor blurb images{ purpose url } dates {added_to_store  removed_from_store }}}}',
-  };
-
-  request.post(process.env.WAKE_UP_URL, {
-    body: JSON.stringify(options),
-  },
-  (err, res) => {
-    if (err) return console.error('waking up failed:', err);
-    console.log('woke up the sleeping lambda\nJSON response: \n', res);
-    return callback(null, res);
   });
 };
