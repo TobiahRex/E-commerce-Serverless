863f8646bfe2bb9b577cbc88fdca0c85d0c1c5cb
TobiahRex <tobiah.rex@gmail.com>
2017-07-09 12:45:31 +0700

    added func. desc. to user model.

--- serverless/db/mongo/models/user.js
+++ serverless/db/mongo/models/user.js
@@ -4,13 +4,13 @@ import userSchema from '../schemas/userSchema';
 
 export default (db) => {
   /**
- * 1) Query User collection using input argument "userId".
- * 2) Resolves || Rejects with result.
- *
- * @param {string} userId - the user's Mongo _id.
- *
- * @return {object} - Promise resolved with db connection.
-*/
+  * 1) Query User collection using input argument "userId".
+  * 2) Resolves || Rejects with result.
+  *
+  * @param {string} userId - the user's Mongo _id.
+  *
+  * @return {object} - Promise resolved with found User document.
+  */
   userSchema.statics.fetchUserProfile = userId =>
   new Promise((resolve, reject) => {
     User
@@ -26,6 +26,19 @@ export default (db) => {
     });
   });
 
+  /**
+  * Checks for previous user matching the input auth0 id.  If found, logs user in. If not found, registers user.
+  *
+  * 1) Saves "auth0Id" & "loginType" to external variables and removes those from input "args" arguments.  By removing from args input, register user can cleanly create new instance with 1 input, "args".
+  * 2) Queries User Collection with "auth0Id".
+  * 3a) If found - calls "loginUser" with loginType, found user & remaining args.
+  * 3b) If not found - calls "registerUser" with input args.
+  * 4) Resolves || Rejects with final result.
+  *
+  * @param {string} userId - the user's Mongo _id.
+  *
+  * @return {object} - Promise resolved with updated User document.
+  */
   userSchema.statics.loginOrRegister = args =>
   new Promise((resolve, reject) => {
     const auth0Id = args.auth0Id;
@@ -33,7 +46,8 @@ export default (db) => {
     delete args.auth0Id;
     delete args.loginType;
 
-    User.findOne({ 'authentication.auth0Identities.user_id': auth0Id })
+    User
+    .findOne({ 'authentication.auth0Identities.user_id': auth0Id })
     .exec()
     .then((dbUser) => {
       if (!dbUser) return User.registerUser(args);
@@ -43,6 +57,16 @@ export default (db) => {
     .catch(error => reject({ problem: error }));
   });
 
+  /**
+  * 1) Modifies dbUser document values based on new successful login and saves result.
+  * 2) Resolves || Rejects with result.
+  *
+  * @param {string} loginType - the Social Auth provider used to login.
+  * @param {object} dbUser - the Mongo User Document to modify.
+  * @param {object} userObj - the new values to update Mongo User Document.
+  *
+  * @return {object} - Promise resolved with updates User Document.
+  */
   userSchema.statics.loginUser = (loginType, dbUser, userObj) =>
   new Promise((resolve) => {
     console.log('Found Existing User.\n');
@@ -56,6 +80,14 @@ export default (db) => {
     .then(resolve);
   });
 
+  /**
+  * 1) Creates new Mongo User Document.
+  * 2) Resolves || Rejects with result.
+  *
+  * @param {object} userObj - New user info.
+  *
+  * @return {object} - Promise resolved with new User Document.
+  */
   userSchema.statics.registerUser = userObj =>
   new Promise((resolve, reject) => {
     const {
@@ -104,9 +136,21 @@ export default (db) => {
     .catch(reject);
   });
 
+  /**
+  * Updates User Document's shopping cart with product information.
+  * 1) Finds user by Mongo _id.
+  * 2) Populates found document's shopping cart with product info.
+  * 3) Saves changes.
+  * 4) Resolves || Rejects with result.
+  *
+  * @param {object} cartObj - userId {string}, qty {number}, nicotineStrength {number}, product {object}.
+  *
+  * @return {object} - Promise resolved with updated User Document.
+  */
   userSchema.statics.addToMemberCart = ({ userId, qty, nicotineStrength, product }) =>
   new Promise((resolve, reject) => {
-    User.findById(userId)
+    User
+    .findById(userId)
     .exec()
     .then((dbUser) => {
       dbUser.shopping.cart.push({
@@ -120,18 +164,23 @@ export default (db) => {
       console.log('Saved product to the User\'s Shopping Cart!');
       resolve(savedUser);
     })
-    .catch(error => reject({
-      problem: `Could not save to the Users shopping cart.
-      args: {
-        userId: ${userId},
-        qty: ${qty},
-        nicotineStrength: ${nicotineStrength},
-        product: ${product},
-      }
-      Mongo Error: ${error}`,
-    }));
+    .catch((error) => {
+      console.log(`Could not save product to Users shopping cart. ERROR = ${error}`);
+      reject(`Could not save product to Users shopping cart. ERROR = ${error}`);
+    });
   });
 
+  /**
+  * Deletes product from users shopping cart.
+  * 1) Finds user by Mongo _id.
+  * 2) filters users shopping cart by productId.
+  * 3) Saves changes.
+  * 4) Resolves || Rejects with result.
+  *
+  * @param {object} cartObj - userId {string}, productId {string}.
+  *
+  * @return {object} - Promise resolved with updated User Document.
+  */
   userSchema.statics.deleteFromCart = ({ userId, productId }) =>
   new Promise((resolve, reject) => {
     User.findById(userId)
@@ -142,36 +191,43 @@ export default (db) => {
       return dbUser.save({ validateBeforeSave: true });
     })
     .then((savedUser) => {
-      console.log(`
-        Deleted Product: ${productId} from User: ${savedUser._id}.
-      `);
+      console.log(`Deleted Product: ${productId} from User: ${savedUser._id}.`);
       resolve(savedUser);
     })
-    .catch(error => reject(`
-      Could not Delete Product: ${productId} from User: ${userId}.
-      Mongo Error = ${error}
-    `));
+    .catch((error) => {
+      console.log(`Could not Delete Product: ${productId} from User: ${userId}. ERROR = ${error}`);
+      reject(`Could not Delete Product: ${productId} from User: ${userId}. ERROR = ${error}`);
+    });
   });
 
+  /**
+  * Modifies product(s) in user's shopping cart.
+  * 1) Finds user by Mongo _id.
+  * 2) Replaces shopping cart with new products array.
+  * 3) Saves changes.
+  * 4) Resolves || Rejects with result.
+  *
+  * @param {object} cartObj - userId {string}, products {array}.
+  *
+  * @return {object} - Promise resolved with updates User Document.
+  */
   userSchema.statics.editToMemberCart = ({ userId, products }) =>
   new Promise((resolve, reject) => {
-    User.findById(userId)
+    User
+    .findById(userId)
     .exec()
     .then((dbUser) => {
       dbUser.shopping.cart = products;
       return dbUser.save({ validateBeforeSave: true });
     })
     .then((updatedUser) => {
-      console.log(`
-        Updated user shopping cart!
-      `);
+      console.log('Updated user shopping cart!');
       resolve(updatedUser);
     })
-    .catch(error => reject(`
-      Could not Update User: ${userId}.
-
-      Mongo Error = ${error}
-    `));
+    .catch((error) => {
+      console.log(`Could not Update User: ${userId}. ERROR = ${error}`);
+      reject(`Could not Update User: ${userId}. ERROR = ${error}`);
+    });
   });
 
   const User = db.model('User', userSchema);
