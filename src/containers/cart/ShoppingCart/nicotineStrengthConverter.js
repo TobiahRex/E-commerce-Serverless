ec56d1ca65dbcb2358e4c0dffdf26e1273bb78d3
TobiahRex <tobiah.rex@gmail.com>
2017-07-19 15:27:55 +0700

    created a nicotineStrengthConverter.js file.

---
+++ src/containers/cart/ShoppingCart/nicotineStrengthConverter.js
@@ -0,0 +1,13 @@
+export default (strength) => {
+  switch (strength) {
+    case 'two': return '2 mg';
+    case 'four': return '4 mg';
+    case 'six': return '6 mg';
+    case 'eight': return '8 mg';
+    case 'ten': return '10 mg';
+    case 'twelve': return '12 mg';
+    case 'fourteen': return '14 mg';
+    case 'sixteen': return '16 mg';
+    default: return strength;
+  }
+};
