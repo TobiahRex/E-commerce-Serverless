930d8d22759aab905c2097d9a938b17f7089ae34
TobiahRex <tobiah.rex@gmail.com>
2017-07-22 14:38:19 +0700

    finished optimizing oAuth cleaning methods.

--- src/sagas/authorization/cleanTwitter.js
+++ src/sagas/authorization/cleanTwitter.js
@@ -60,11 +60,10 @@ export default ({ orders, user, geo, locale, mobile }, auth0Twitter) => {
     },
   };
 
-  if (orders.guest && orders.guest.length) {
-    orders.guest.forEach(({ id: product, qty, strength }) => {
-      profile.shopping.cart.push({ qty, strength, product });
-    });
+  if (!!orders.cart && orders.cart.length) {
+    profile.shoppingCart = orders.cart.map(({ _id, qty }) => ({ productId: _id, qty }));
   }
+
   return {
     profile,
     loginType: 'twitter',
