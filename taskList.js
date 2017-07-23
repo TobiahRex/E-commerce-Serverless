7ac695c963a62a5439be50512effd8ae77896c6c
TobiahRex <tobiah.rex@gmail.com>
2017-07-23 20:06:45 +0700

    updated taskList.js

--- taskList.js
+++ taskList.js
@@ -4,9 +4,6 @@
 
 
   TODO: Backlog
-  - If the user is logged in, call a GraphQL query for the products in the logged in users cart by the products _id.  The response data should be distributed to Navbar, and Cart.
-
-  - once the user logs in, also clear the guest Cart from local storage and redux-persist.
 
   - Build "Transaction" model & schema.
 
