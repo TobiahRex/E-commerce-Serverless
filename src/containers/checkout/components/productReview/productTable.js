72c79cb0929f4b9dd5d85041b2e62708072d4080
TobiahRex <tobiah.rex@gmail.com>
2017-07-25 18:52:19 +0700

    moved ProductReview > table rows into their own sub-render function.

--- src/containers/checkout/components/productReview/productTable.js
+++ src/containers/checkout/components/productReview/productTable.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 
-class ProductReviewComments extends PureComponent {
+class ProductReviewTable extends PureComponent {
   static propTypes = {
     cart: arrayOf(object).isRequired,
   }
@@ -18,6 +18,38 @@ class ProductReviewComments extends PureComponent {
     this.setState(() => ({ cart: [...nextProps] }));
   }
 
+  renderTableRow = (cart) => {
+    console.log('%ccart', 'background:lime;', cart);
+    return cart.map(({ _id, qty, product }) => {
+      return (
+        <tr className="body__row" key={_id}>
+          <td className="body__row--product-name">
+            <div className="image__container">
+              <img alt="Juice" className="image--source" />
+            </div>
+            <div className="description__container">
+              <p>{product.title}</p>
+              <p>Nicotine Strength: <i>{product.nicotineStrength}</i></p>
+            </div>
+          </td>
+          <td className="body__row--product-qty">
+            <div className="qty--container">
+              <p>3</p>
+            </div>
+          </td>
+          <td className="body__row--product-subtotal">
+            <div className="product-subtotal-container">
+              <p>
+                <FontAwesome name="usd" />{'\u00A0'}
+                {Number(product.price) * qty}
+              </p>
+            </div>
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     const {
       cart,
@@ -27,62 +59,21 @@ class ProductReviewComments extends PureComponent {
       <table className="table__container">
         <thead className="table__header">
           <tr className="header__row">
-            <th className="header__row--product-name">Product Name</th>
+            <th className="header__row--product-name">Product</th>
             <th className="header__row--qty">Qty</th>
             <th className="header__row--subtotal">Subtotal</th>
           </tr>
         </thead>
         <tbody className="table__body">
-          <tr className="body__row">
-            <td className="body__row--product-name">
-              <div className="image__container">
-                <img alt="Juice" className="image--source" />
-              </div>
-              <div className="description__container">
-                <p>Fruity Bamm-Bamm</p>
-                <p>Nicotine Strength: <i>6mg</i></p>
-              </div>
-            </td>
-            <td className="body__row--product-qty">
-              <div className="qty--container">
-                <p>3</p>
-              </div>
-            </td>
-            <td className="body__row--product-subtotal">
-              <div className="product-subtotal-container">
-                <p><FontAwesome name="usd" />{'\u00A0'}90.00</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="body__row">
-            <td className="body__row--product-name">
-              <div className="image__container">
-                <img alt="Juice" className="image--source" />
-              </div>
-              <div className="description__container">
-                <p>French Vanilla Mocha</p>
-                <p>Nicotine Strength: <i>8mg</i></p>
-              </div>
-            </td>
-            <td className="body__row--product-qty">
-              <div className="qty--container">
-                <p>1</p>
-              </div>
-            </td>
-            <td className="body__row--product-subtotal">
-              <div className="product-subtotal-container">
-                <p><FontAwesome name="usd" />{'\u00A0'}30.00</p>
-              </div>
-            </td>
-          </tr>
+          {this.renderTableRow(cart)}
         </tbody>
       </table>
     );
   }
 }
 
-ProductReviewComments.propTypes = {
+ProductReviewTable.propTypes = {
   cart: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default ProductReviewComments;
+export default ProductReviewTable;
