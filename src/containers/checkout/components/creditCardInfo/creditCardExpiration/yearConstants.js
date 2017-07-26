d263420feefb816e187f16d3fc95fe7e6f5ac1bf
TobiahRex <tobiah.rex@gmail.com>
2017-07-26 17:27:35 +0700

    finished building a func. desc. for "calculateYears".

--- src/containers/checkout/components/creditCardInfo/creditCardExpiration/yearConstants.js
+++ src/containers/checkout/components/creditCardInfo/creditCardExpiration/yearConstants.js
@@ -1,5 +1,14 @@
-/* Function: "calculateYears"
+/** Function: "calculateYears"
 * 1) Establish today's date using Date constructore.
+* 2) Get year from step 1 result.
+* 3) Create a for loop, and generate a dynamic, incrementing year value.
+* 4) Cast that Number to a String so it can be used as a react "key" value later with the Buffer constructor. (First argument to a Buffer when specifying encoding, must be a string).
+* 5) Push result into "years" array.
+* 6) Return years array.
+*
+* @param {none}
+*
+* @return {array} years - An array with 4 digit years {string}.
 */
 
 function calculateYears() {
