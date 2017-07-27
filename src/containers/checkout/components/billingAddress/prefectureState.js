55d636c3fba9cde38ca7fcf02259a04b3a89730d
TobiahRex <tobiah.rex@gmail.com>
2017-07-27 11:49:16 +0700

    building a dynamic dropdown for Prefecture / State @ Billing Address based on Country input.

--- src/containers/checkout/components/billingAddress/prefectureState.js
+++ src/containers/checkout/components/billingAddress/prefectureState.js
@@ -1,7 +1,12 @@
 import React, { PureComponent } from 'react';
-import PrefectureAndStates from './prefectureAndStates';
+import PropTypes from 'prop-types';
+import Prefectures from './prefectureConstants';
+import States from './stateConstants';
 
 class PrefectureState extends PureComponent {
+  static propTypes = {
+    country: PropTypes.string.isRequired,
+  }
   constructor(props) {
     super(props);
 
@@ -11,6 +16,17 @@ class PrefectureState extends PureComponent {
   }
 
   handleChange = e => this.setState({ prefectureState: e.target.value })
+
+  renderOptions = (country, prefectures, states) => {
+    switch (country) {
+      case 'Japan (JA)': return prefectures.map(({ en, kanji }) => (
+        <div></div>
+      ));
+      case 'United States (US)': return prefectures.map(({ name, code }) => (
+        <div></div>
+      ));
+    } default; break;
+  }
 
   render() {
     return (
