381d512fe26329a5ced51fffeecb876ee4be5396
TobiahRex <tobiah.rex@gmail.com>
2017-07-29 16:39:36 +0700

    created "checkNewUser" @ services/utils/determineNewUser.js

--- src/containers/checkout/utilities.imports.js
+++ src/containers/checkout/utilities.imports.js
@@ -1,3 +1,4 @@
 export zipUserCart from '../../services/utils/zipUserCart';
 export determineCartType from '../../services/utils/determineCartType';
 export nicotineStrengthConverter from '../../services/utils/nicotineStrengthConverter';
+export determineNewUser from '../../services/utils/determineNewUser';
---
+++ src/services/utils/determineNewUser.js
@@ -0,0 +1,15 @@
+/**
+* Function: "checkNewUser"
+* 1) First checks if the user is even logged in.  If they are not, then the next step will throw an error if allowed to continue.
+* 2) Navigates through the user profile to their shopping cart, and checks to see if they've ever made a purchase.  If they have, then we know they are not a new user.  If they haven't then we know that they are allowed to tuse the 10% new user discount.
+*
+* @param {object} user - the users logged in profile.
+* @param {bool} loggedIn - Whether or not the user is logged in.
+*
+* @return {bool} - Final answer.
+*/
+
+export default (user, loggedIn) => {
+  if (!loggedIn) return false;
+  return !user.profile.shopping.cart.length;
+};
