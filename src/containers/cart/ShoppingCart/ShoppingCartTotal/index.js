d675d29b339e9c535c67c738f05c0ccc91e8867c
TobiahRex <tobiah.rex@gmail.com>
2017-07-29 21:50:39 +0700

    corrected issue in prop validation for async ShoppingCartTotal.

--- src/containers/cart/ShoppingCart/ShoppingCartTotal/index.js
+++ src/containers/cart/ShoppingCart/ShoppingCartTotal/index.js
@@ -44,34 +44,34 @@ function ShoppingCartTotal({ total }) {
       </div>
       {
         discount.qty &&
-          <div className="shopping-cart-analysis-qty-discount">
-            <div className="shopping-cart-analysis-qty-discount-title">
-              <h4 className="title required" style={{ textAlign: 'left' }}>Quantity Discount</h4>
-            </div>
-            <div className="shopping-cart-analysis-qty-discount-cost required">
-              <h4 style={{ color: '#FC2525' }}> 25%{'\u00A0'}</h4>
-              <FontAwesome name="usd" style={{ color: '#FC2525' }} />
-              <h4 style={{ color: '#FC2525' }}>
-                {'\u00A0'}-{discount.qtyAmount .toFixed(2)}
-              </h4>
-            </div>
+        <div className="shopping-cart-analysis-qty-discount">
+          <div className="shopping-cart-analysis-qty-discount-title">
+            <h4 className="title required" style={{ textAlign: 'left' }}>Quantity Discount</h4>
           </div>
+          <div className="shopping-cart-analysis-qty-discount-cost required">
+            <h4 style={{ color: '#FC2525' }}> 25%{'\u00A0'}</h4>
+            <FontAwesome name="usd" style={{ color: '#FC2525' }} />
+            <h4 style={{ color: '#FC2525' }}>
+              {'\u00A0'}-{discount.qtyAmount .toFixed(2)}
+            </h4>
+          </div>
+        </div>
       }
 
       {
         discount.register &&
-          <div className="shopping-cart-analysis-register-discount">
-            <div className="shopping-cart-analysis-register-discount-title">
-              <h4 className="title required">Register Discount</h4>
-            </div>
-            <div className="shopping-cart-analysis-register-discount-cost required">
-              <h4 style={{ color: '#FC2525' }}> 10%{'\u00A0'}</h4>
-              <FontAwesome name="usd" style={{ color: '#FC2525' }} />
-              <h4 style={{ color: '#FC2525' }}>
-                {'\u00A0'}-{discount.registerAmount.toFixed(2)}
-              </h4>
-            </div>
+        <div className="shopping-cart-analysis-register-discount">
+          <div className="shopping-cart-analysis-register-discount-title">
+            <h4 className="title required">Register Discount</h4>
           </div>
+          <div className="shopping-cart-analysis-register-discount-cost required">
+            <h4 style={{ color: '#FC2525' }}> 10%{'\u00A0'}</h4>
+            <FontAwesome name="usd" style={{ color: '#FC2525' }} />
+            <h4 style={{ color: '#FC2525' }}>
+              {'\u00A0'}-{discount.registerAmount.toFixed(2)}
+            </h4>
+          </div>
+        </div>
 
       }
 
@@ -90,48 +90,48 @@ function ShoppingCartTotal({ total }) {
 {/* TODO: Change the style of the Cart Total to this element instead.
   <div className="checkout__grand-total">
   <div className="title">
-    <h3>Total</h3>
-  </div>
+  <h3>Total</h3>
+</div>
 
-  <div className="analysis-container">
-    <div className="analysis-container--subtotal">
-      <p>Subtotal</p>
-      <p><FontAwesome name="usd" />{'\u00A0'}90.00</p>
-    </div>
-    <div className="analysis-container--shipping">
-      <p>Shipping & Handling</p>
-      <p><i>Free</i></p>
-    </div>
-    <div className="analysis-container--discount">
-      <p>New Member Discount</p>
-      <p><FontAwesome name="usd" />{'\u00A0'}-9.00</p>
-    </div>
-    <div className="analysis-container--taxes">
-      <p>Taxes</p>
-      <p><FontAwesome name="usd" />{'\u00A0'}8.10</p>
-    </div>
-    <div className="analysis-container--grand-total">
-      <h3>Grand Total</h3>
-      <h3><FontAwesome name="usd" />{'\u00A0'}8.10</h3>
-    </div>
-  </div>
-  <div className="terms-agreement">
-    <input type="checkbox" className="checkbox" value={'\f067'} />
-    <p>I have read & agree to all <Link to="/terms_and_conditions">
-      Terms & Conditions
-    </Link></p>
-  </div>
-  <div className="purchase-btn">
-    <button
-      onClick={() => console.info('PLACE ORDER')}
-    >
-      <span className="btn-flex-parent">
-        <FontAwesome name="barcode" />
-        {'\u00A0'}
-        <p>Place Order Now</p>
-      </span>
-    </button>
-  </div>
+<div className="analysis-container">
+<div className="analysis-container--subtotal">
+<p>Subtotal</p>
+<p><FontAwesome name="usd" />{'\u00A0'}90.00</p>
+</div>
+<div className="analysis-container--shipping">
+<p>Shipping & Handling</p>
+<p><i>Free</i></p>
+</div>
+<div className="analysis-container--discount">
+<p>New Member Discount</p>
+<p><FontAwesome name="usd" />{'\u00A0'}-9.00</p>
+</div>
+<div className="analysis-container--taxes">
+<p>Taxes</p>
+<p><FontAwesome name="usd" />{'\u00A0'}8.10</p>
+</div>
+<div className="analysis-container--grand-total">
+<h3>Grand Total</h3>
+<h3><FontAwesome name="usd" />{'\u00A0'}8.10</h3>
+</div>
+</div>
+<div className="terms-agreement">
+<input type="checkbox" className="checkbox" value={'\f067'} />
+<p>I have read & agree to all <Link to="/terms_and_conditions">
+Terms & Conditions
+</Link></p>
+</div>
+<div className="purchase-btn">
+<button
+onClick={() => console.info('PLACE ORDER')}
+>
+<span className="btn-flex-parent">
+<FontAwesome name="barcode" />
+{'\u00A0'}
+<p>Place Order Now</p>
+</span>
+</button>
+</div>
 </div> */}
 
 const { bool, number, shape } = PropTypes;
@@ -146,6 +146,19 @@ ShoppingCartTotal.propTypes = {
     taxes: number.isRequired,
     grandTotal: number.isRequired,
     subTotal: number.isRequired,
-  }).isRequired,
+  }),
+};
+ShoppingCartTotal.defaultProps = {
+  total: {
+    discount: {
+      qty: false,
+      qtyAmount: 0,
+      register: false,
+      registerAmount: 0,
+    },
+    taxes: 0,
+    grandTotal: 0,
+    subTotal: 0,
+  },
 };
 export default ShoppingCartTotal;
