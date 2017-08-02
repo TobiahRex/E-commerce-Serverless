be56a164a823cee609ca5d28d8c817c5271116b5
TobiahRex <tobiah.rex@gmail.com>
2017-08-02 07:33:01 +0700

    building "recursiveArrayCheck".

--- src/services/utils/cleanOffTypename.js
+++ src/services/utils/cleanOffTypename.js
@@ -3,7 +3,7 @@
 * - Removes the property "__typename" from objects received from Apollo graphql queries.
 */
 
-const example = [{
+const example = {
   __typename: 'layer1',
   layer1Name: 'toby',
   entry1: {
@@ -17,22 +17,40 @@ const example = [{
       layer3Name: 'bickley',
     }],
   },
-}];
+};
 
-const cleanOffTypename = (cleanMe) => {
+// const cleanOffTypename = (cleanMe) => {
+//
+//
+//   if (Array.isArray(cleanMe)) {
+//     cleanMe.map((object) => {
+//
+//     })
+//   } else if (!Array.isArray(cleanMe) && (typeof cleanMe === 'object')) {
+//     const newObject = {};
+//     Object.keys(cleanMe)
+//     .forEach((key) => {
+//       if (key !== '__typename') {
+//         newObject[key] = cleanMe[key];
+//       }
+//     });
+//   }
+// };
 
+// 1) Check if input is an array.  If so, recursively check again.
+// 2) Once an object is found, then do some work, and return the result.
 
-  if (Array.isArray(cleanMe)) {
-    cleanMe.map((object) => {
+const recursiveArrayCheck = (input) => {
+  if (typeof input === 'object') {
+    if (!Array.isArray(input)) {
+      Object.keys(input).forEach((key) => {
+        if (typeof input[key] !== 'object') {
+          if (key === '__typename') delete input[key];
+        }
+      });
+    } else {
 
-    })
-  } else if (!Array.isArray(cleanMe) && (typeof cleanMe === 'object')) {
-    const newObject = {};
-    Object.keys(cleanMe)
-    .forEach((key) => {
-      if (key !== '__typename') {
-        newObject[key] = cleanMe[key];
-      }
-    });
+    }
   }
+  return input;
 };
