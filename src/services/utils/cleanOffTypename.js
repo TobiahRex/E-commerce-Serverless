696196f37e29e15b40afc85a9881f09683e5cb48
TobiahRex <tobiah.rex@gmail.com>
2017-08-02 07:15:56 +0700

    still building "cleanOffTypename".

--- src/services/utils/cleanOffTypename.js
+++ src/services/utils/cleanOffTypename.js
@@ -3,8 +3,36 @@
 * - Removes the property "__typename" from objects received from Apollo graphql queries.
 */
 
+const example = [{
+  __typename: 'layer1',
+  layer1Name: 'toby',
+  entry1: {
+    __typename: 'layer2',
+    layer2Name: 'tobiah',
+    entry2: [{
+      __typename: 'layer3',
+      layer3Name: 'tobiah rex',
+    }, {
+      __typename: 'layer3',
+      layer3Name: 'bickley',
+    }],
+  },
+}];
+
 const cleanOffTypename = (cleanMe) => {
+
+
   if (Array.isArray(cleanMe)) {
-    cleanMe.map((object))
+    cleanMe.map((object) => {
+
+    })
+  } else if (!Array.isArray(cleanMe) && (typeof cleanMe === 'object')) {
+    const newObject = {};
+    Object.keys(cleanMe)
+    .forEach((key) => {
+      if (key !== '__typename') {
+        newObject[key] = cleanMe[key];
+      }
+    });
   }
-}
+};
