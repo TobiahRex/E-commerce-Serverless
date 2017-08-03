9540f6769f54b6c6f4b603ac871a9afec2f3993b
TobiahRex <tobiah.rex@gmail.com>
2017-08-03 19:06:18 +0900

    finished function description.

--- src/services/utils/cleanOffTypename.js
+++ src/services/utils/cleanOffTypename.js
@@ -22,11 +22,26 @@ const example = {
 /**
 * Function: "cleanOffTypename"
 * - Takes any input, but acts only on an object.  Looks through each key, if a key is "__typename" then the key and value are removed.  If the key is a nested property, then the nested property is recursively called and the same evaluation takes place.
-1) Ensure type is an object (array or object literal).
-2) If an object literal, map through each key.
-3) If key is a Scalar type...
-3a) And key is "__typename", delete and return result.
-3b) if key is
+* 1) Ensure type is an object (array or object literal).
+* -----
+* 2a) If an object literal, map through each key.
+* 2a-1) If key is a Scalar type...
+* 2a-1-1) And key is "__typename", delete and return result.
+* 2a-1-2) if key is something else, simply assign the Scalar value to a new object and return.
+* 2a-2) If key is an array...
+* 2a-2-1) Recursively call "cleanOffTypename" with array content.
+* 2a-2-2) Once complete, assign the parent object value the array result and return.
+* 2a-3) If key is an object literal...
+* 2a-3-1) Recursively call "cleanOffTypename" with the nested Object literal content.
+* 2a-3-2) Once compelte, assign the parent object value the cleaned object and return.
+* -----
+* 2b)If an Array, map through each array value.
+* 2b-1) Recursively call "cleanOffTypename" with array content.
+* 2b-2) Once compelte, return the result.
+*
+* @param {object || array || scalar} input - Any JS primitive.
+*
+* @return {object || scalar} - The cleaned object result in case work has been done.  Scalar value if no work has been done.
 */
 
 const cleanOffTypename = (input) => {
@@ -62,25 +77,25 @@ const cleanOffTypename = (input) => {
   }
   return input;
 };
-console.log(JSON.stringify(cleanTypeName(example), null, 2));
+console.log(JSON.stringify(cleanOffTypename(example), null, 2));
 
 
-const example2 = [0, 0, [1, 1, 1, [2, 2, [3, 3, 3]]]];
-
-const rescursiveArrayIncrementor = (array, layerNumber) => {
-  let layer = layerNumber || 1;
-
-  if (!Array.isArray(array)) {
-    const result = array.map((content) => {
-      if (!Array.isArray(content)) {
-        content += layer;
-        return content;
-      }
-      layer += 1;
-      return rescursiveArrayIncrementor(content, layer);
-    });
-    return result;
-  }
-  throw Error(`The first argument should be an array. You passed a(n) "${typeof array}"`);
-};
-// console.log(JSON.stringify(rescursiveArrayIncrementor(example2), null, 2));
+// const example2 = [0, 0, [1, 1, 1, [2, 2, [3, 3, 3]]]];
+//
+// const rescursiveArrayIncrementor = (array, layerNumber) => {
+//   let layer = layerNumber || 1;
+//
+//   if (!Array.isArray(array)) {
+//     const result = array.map((content) => {
+//       if (!Array.isArray(content)) {
+//         content += layer;
+//         return content;
+//       }
+//       layer += 1;
+//       return rescursiveArrayIncrementor(content, layer);
+//     });
+//     return result;
+//   }
+//   throw Error(`The first argument should be an array. You passed a(n) "${typeof array}"`);
+// };
+// // console.log(JSON.stringify(rescursiveArrayIncrementor(example2), null, 2));
