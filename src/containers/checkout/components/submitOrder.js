b5e6b60369f34918a53b84d029b75bc08e729b3d
TobiahRex <tobiah.rex@gmail.com>
2017-08-08 14:04:52 +0900

    added a dynamic render to the ExpressCheckout > SubmitOrder button based on cart population.

--- src/containers/checkout/components/submitOrder.js
+++ src/containers/checkout/components/submitOrder.js
@@ -3,17 +3,31 @@ import PropTypes from 'prop-types';
 import Validation from 'react-validation';
 import FontAwesome from 'react-fontawesome';
 
-function SubmitOrder() {
+function SubmitOrder({ show }) {
   return (
-    <div className="checkout__purchase-btn">
-      <Validation.components.Button className="button" errorClassName="asd" >
-        <span className="btn-flex-parent">
-          <FontAwesome name="barcode" />
-          {'\u00A0'}
-          <p>Place Order Now</p>
-        </span>
-      </Validation.components.Button>
-    </div>
+    show ?
+      <div className="checkout__purchase-btn">
+        <Validation.components.Button
+          className="button"
+          errorClassName="asd"
+        >
+          <span className="btn-flex-parent">
+            <FontAwesome name="barcode" />
+            {'\u00A0'}
+            <p>Place Order Now</p>
+          </span>
+        </Validation.components.Button>
+      </div>
+      :
+      <div className="checkout__purchase-btn">
+        <button className="button" disabled>
+          <span className="btn-flex-parent">
+            <FontAwesome name="barcode" />
+            {'\u00A0'}
+            <p>Place Order Now</p>
+          </span>
+        </button>
+      </div>
   );
 }
 SubmitOrder.propTypes = {
