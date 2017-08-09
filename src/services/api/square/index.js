b30d274b593d31eae07c68a6517fdef95fb9f3ba
TobiahRex <tobiah.rex@gmail.com>
2017-08-09 19:17:38 +0900

    building out request body for "authorizeDelayTransaction".

--- src/services/api/square/index.js
+++ src/services/api/square/index.js
@@ -9,11 +9,21 @@ const createSquareAPI = () => {
   const authorizeDelayTransaction = ({
     locationId,
     transactionId,
+    shippingEmail,
   }) => api.post(
     `/locations/${locationId}/transactions`,
     {
       data: {
         idempotency_key: transactionId,
+        buyer_email_address: shippingEmail,
+        shipping_address: {
+          address_line_1: shippingAddressLine1,
+          address_line_2: shippingAddressLine2,
+          locality: shippingCity,
+          administrative_district_level_1: shippingPrefecture,
+          postal_code: shippingPostalCode,
+          country: shippingCountry,
+        },
       },
     },
 
