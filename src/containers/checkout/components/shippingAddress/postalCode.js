f2937cd5897c00d5043d6fb5078315dc1adb8bc4
TobiahRex <tobiah.rex@gmail.com>
2017-08-13 03:12:34 +0900

    converted postalCode.js into a func. component so that it will re-render based on Shipping Component.

--- src/containers/checkout/components/shippingAddress/postalCode.js
+++ src/containers/checkout/components/shippingAddress/postalCode.js
@@ -2,42 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Validation from 'react-validation';
 
-const { func, string } = PropTypes;
-
-class PostalCode extends React.Component {
-  static propTypes = {
-    apiError: string,
-    handleOnChange: func.isRequired,
-    shippingPostalCode: string,
-    SgValidatePostal: func.isRequired,
-  }
-
-  static defaultProps = {
-    apiError: '',
-    shippingPostalCode: '',
-  }
-
-  handleOnChange = e => this.props.handleOnChange(e)
-
-  render() {
-    return (
-      <div className="input__row">
-        <div className="input__row--postal-code">
-          <p>Postal Code <span className="required">*</span></p>
-          <Validation.components.Input
-            errorClassName="is-invalid-input"
-            type="text"
-            containerClassName=""
-            name="shippingPostalCode"
-            validations={['required', 'japan-postal']}
-            onChange={this.handleOnChange}
-            onBlur={this.validatePostal}
-            value={this.props.shippingPostalCode}
-          />
-        </div>
+function PostalCode() {
+  return (
+    <div className="input__row">
+      <div className="input__row--postal-code">
+        <p>Postal Code <span className="required">*</span></p>
+        <Validation.components.Input
+          errorClassName="is-invalid-input"
+          type="text"
+          containerClassName=""
+          name="shippingPostalCode"
+          validations={['required', 'japan-postal']}
+          onChange={this.handleOnChange}
+          onBlur={this.props.SgValidatePostal}
+          value={this.props.shippingPostalCode}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+const { func, string } = PropTypes;
+PostalCode.propTypes = {
+  apiError: string,
+  handleOnChange: func.isRequired,
+  shippingPostalCode: string,
+  SgValidatePostal: func.isRequired,
+};
+
+PostalCode.defaultProps = {
+  apiError: '',
+  shippingPostalCode: '',
+};
 
 export default PostalCode;
