a325525e9bc8dd19eaeccdc4cf1da2a23aa88783
TobiahRex <tobiah.rex@gmail.com>
2017-08-12 23:06:25 +0900

    finished building "sagwaResponse.postal" handler.

--- src/services/utils/cleanSagawaResponse.js
+++ src/services/utils/cleanSagawaResponse.js
@@ -18,30 +18,30 @@ const postal = (response) => {
   console.log('RESPONSE:\n', response, '\n\n');
 
   if (problem) {
-    console.log('\nERROR: ', problem);
+    problem = `There was a Network Error.  ${problem}.  Please try again.  If the problem persists, please contact support.  We apologize for the inconvenience.`;
+  } else if (ok) {
     xml2js.parseString(data, (err, results) => {
       if (err) {
-        problem = err;
+        problem = `There was an internal Error:  ${err}.  Please try again.  If the problem persists, please contact support.  We apologize for the inconvenience.`;
       } else {
-        let response = results['soapenv:Envelope']['soapenv:Body'][0]['ns:getAddrResponse'][0]['ns:return'][0];
-
-        const postalCode = response.split('|')[0];
-        const jpAddress = response.split('|')[1];
-
-        data = {
-          postalCode,
-          jpAddress,
-        };
+        const { error, postalCode, jpAddress } = extractData(results);
+        if (error) {
+          problem = 'That postal code is invalid.  Verify you\'ve entered the correct postal code and please try again.';
+        } else {
+          data = {
+            postalCode,
+            jpAddress,
+          };
+          return ({
+            ok,
+            data,
+            problem,
+          });
+        }
       }
     });
   }
-  if (ok) {
-    xml2js.parseString(data, (err, results) => {
-      if (err) console.log('PARSE ERROR: \n', err);
-      console.log('PARSE OK: \n', JSON.stringify(results, null, 2));
-    });
-  }
-}
+};
 
 export default {
   postal,
