74050db6161e632283642cb29730b55cd3622e88
TobiahRex <tobiah.rex@gmail.com>
2017-08-14 00:49:58 +0900

    creating a "ValidatePostal" graphql query method on the client.

--- src/graphQL/queries.js
+++ src/graphQL/queries.js
@@ -106,43 +106,9 @@ export const FetchMultipleProductsOptions = ({ loggedIn, userCart, guestCart }) 
   });
 };
 
-export const FetchSquareLocations = gql`
-  query FetchSquareLocations {
-    FetchSquareLocations {
-      error {
-        hard
-        soft
-        message
-      }
-      id
-      name
-      address
-      timezone
-      capabilities
-      status
-    }
-  }
+export const ValidatePostal = gql`
+  query ValidatePostal(
+    $postalCode: StringType!
+    $userId: ID!
+  )
 `;
-// export const FetchSquareLocations = gql`
-//   query FetchSquareLocations(
-//     # $userId: String!
-//     # $accessToken: String!
-//   ) {
-//     FetchSquareLocations(
-//       # userId: $userId
-//       # accessToken: $accessToken
-//     ) {
-//       error {
-//         hard
-//         soft
-//         message
-//       }
-//       id
-//       name
-//       address
-//       timezone
-//       capabilities
-//       status
-//     }
-//   }
-// `;
