03292a08ed89c473d698e2690b7b6e45e3e66507
TobiahRex <tobiah.rex@gmail.com>
2017-08-13 16:26:58 +0900

    building a "verifyPostal" method on the new "sagwaSchema".

---
+++ server/graphiql/db/mongo/models/sagawa.js
@@ -0,0 +1,8 @@
+import { Promise as bbPromise } from 'bluebird';
+import sagawaSchema from '../schemas/sagawaSchema';
+import db from '../connection';
+
+sagawaSchema.statics.verifyPostal = (postal) =>
+new Promise((resolve, reject) => ({
+  
+}))
