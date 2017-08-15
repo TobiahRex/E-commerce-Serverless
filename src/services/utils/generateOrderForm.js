eb99a38afd564b5be96ccd2ed93de3fd41fbf60b
TobiahRex <tobiah.rex@gmail.com>
2017-08-15 21:45:16 +0900

    CONTD - building "generateOrderForm".

--- src/services/utils/generateOrderForm.js
+++ src/services/utils/generateOrderForm.js
@@ -1,59 +1,65 @@
 export default function GenerateOrerForm({
   userId,
-
+  taxRate,
+  total,
 }) {
   const result = {
     userId,
     cart: cart.reduce((a, n) => {
-      
+      if (!!n._id) {
+        a._id = n._id;
+        a.qty = n.qty;
+        return a;
+      }
+      return a;
     },{}),
     taxes: {
-      city: "0.03"
-      state: "0.06"
-      total: "0.9"
-    }
+      city: String(taxRate.city),
+      state: String(taxRate.state),
+      total: String(taxRate.total),
+    },
     total: {
-      subTotal: "120"
-      tax: "12.23"
-      grandTotal: "132.23"
+      subTotal: total.subTotal,
+      tax: total.tax,
+      grandTotal: total.grandTotal,
       discount: {
-        qty: false
-        qtyAmount: ""
-        register: false
-        registerAmount: ""
-      }
-    }
+        qty: false,
+        qtyAmount: "",
+        register: false,
+        registerAmount: "",
+      },
+    },
     sagawa: {
-      sagawaId: "123123123123"
+      sagawaId: "123123123123",
       shippingAddress: {
-        givenName: "Toby"
-        familyName: "Bickley"
-        email: "bob@bob.com"
-        postalCode: 1231234
-        addressLine1: "asdfasdfsadf"
-        addressLine2: "asdfasdfsd"
-        country: "JP"
-        phoneNumber: "08039188013"
-      }
-    }
+        givenName: "Toby",
+        familyName: "Bickley",
+        email: "bob@bob.com",
+        postalCode: 1231234,
+        addressLine1: "asdfasdfsadf",
+        addressLine2: "asdfasdfsd",
+        country: "JP",
+        phoneNumber: "08039188013",
+      },
+    },
     square: {
       billingAddress: {
-        billingCountry: "Japan"
-        billingCity: "Yokohama"
-        billingPrefecture: "Kanagawa"
-      }
+        billingCountry: "Japan",
+        billingCity: "Yokohama",
+        billingPrefecture: "Kanagawa",
+      },
       cardInfo: {
-        last4: 1234
-        nameOnCard: "Bob Bobby"
-        cardNonce: "123asdf123"
-        expiration: "12/18"
-        cvn: 123
-        postalCode: "2342345"
-      }
+        last4: 1234,
+        nameOnCard: "Bob Bobby",
+        cardNonce: "123asdf123",
+        expiration: "12/18",
+        cvn: 123,
+        postalCode: "2342345",
+      },
       charge: {
-        amount: 120
-        currency: "USD"
-      }
-    }
-  }
+        amount: 120,
+        currency: "USD",
+      },
+    },
+  };
 };
