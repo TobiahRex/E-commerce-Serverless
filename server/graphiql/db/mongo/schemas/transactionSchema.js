dfa566a60572d3f98a7ee8ed159914ada4dbe166
TobiahRex <tobiah.rex@gmail.com>
2017-08-15 17:34:31 +0900

    updated transactionSchema in light of SubmitFinalOrder transactionTypes being finished.

--- server/graphiql/db/mongo/schemas/transactionSchema.js
+++ server/graphiql/db/mongo/schemas/transactionSchema.js
@@ -29,28 +29,29 @@ const transactionSchema = new Schema({
   sagawa: { type: ObjectId, ref: 'Sagawa' },
   marketHero: { type: ObjectId, ref: 'MarketHero' },
   invoiceEmail: { type: ObjectId, ref: 'Email' },
-  subtotal: { type: String, required: true },
-  tax: {
+  taxes: {
     city: { type: Number, required: true },
     state: { type: Number, required: true },
     total: { type: String, required: true },
   },
-  discount: {
-    qty: { type: Boolean, default: false },
-    qtyAmount: { type: String, default: '' },
-    register: { type: Boolean, default: false },
-    registerAmount: { type: String, default: '' },
+  total: {
+    subtotal: { type: String, required: true },
+    tax: { type: Number, required: true },
+    discount: {
+      qty: { type: Boolean, default: false },
+      qtyAmount: { type: String, default: '' },
+      register: { type: Boolean, default: false },
+      registerAmount: { type: String, default: '' },
+    },
+    grandTotal: { type: String, required: true },
   },
-  grandTotal: { type: String, required: true },
   square: {
     locationId: { type: String, default: '' },
     transactionId: { type: String, default: '' },
     cardNonce: { type: String, default: '' },
-    billingInfo: {
-      nameOnCard: { type: String, required: true },
-      last4: { type: String },
-      amount: { type: Number, required: true },
-    },
+    nameOnCard: { type: String, required: true },
+    last4: { type: String },
+    amount: { type: Number, required: true },
   },
 });
 export default transactionSchema;
