5ef6f856b905232a49ef2ef5b1d4f26f31800e12
TobiahRex <tobiah.rex@gmail.com>
2017-08-18 20:31:26 +0900

    finished writing "returnNextBusinessDay".

--- server/graphiql/db/mongo/models/sagawa/checkNextBusinessDay.js
+++ server/graphiql/db/mongo/models/sagawa/checkNextBusinessDay.js
@@ -3,12 +3,14 @@ import moment from 'moment';
 export default function returnNextBusinessDay() {
   const today = moment().format('dddd');
 
-  switch(today) {
-    case 'Monday': return 3;
-    case 'Tuesday': return 3;
-    case 'Wednesday': return 3;
-    case 'Thursday': return 3;
-    case 'Friday': return 5;
-    case 'Saturday'
+  switch (today) {
+    case 'Monday': return moment().add(3, 'days').format('YYYY/MM/DD');
+    case 'Tuesday': return moment().add(3, 'days').format('YYYY/MM/DD');
+    case 'Wednesday': return moment().add(3, 'days').format('YYYY/MM/DD');
+    case 'Thursday': return moment().add(3, 'days').format('YYYY/MM/DD');
+    case 'Friday': return moment().add(6, 'days').format('YYYY/MM/DD');
+    case 'Saturday': return moment().add(5, 'days').format('YYYY/MM/DD');
+    case 'Sunday': return moment().add(4, 'days').format('YYYY/MM/DD');
+    default: return moment().add(3, 'days').format('YYYY/MM/DD');
   }
 }
