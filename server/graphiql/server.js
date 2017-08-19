a5c7600892622ff626082420180100a7f57e8f33
Lakshman Diwaakar <diwaakartg@gmail.com>
2017-08-19 21:42:41 +0900

    Sagawa upload helper (#28)
    * Updated the cloud architecture diagram
    
    * Updated the correct imgur link
    
    * updated orderUpload statistic with URL and request parameters
    
    * updated express with /api route to mock sagawa independent lambda
    
    * api folder to mock independent sagawa lambda
    
    * updated sagawa with XML wrapper
    
    * updated the documentation for the mock sagawa lambda
    
    * replaced sagwa with sagawa
    
    * updated the orderUpload with URL and body to send to sagawa
    
    * changed the shipDate's datatype from Date to String
    
    * dleted unnecessary log statemnets
    
    * Updated the cloud architecture diagram
    
    * Updated the correct imgur link

--- server/graphiql/server.js
+++ server/graphiql/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import cors from 'cors';
 import schema from './db/graphql/schema';
+import api from './api/index';
 
 require('dotenv').load({ silent: true });
 
@@ -14,8 +15,10 @@ server.use('*', cors({ origin: 'http://localhost:3000' }));
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
 }));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
+server.use('/api', api);
 server.listen(PORT, () => console.log(`Server listening @ ${PORT}
 Graphiql Server @ http://localhost:${PORT}/graphiql`));
