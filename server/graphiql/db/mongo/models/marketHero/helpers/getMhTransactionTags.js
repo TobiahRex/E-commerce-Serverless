29154709d40930e6c7568fd661c93398fadd1bb8
TobiahRex <tobiah.rex@gmail.com>
2017-08-20 18:03:08 +0900

    finished refactoring "getMhTransactionTags".

--- server/graphiql/db/mongo/models/marketHero/helpers/getMhTransactionTags.js
+++ server/graphiql/db/mongo/models/marketHero/helpers/getMhTransactionTags.js
@@ -14,14 +14,19 @@ export default function getMhTransactionTags({
   if (register) tags.push('!Discount_register');
 
   const productTags = cart.reduce((acc, next) => {
-    const vendor = next.product.vendor.replace(/(\sJuice)/g, 'juice');
-    const productName = next.product.flavor.split(' ').reduce((a, n) => {
-      a += n.toUpperCase();
-      return a;
-    }, '');
-    const strength = `${next.product.nicotineStrength}mg`;
+    let qtyCounter = next.qty;
+    const flavorTags = [];
+    while (qtyCounter--) { //eslint-disable-line
+      const vendor = next.product.vendor.replace(/(\sJuice)/g, 'juice');
+      const productName = next.product.flavor.split(' ').reduce((a, n) => {
+        a += n.toUpperCase();
+        return a;
+      }, '');
+      const strength = `${next.product.nicotineStrength}mg`;
+      flavorTags.push(`${vendor}_${productName}_${strength}`);
+    }
 
-    acc = [...acc, `${vendor}_${productName}_${strength}`];
+    acc = [...acc, ...flavorTags];
 
     return acc;
   }, []);
