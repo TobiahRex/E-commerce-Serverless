acf6d125ba058222119d3baf8ac601ff69045848
TobiahRex <tobiah.rex@gmail.com>
2017-08-20 16:53:04 +0900

    finished writing "GetMHTransactionTags".

--- server/graphiql/db/mongo/models/marketHero/helpers/index.js
+++ server/graphiql/db/mongo/models/marketHero/helpers/index.js
@@ -1,6 +1,6 @@
 export default function GetMHTransactionTags({
   cart,
-  langauge,
+  language,
   total: {
     discount: {
       qty,
@@ -13,12 +13,18 @@ export default function GetMHTransactionTags({
   if (qty) tags.push('!Discount_qty');
   if (register) tags.push('!Discount_register');
 
-  let productTags = cart.reduce((acc, next) => {
-    let vendor = next.product.vendor.replace(/(\sJuice)/g, 'juice');
-    let productName = next.product.flavor.split(' ').reduce((a, n) => {
-
+  const productTags = cart.reduce((acc, next) => {
+    const vendor = next.product.vendor.replace(/(\sJuice)/g, 'juice');
+    const productName = next.product.flavor.split(' ').reduce((a, n) => {
+      a += n.toUpperCase();
+      return a;
     }, '');
+    const strength = `${next.product.nicotineStrength}mg`;
+
+    acc = [...acc, `${vendor}_${productName}_${strength}`];
 
     return acc;
   }, []);
+
+  tags.push(productTags);
 }
