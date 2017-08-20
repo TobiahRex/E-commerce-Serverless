b2d0b0681955de7a51932adadb2b557e0cb06d0e
TobiahRex <tobiah.rex@gmail.com>
2017-08-20 16:48:13 +0900

    building out "GetMHTransactionTags".

--- server/graphiql/db/mongo/models/marketHero/helpers/index.js
+++ server/graphiql/db/mongo/models/marketHero/helpers/index.js
@@ -12,9 +12,12 @@ export default function GetMHTransactionTags({
 
   if (qty) tags.push('!Discount_qty');
   if (register) tags.push('!Discount_register');
-  
 
   let productTags = cart.reduce((acc, next) => {
+    let vendor = next.product.vendor.replace(/(\sJuice)/g, 'juice');
+    let productName = next.product.flavor.split(' ').reduce((a, n) => {
+
+    }, '');
 
     return acc;
   }, []);
