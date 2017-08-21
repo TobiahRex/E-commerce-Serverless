fddfe96f5f11922a68397ef8326338bc764dca22
Lakshman <lakshmanTgld>
2017-08-21 21:18:16 +0900

    mocked cronJob in api route sagawa

--- server/graphiql/api/index.js
+++ server/graphiql/api/index.js
@@ -12,15 +12,28 @@ const router = new express.Router();
 */
 
 router.post('/sagawa', (req, res) => {
-  Sagawa.uploadOrderAndSendEmail(req.body)
-    .then((response) => {
-      console.log('SUCCEEDED: Upload Sagawa and Send Invoice Email.');
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      console.log('FAILED: Upload Sagawa and Send Invoice Email: ', error);
-      res.status(400).send(error);
-    });
+
+  if(req.body.eventSource === 'onDemand') {
+    Sagawa.uploadOrderAndSendEmail(req.body)
+      .then((response) => {
+        console.log('SUCCEEDED: Upload Sagawa and Send Invoice Email.');
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log('FAILED: Upload Sagawa and Send Invoice Email: ', error);
+        res.status(400).send(error);
+      });
+  } else {
+    Sagawa.findSagawaByStatus('uploaded')
+      .then((sagawaDocs) => {
+        console.log('SUCCEEDED: Retrieved Sagawa documents with matching status.');
+        res.status(200).send(sagawaDocs);
+      })
+      .catch((error) => {
+        console.log('FAILED: Retrieved Sagawa documents with matching status: ', error);
+        res.status(400).send(error);
+      });
+  }
 });
 
 export default router;
