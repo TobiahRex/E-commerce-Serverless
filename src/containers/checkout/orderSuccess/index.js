ee4bd8882abd31c8d64bcc3f1b2a1088975733bc
TobiahRex <tobiah.rex@gmail.com>
2017-08-23 17:19:09 +0900

    modified propTypes for orderSuccess.

--- src/containers/checkout/orderSuccess/index.js
+++ src/containers/checkout/orderSuccess/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import FontAwesome from 'react-fontawesome';
+import { graphql } from 'react-apollo';
 
 import {
   OrderHeader,
@@ -9,6 +10,10 @@ import {
   OrderSummary,
 } from './components';
 
+import {
+  FetchSagawa,
+} from '../../../graphql/queries';
+
 class OrderSuccess extends React.Component {
   constructor() {
     super(props);
@@ -16,6 +21,12 @@ class OrderSuccess extends React.Component {
   }
   render () {
     console.log('this.props: ', this.props);
+
+    const {
+      trackingInfo,
+      sagawaInfo,
+    } = this.props;
+
     return (
       <div className="ordered--main">
         <div className="ordered--container">
@@ -56,7 +67,15 @@ class OrderSuccess extends React.Component {
           />
         </div>
         <OrderSummary
-
+          shippingStatus={}
+          trackingId={}
+          qty={}
+          nicotineStrength={}
+          sku={}
+          price={}
+          subTotal={}
+          taxes={}
+          grandTotal={}
         />
         <div className="ordered__action-btns">
           <button
@@ -82,5 +101,104 @@ const OrderSuccessWithOrderInfo = graphql(FetchSagawa, {
   name: 'sagawaInfo',
 })(OrderSuccessWithState);
 
+const { shape, string, bool, number, arrayOf } = PropTypes;
+
+OrderSuccess.propTypes = {
+  transactionInfo: shape({
+    _id: string,
+    error: shape({
+      hard: bool,
+      soft: bool,
+      message: string,
+    }),
+    date: string,
+    comments: string,
+    termsAgreement: bool,
+    user: string,
+    products: arrayOf(
+      shape({
+        _id: string,
+        qty: number,
+      }),
+    ),
+    sagawa: string,
+    emailAddress: string,
+    emailLanguage: string,
+    invoiceEmailNoTracking: string,
+    jpyFxRate: string,
+    taxes: shape({
+      cityRate: string,
+      stateRate: string,
+      totalRate: string,
+    }),
+    total: shape({
+      subTotal: string,
+      taxes: string,
+      grandTotal: string,
+      discount: shape({
+        qty: bool,
+        qtyAmount: string,
+        register: bool,
+        registerAmount: string,
+      }),
+    }),
+    square: shape({
+      locationId: string,
+      transactionId: string,
+      billingCountry: string,
+      shippingAddress: shape({
+        shippingPrefecture: string,
+        shippingCity: string,
+      }),
+      cardInfo: shape({
+        last4: number,
+        nameOnCard: string,
+        cardNonce: string,
+        postalCode: string,
+      }),
+      charge: shape({
+        amount: string,
+        currency: string,
+      }),
+    }),
+  }).isRequired,
+  sagawaInfo: shape({
+    userId: string,
+    transactionId: string,
+    status: string,
+    uploadForm: string,
+    shippingAddress: shape({
+      awbId: string,
+      referenceId: string,
+      boxid: string,
+      shipdate: string,
+      customerName: string,
+      postal: string,
+      jpaddress1: string,
+      jpaddress2: string,
+      phoneNumber: string,
+      kbn: string,
+      wgt: number,
+      grandTotal: number,
+      deliveryDate: string,
+      deliveryTime: number,
+      souryo: number,
+      tesuryo: number,
+      ttlAmount: number,
+      codFlg: number,
+    }),
+    items: arrayOf(
+      shape({
+        productId: string,
+        itemcd: string,
+        itemname: string,
+        usage: number,
+        origin: string,
+        piece: number,
+        unitprice: number,
+      })
+    ),
+  }),
+};
 
 export default OrderSuccessWithOrderInfo;
