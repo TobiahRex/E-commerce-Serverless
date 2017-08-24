396f3277a8f12e24cfefd05dfde21d6dbc750ab6
TobiahRex <tobiah.rex@gmail.com>
2017-08-24 13:25:38 +0900

    connected OrderTracking comp. to redux and react-apollo.  Creating "FetchTrackingInfo" query now.

--- src/containers/tracking/container/index.js
+++ src/containers/tracking/container/index.js
@@ -1,103 +1,129 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import {
+  FetchTrackingInfo,
+} from '../../../graphql/queries';
 
 import {
   BreadCrumb,
 } from './component.imports';
 
 
-function OrderTracking() {
-  return (
-    <div className="order-tracking">
-      <BreadCrumb
-        paths={['Home']}
-        classes={['home']}
-        destination={['']}
-        lastCrumb="Express Checkout"
-      />
-      <div className="tracking__title">
-        <h1>Order Tracking</h1>
-      </div>
-      <div className="order-tracking__header">
-        <h4>Shipped Date: {Date.now()}</h4>
-        <h4>Tracking #: 123123123123</h4>
-      </div>
+class OrderTracking extends React.Component {
+  render() {
+    return (
+      <div className="order-tracking">
+        <BreadCrumb
+          paths={['Home']}
+          classes={['home']}
+          destination={['']}
+          lastCrumb="Express Checkout"
+        />
+        <div className="tracking__title">
+          <h1>Order Tracking</h1>
+        </div>
+        <div className="order-tracking__header">
+          <h4>Shipped Date: {Date.now()}</h4>
+          <h4>Tracking #: 123123123123</h4>
+        </div>
 
-      {/* NOTE: The background-colors for borders need to be dynamically created. */}
-      <div className="order-tracking__status-bar">
-        <ul className="status-bar--list">
-          <li className="list--option">
-            <div>
-              <p>Packaging</p>
-            </div>
-          </li>
-          <li className="list--option">
-            <div>
-              <p>Shipped</p>
-            </div>
-          </li>
-          <li className="list--option">
-            <div>
-              <p>Delivered</p>
-            </div>
-          </li>
-        </ul>
-      </div>
+        {/* NOTE: The background-colors for borders need to be dynamically created. */}
+        <div className="order-tracking__status-bar">
+          <ul className="status-bar--list">
+            <li className="list--option">
+              <div>
+                <p>Packaging</p>
+              </div>
+            </li>
+            <li className="list--option">
+              <div>
+                <p>Shipped</p>
+              </div>
+            </li>
+            <li className="list--option">
+              <div>
+                <p>Delivered</p>
+              </div>
+            </li>
+          </ul>
+        </div>
 
-      <div className="order-tracking__latest-activity">
-        <table className="activity--table">
-          <thead className="table__header">
-            <tr className="header-row">
-              <td className="header--location">
-                <h3>Location</h3>
-              </td>
-              <td className="header--date">
-                <h3>Date</h3>
-                <p>(YYYYMMDD)</p>
-              </td>
-              <td className="header--activity">
-                <h3>Activity</h3>
-              </td>
-            </tr>
-          </thead>
-          <tbody className="table__body">
-            {/* NOTE: Table row needs to be dynamically generated. */}
-            <tr className="body--row">
-              <td className="body--location">
-                <p>Los Angeles, CA</p>
-              </td>
-              <td className="body--date">
-                <p>20171017</p>
-              </td>
-              <td className="body--activity">
-                <p>Arrived at Transit Point</p>
-              </td>
-            </tr>
-            <tr className="body--row">
-              <td className="body--location">
-                <p>Tokyo, Japan</p>
-              </td>
-              <td className="body--date">
-                <p>20171017</p>
-              </td>
-              <td className="body--activity">
-                <p>Arrived</p>
-              </td>
-            </tr>
-            <tr className="body--row">
-              <td className="body--location">
-                <p>Kansai, Japan</p>
-              </td>
-              <td className="body--date">
-                <p>20171017</p>
-              </td>
-              <td className="body--activity">
-                <p>Arrived At Destination</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="order-tracking__latest-activity">
+          <table className="activity--table">
+            <thead className="table__header">
+              <tr className="header-row">
+                <td className="header--location">
+                  <h3>Location</h3>
+                </td>
+                <td className="header--date">
+                  <h3>Date</h3>
+                  <p>(YYYYMMDD)</p>
+                </td>
+                <td className="header--activity">
+                  <h3>Activity</h3>
+                </td>
+              </tr>
+            </thead>
+            <tbody className="table__body">
+              {/* NOTE: Table row needs to be dynamically generated. */}
+              <tr className="body--row">
+                <td className="body--location">
+                  <p>Los Angeles, CA</p>
+                </td>
+                <td className="body--date">
+                  <p>20171017</p>
+                </td>
+                <td className="body--activity">
+                  <p>Arrived at Transit Point</p>
+                </td>
+              </tr>
+              <tr className="body--row">
+                <td className="body--location">
+                  <p>Tokyo, Japan</p>
+                </td>
+                <td className="body--date">
+                  <p>20171017</p>
+                </td>
+                <td className="body--activity">
+                  <p>Arrived</p>
+                </td>
+              </tr>
+              <tr className="body--row">
+                <td className="body--location">
+                  <p>Kansai, Japan</p>
+                </td>
+                <td className="body--date">
+                  <p>20171017</p>
+                </td>
+                <td className="body--activity">
+                  <p>Arrived At Destination</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-export default OrderTracking;
+
+const OrderTrackingWithData = graphql(
+  FetchTrackingInfo, {
+    name: 'TrackingInfo',
+    options: ({ queryParams }) => {
+      const token = queryParams.token ? queryParams.token : '';
+      return ({
+        variables: { token },
+      });
+    },
+  },
+)(OrderTracking);
+
+const OrderTrackingWithStateAndData = connect(({ routing }) => ({
+  queryParams: routing.locationBeforeTransitions.query,
+}))(OrderTracking);
+
+
+export default OrderTrackingWithStateAndData;
