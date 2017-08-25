a8ce69f5473288e36338d356888345e0d9b56212
TobiahRex <tobiah.rex@gmail.com>
2017-08-25 16:28:03 +0900

    finished updating the sagawaOrderUpload error handling.

--- server/graphiql/db/mongo/models/transaction/index.js
+++ server/graphiql/db/mongo/models/transaction/index.js
@@ -213,13 +213,13 @@ new Promise((resolve, reject) => {
     Transaction.fetchSquareLocation(square.billingCountry),
     Product.find({ _id: { $in: cart.map(({ _id }) => _id) } }).exec(),
   ])
-  .then((results) => {
-    if (!results[0] || !results[1]) {
+  .then((results) => { // eslint-disable-line
+    if (!results[0] || !results[1] || !results[2] || !results[3]) {
       resolve({
         error: {
           hard: true,
           soft: false,
-          message: 'There was a network error and we\'re unable to process your order at this time.',
+          message: 'Oops! Looks like there\'s a network error.  Please try your order again later.',
         },
       });
     } else {
@@ -246,11 +246,8 @@ new Promise((resolve, reject) => {
       });
     }
   })
-  .then((response) => {
-    console.log('3] SUCCEEDED: Square Charge Customer.\n', response.data);
-
-    if (response.status !== 200) {
-      console.log('3a] FAILED: Square Charge Customer: ', response.data);
+  .then((response) => { //eslint-disable-line
+    if (response.status !== 2) {
       resolve({
         error: {
           hard: true,
@@ -258,136 +255,166 @@ new Promise((resolve, reject) => {
           message: HandleSquareErrors(response),
         },
       });
-    }
-    console.log('3b] Successfully charged customer card.');
-
-    return Promise.all([
-      User.findByIdAndUpdate(userDoc._id, {
-        $set: {
-          'shopping.transactions': [...userDoc.shopping.transactions, newTransactionDoc._id],
-          'shopping.cart': [],
-        },
-      }, { new: true }),
-      MarketHero.checkForLead(sagawa.shippingAddress.email),
-      Sagawa.handleNewTransaction({
-        cart: cartProducts,
-        total,
-        userId,
-        sagawa,
-        transactionId: newTransactionDoc._id,
-      }),
-    ]);
-  })
-  .then((results) => {
-    console.log('4] SUCCEEDED: 1) Updated User "cart" and "transactions" history.\n', results[0]._doc, '\n 2) Checked for existing Market Hero document.\n', results[1], '\n3) Created Sagawa document for this transaction.\n', results[2]._doc);
-
-    userDoc = { ...results[0]._doc };
-    marketHeroOp = results[1] ? 'updateMongoLead' : 'createMongoLead';
-
-    const lead = {
-      language,
-      email: sagawa.shippingAddress.email,
-      givenName: sagawa.shippingAddress.givenName,
-      familyName: sagawa.shippingAddress.familyName,
-    };
-
-    return Promise.all([
-      Email.createInvoiceEmailBody({
-        cart: cartProducts,
-        square,
-        sagawa: results[2]._doc,
-        language,
-        transaction: newTransactionDoc,
-      }),
-      MarketHero[marketHeroOp]({
-        lead,
-        tags: GetMhTransactionTagsMongo({
-          total,
-          language,
+    } else {
+      console.log('3] SUCCEEDED: Square Charge Customer.\n', response.data);
+      return Promise.all([
+        User.findByIdAndUpdate(userDoc._id, {
+          $set: {
+            'shopping.transactions': [...userDoc.shopping.transactions, newTransactionDoc._id],
+            'shopping.cart': [],
+          },
+        }, { new: true }),
+        MarketHero.checkForLead(sagawa.shippingAddress.email),
+        Sagawa.handleNewTransaction({
           cart: cartProducts,
-          subscribed: !!newsletterDecision,
-        }),
-      }),
-      MarketHero.createOrUpdateLead({
-        lead,
-        tags: GetMhTransactionTagsApi({
           total,
-          language,
-          cart: cartProducts,
-          subscribed: !!newsletterDecision,
+          userId,
+          sagawa,
+          transactionId: newTransactionDoc._id,
         }),
-      }),
-    ]);
-  })
-  .then((results) => {
-    console.log('5] SUCCEEDED: 1) Generate Invoice Email body and insert result into Transaction document.\n', results[0], '\n 2) Create or Update Mongo Market Hero document.\n', results[1], '\n 3) Create or Update Market Hero API lead.\n', results[2]);
-
-    newTransactionDoc = { ...results[0]._doc };
-
-    const promise1 = axios.post('http://localhost:3001/api/sagawa', {
-      userId,
-      sagawaId: newTransactionDoc.sagawa,
-      transactionId: newTransactionDoc._id,
-    });
-    let promise2 = null;
-    let promiseArray = [promise1];
-
-    if (marketHeroOp === 'createMongoLead') {
-      promise2 = User.findByIdAndUpdate(userId, {
-        $set: { 'marketing.marketHero': results[1]._id },
-      }, { new: true });
-      promiseArray = [...promiseArray, promise2];
+      ]);
     }
-
-    return Promise.all([...promiseArray]);
   })
-  .then((results) => {
-    console.log('6] SUCCEEDED: 1) Call Sagawa Order Upload lambda.', results[0].status, '\n2) Update User Document with new MarketHero Doc _id (if necessary).', results[1]);
-
-    const { status, data } = results[0];
-
-    if (results.length === 2) userDoc = results[1];
-
-    if (status !== 200) {
-      console.log('FAILED: Upload Order to Sagawa: ', data);
+  .then((results) => { //eslint-disable-line
+    if (!results[0] || !results[1] || !results[2]) {
       resolve({
         error: {
           hard: true,
           soft: false,
-          message: `Was not able to complete the order: ${data}`,
+          message: 'Oops! Looks like something went wrong.  Please try your order again later.',
         },
       });
+    } else {
+      console.log('4] SUCCEEDED: 1) Updated User "cart" and "transactions" history.\n', results[0]._doc, '\n 2) Checked for existing Market Hero document.\n', results[1], '\n3) Created Sagawa document for this transaction.\n', results[2]._doc);
+
+      userDoc = { ...results[0]._doc };
+      marketHeroOp = results[1] ? 'updateMongoLead' : 'createMongoLead';
+
+      const lead = {
+        language,
+        email: sagawa.shippingAddress.email,
+        givenName: sagawa.shippingAddress.givenName,
+        familyName: sagawa.shippingAddress.familyName,
+      };
+
+      return Promise.all([
+        Email.createInvoiceEmailBody({
+          cart: cartProducts,
+          square,
+          sagawa: results[2]._doc,
+          language,
+          transaction: newTransactionDoc,
+        }),
+        MarketHero[marketHeroOp]({
+          lead,
+          tags: GetMhTransactionTagsMongo({
+            total,
+            language,
+            cart: cartProducts,
+            subscribed: !!newsletterDecision,
+          }),
+        }),
+        MarketHero.createOrUpdateLead({
+          lead,
+          tags: GetMhTransactionTagsApi({
+            total,
+            language,
+            cart: cartProducts,
+            subscribed: !!newsletterDecision,
+          }),
+        }),
+      ]);
     }
-
-    cartProducts.forEach((productDoc) => {
-      const {
-        _id,
-        product,
-        statistics,
-      } = productDoc;
-
-      Product.findByIdAndUpdate(_id, {
-        $set: {
-          'product.quantities.inCarts': product.quantities.inCarts -= 1,
-          'product.quantities.purchased': product.quantities.inCarts += 1,
-          'statistics.completedCheckouts': statistics.completedCheckouts += 1,
-          'statistics.transactions': [...statistics.transactions, {
-            transactionId: newTransactionDoc._id,
-            userId,
-          }],
+  })
+  .then((results) => { //eslint-disable-line
+    if (!results[0] || !results[1] || !results[2]) {
+      resolve({
+        error: {
+          hard: true,
+          soft: false,
+          message: 'Oops! Looks like something went wrong.  Please try your order again later.',
         },
-      }, { new: true })
-      .then((savedDoc) => {
-        console.log('7] SUCCEEDED: Update "statistics" & "quantities" keys for product: ', `${savedDoc.product.flavor}_${savedDoc.product.nicotineStrength}mg`);
-      })
-      .catch((error) => {
-        console.log('FAILED: Update "statistics" & "quantities" keys for product: ', `${productDoc.product.flavor}_${productDoc.product.nicotineStrength}mg`, '. Error: ', error);
-        reject(new Error('FAILED: Update "statistics" & "quantities" keys for product: ', `${productDoc.product.flavor}_${productDoc.product.nicotineStrength}mg`));
       });
-    });
+    } else {
+      console.log('5] SUCCEEDED: 1) Generate Invoice Email body and insert result into Transaction document.\n', results[0], '\n 2) Create or Update Mongo Market Hero document.\n', results[1], '\n 3) Create or Update Market Hero API lead.\n', results[2]);
 
-    console.log('8] Order complete! Resolving with 1) User doc, 2) Transaction doc.');
-    resolve({ transaction: newTransactionDoc, user: userDoc });
+      newTransactionDoc = { ...results[0]._doc };
+
+      const promise1 = axios.post('http://localhost:3001/api/sagawa', {
+        userId,
+        sagawaId: newTransactionDoc.sagawa,
+        transactionId: newTransactionDoc._id,
+      });
+      let promise2 = null;
+      let promiseArray = [promise1];
+
+      if (marketHeroOp === 'createMongoLead') {
+        promise2 = User.findByIdAndUpdate(userId, {
+          $set: { 'marketing.marketHero': results[1]._id },
+        }, { new: true });
+        promiseArray = [...promiseArray, promise2];
+      }
+
+      return Promise.all([...promiseArray]);
+    }
+  })
+  .then((results) => { //eslint-disable-line
+    if (!results[0] || !results[1]) {
+      resolve({
+        error: {
+          hard: true,
+          soft: false,
+          message: 'Oops! Looks like something went wrong.  Please try your order again later.',
+        },
+      });
+    } else {
+      console.log('6] SUCCEEDED: 1) Call Sagawa Order Upload lambda.', results[0].status, '\n2) Update User Document with new MarketHero Doc _id (if necessary).', results[1]);
+
+      const { status, data } = results[0];
+
+      if (results.length === 2) userDoc = results[1];
+
+      if (status !== 200) {
+        console.log('FAILED: Upload Order to Sagawa: ', data);
+        resolve({
+          error: {
+            hard: true,
+            soft: false,
+            message: `Was not able to complete the order: ${data}`,
+          },
+        });
+      }
+
+      cartProducts.forEach((productDoc) => {
+        const {
+          _id,
+          product,
+          statistics,
+        } = productDoc;
+
+        Product.findByIdAndUpdate(_id, {
+          $set: {
+            'product.quantities.inCarts': product.quantities.inCarts -= 1,
+            'product.quantities.purchased': product.quantities.inCarts += 1,
+            'statistics.completedCheckouts': statistics.completedCheckouts += 1,
+            'statistics.transactions': [...statistics.transactions, {
+              transactionId: newTransactionDoc._id,
+              userId,
+            }],
+          },
+        }, { new: true })
+        .then((savedDoc) => {
+          console.log('7] SUCCEEDED: Update "statistics" & "quantities" keys for product: ', `${savedDoc.product.flavor}_${savedDoc.product.nicotineStrength}mg`);
+        })
+        .catch((error) => {
+          console.log('FAILED: Update "statistics" & "quantities" keys for product: ', `${productDoc.product.flavor}_${productDoc.product.nicotineStrength}mg`, '. Error: ', error);
+          reject(new Error('FAILED: Update "statistics" & "quantities" keys for product: ', `${productDoc.product.flavor}_${productDoc.product.nicotineStrength}mg`));
+        });
+      });
+
+      console.log('8] Order complete! Resolving with 1) User doc, 2) Transaction doc.');
+      resolve({ transaction: newTransactionDoc, user: userDoc });
+    }
   })
   .catch((error) => {
     console.log('Failed to submit order due to error: ', error);
