238d962d8b4b7a9af42cd00d08669df56e5660d0
Lakshman <lakshmanTgld>
2017-08-26 15:29:19 +0900

    constructed the emailRequest body after getting inputs from contactForm

--- server/graphiql/db/mongo/models/contact.js
+++ server/graphiql/db/mongo/models/contact.js
@@ -3,35 +3,47 @@ import { Promise as bbPromise } from 'bluebird';
 import contactSchema from '../schemas/contactSchema';
 import db from '../connection';
 import Email from './email';
+
 /**
 * Function: sendSupportMailAndNotifySlack
 *
-* 1) Invoke sendRawEmail from Email schema.
-* 2) Save the document in Contact collection with messageId from sendRawEmail response.
-* 3) Invoke notifySlack from Email model.
-* 4) Resolves || Rejects with result.
+* 1) Construct the emailRequest object for sendRawEmail from user input.
+* 2) Invoke sendRawEmail from Email schema.
+* 3) Save the document in Contact collection with messageId from sendRawEmail response.
+* 4) Invoke notifySlack from Email model.
+* 5) Resolves || Rejects with result.
 *
-* @param {Object} emailRequest - {
-    name - Name of the customer filing support
-    userId - mongo ID of user doc
-    bccEmailAddresses - {array} of mailIds
-    ccEmailAddresses - {array} of mailIds
-    toEmailAddresses - {array} of mailIds
-    sourceEmail - {string} from mailId (SES verified mailId)
-    replyToAddresses - {array} of mailIds
-    bodyHtmlData - {string} mail body html
-    bodyHtmlCharset - {string} mail body html's charset
-    bodyTextData - {string} mail body text
-    bodyTextCharset - {string} mail body text's charset
-    subjectData - {string} subject data
-    subjectCharset - {string} subject data's charset
+* @param {Object} contactForm - {
+    name - {string} Name of the customer filing support
+    userId - {string} mongo ID of user doc
+    message - {string} customer's support message
+    emailAddress - {string} customer's mailId
+    ccUser - {bool} whether to add user to CC this message
   } - Email JSON containing all the required SES parameters.
 *
 * @return {object} - Promise: resolve or reject the response.
 */
-contactSchema.statics.sendSupportMailAndNotifySlack = emailRequest =>
+contactSchema.statics.sendSupportMailAndNotifySlack = contactForm =>
 new Promise((resolve, reject) => {
   console.log('\n\n@Contact.sendSupportMailAndNotifySlack\n');
+  let emailRequest = {
+    sourceEmail: 'support@nj2jp.com',
+    toEmailAddresses: ['support@nj2jp.com'],
+    replyToAddresses: [contactForm.emailAddress],
+    bodyTextData: contactForm.message,
+    bodyTextCharset: 'utf8',
+    subjectData: 'Customer Name: ' + contactForm.name + ' - Support Required!!',
+    subjectCharset: 'utf8'
+  };
+
+  if (contactForm.ccUser) {
+    emailRequest.ccEmailAddresses = [contactForm.emailAddress];
+  }
+
+  if (contactForm.userId) {
+    emailRequest.userId = contactForm.userId;
+  }
+
   let contactDocument = {};
   let slackWebhook = '';
   let slackMessage = '';
@@ -39,20 +51,20 @@ new Promise((resolve, reject) => {
   Email.sendRawEmail(emailRequest)
   .then((response) => {
     console.log('SUCCEEDED: Email has been sent to NJ2JP support:', response);
-    contactDocument.messageId = response.messageId;
+    contactDocument.messageId = response.MessageId;
 
     slackWebhook = process.env.SLACK_CUSTOMER_WEBHOOK;
-    slackMessage = 'We have got a customer support from ' + emailRequest.replyToAddresses[0] + ' Log into <https://privateemail.com/appsuite/> to answer their query.'
+    slackMessage = 'We have got a customer support from ' + contactForm.emailAddress + '. Log into <https://privateemail.com/appsuite/> to answer their query.'
     return Email.notifySlack(slackWebhook, slackMessage);
   })
   .then((slackResponse) => {
     console.log('SUCCEEDED: Notification to Slack Customer channel:', slackResponse);
 
-    contactDocument.name = emailRequest.name;
-    contactDocument.emailAddress = emailRequest.replyToAddresses[0];
-    contactDocument.message = emailRequest.bodyTextData;
-    if (emailRequest.userId) {
-      contactDocument.user = emailRequest.userId;
+    contactDocument.name = contactForm.name;
+    contactDocument.emailAddress = contactForm.emailAddress;
+    contactDocument.message = contactForm.message;
+    if (contactForm.userId) {
+      contactDocument.userId = contactForm.userId;
     }
 
     return bbPromise.fromCallback(cb => Contact.create(contactDocument, cb));
