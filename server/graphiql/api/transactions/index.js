b53564674de4843588dd38ad7378e644eb7a318a
TobiahRex <tobiah.rex@gmail.com>
2017-09-01 01:22:32 +0900

    adding test api authorization to test api.

--- server/graphiql/api/transactions/index.js
+++ server/graphiql/api/transactions/index.js
@@ -6,14 +6,22 @@ import Transaction from '../../db/mongo/models/transaction';
 const router = new express.Router();
 
 router.post('/squareChargeCard', (req, res) => {
-  Transaction.squareChargeCard(req.body)
-  .then(() => res.status(200).send())
-  .catch(error => res.status(400).send(error));
+  if (req.body.password !== process.env.TEST_API_PASSWORD) {
+    res.unauthorized();
+  } else {
+    Transaction.squareChargeCard(req.body)
+    .then(() => res.status(200).send())
+    .catch(error => res.status(400).send(error));
+  }
 });
 
 router.post('/issueUserRefund', (req, res) => {
-  Transaction.issueUserRefund(req.body)
-  .then(() => res.status(200).send('Refund has been issued.'))
-  .catch(error => res.status(400).send(error.message));
+  if (req.body.password !== process.env.TEST_API_PASSWORD) {
+    res.unauthorized();
+  } else {
+    Transaction.issueUserRefund(req.body)
+    .then(() => res.status(200).send('Refund has been issued.'))
+    .catch(error => res.status(400).send(error.message));
+  }
 });
 export default router;
