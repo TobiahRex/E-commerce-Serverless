78f85560d24d44c6ad8b10a1a1d91ee5c64853a1
TobiahRex <tobiah.rex@gmail.com>
2017-09-04 21:53:04 +0900

    copied over reportSchema to serverless/.

---
+++ serverless/db/mongo/schemas/reportSchema/index.js
@@ -0,0 +1,27 @@
+import { getDate as GetDate } from './helpers';
+
+const Schema = require('mongoose').Schema;
+
+export const ObjectId = Schema.Types.ObjectId;
+
+const reportSchema = new Schema({
+  reportType: {
+    type: String,
+    enum: [
+      'cronJobError',
+      'cronJobSummary',
+      'cronJobEmpty',
+    ],
+    default: '',
+    required: true,
+  },
+  created: { type: String, default: GetDate },
+  messageId: { type: String, default: '' },
+  mainTitle: { type: String, default: '', required: true },
+  subTitle: { type: String, default: '', required: true },
+  headerBlurb: { type: String, default: '' },
+  data: {},
+}, {
+  bufferCommands: true,
+});
+export default reportSchema;
