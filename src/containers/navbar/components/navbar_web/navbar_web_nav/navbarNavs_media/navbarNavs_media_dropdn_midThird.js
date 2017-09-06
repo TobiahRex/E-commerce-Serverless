6395c41aba09a737ad4911687fc3401fc4d00435
TobiahRex <tobiah.rex@gmail.com>
2017-09-07 03:02:24 +0900

    modified media dropdown images to let images be links.

--- src/containers/navbar/components/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
+++ src/containers/navbar/components/navbar_web/navbar_web_nav/navbarNavs_media/navbarNavs_media_dropdn_midThird.js
@@ -1,31 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function NavbarNavsMediaDropdnTopthird() {
   return (
     <div className="media-dropdown-content-topThird">
       <div className="media-dropdown-content-topThird-images">
-        <img
-          className="media-dropdown-content-topThird-images-contactUs"
-          alt="Contact Us"
-        />
+        <Link
+          to={'/contact_us'} className="media-dropdown-content-midThird-links"
+        >
+          <img
+            className="media-dropdown-content-topThird-images-contactUs"
+            alt="Contact Us"
+          />
+        </Link>
       </div>
       <div className="media-dropdown-content-topThird-images">
-        <img
-          className="media-dropdown-content-topThird-images-juiceReviews"
-          alt="Juice Review"
-        />
+        <Link
+          to={'/reviews'} className="media-dropdown-content-midThird-links"
+        >
+          <img
+            className="media-dropdown-content-topThird-images-juiceReviews"
+            alt="Juice Review"
+          />
+        </Link>
       </div>
       <div className="media-dropdown-content-topThird-images">
-        <img
-          className="media-dropdown-content-topThird-images-vapeNews"
-          alt="Vape News"
-        />
+        <Link
+          to={'/vape_news'} className="media-dropdown-content-midThird-links"
+        >
+          <img
+            className="media-dropdown-content-topThird-images-vapeNews"
+            alt="Vape News"
+          />
+        </Link>
       </div>
       <div className="media-dropdown-content-topThird-images">
-        <img
-          className="media-dropdown-content-topThird-images-userStories"
-          alt="User Stories"
-        />
+        <Link
+          to={'/stories'} className="media-dropdown-content-midThird-links"
+        >
+          <img
+            className="media-dropdown-content-topThird-images-userStories"
+            alt="User Stories"
+          />
+        </Link>
       </div>
     </div>
   );
