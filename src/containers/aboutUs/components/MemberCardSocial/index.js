058d5d7481bb4269b003818ac5d04b5a679cfd52
TobiahRex <tobiah.rex@gmail.com>
2017-09-06 20:27:09 +0900

    refactored MemberCardSocial > AboutUs.

--- src/containers/aboutUs/components/MemberCardSocial/index.js
+++ src/containers/aboutUs/components/MemberCardSocial/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { WebflowJs } from './assets/utils';
 
 import {
@@ -9,18 +9,30 @@ import {
   MemberCardSocialInstagram,
 } from '../../components';
 
-const MemberCardSocial = () => {
+const MemberCardSocial = (props) => {
   WebflowJs(); //eslint-disable-line
+
+  const {
+    facebook,
+    twitter,
+    linkedin,
+    instagram,
+  } = props;
 
   return (
     <div className="about-container__staff staff--social-media">
-      <MemberSocialFacebook />
-      <MemberSocialLinkedin />
-      <MemberSocialTwitter />
-      <MemberCardSocialInstagram />
+      {facebook && <MemberSocialFacebook />}
+      {linkedin && <MemberSocialLinkedin />}
+      {twitter && <MemberSocialTwitter />}
+      {instagram && <MemberCardSocialInstagram />}
     </div>
   );
 };
-
+MemberCardSocial.propTypes = {
+  facebook: PropTypes.bool.isRequired,
+  instagram: PropTypes.bool.isRequired,
+  twitter: PropTypes.bool.isRequired,
+  linkedin: PropTypes.bool.isRequired,
+};
 
 export default MemberCardSocial;
