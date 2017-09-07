38f5aac7e81d7be58b0cede7a58c5602d5d2211b
Lakshman <lakshmanTgld>
2017-09-08 00:40:29 +0900

    replicated the changes to server/

--- server/graphiql/db/mongo/models/sagawa/helpers/getSagawaKbn.js
+++ server/graphiql/db/mongo/models/sagawa/helpers/getSagawaKbn.js
@@ -1,16 +1,9 @@
 export default function getSagawaKbn(shippingCountry) {
   const {
-    SAGAWA_ENV: sagawaEnv,
     SAGAWA_ENGLISH: sagawaKbnUs,
     SAGAWA_JAPANESE: sagawaKbnJp,
-    SAGAWA_TEST_ENGLISH: sagawaKbnUsTest,
-    SAGAWA_TEST_JAPANESE: sagawaKbnJpTest,
   } = process.env;
 
-  if (sagawaEnv === 'development') {
-    if (shippingCountry === 'JP') return sagawaKbnJpTest;
-    return sagawaKbnUsTest;
-  }
   if (shippingCountry === 'JP') return sagawaKbnJp;
   return sagawaKbnUs;
 }
