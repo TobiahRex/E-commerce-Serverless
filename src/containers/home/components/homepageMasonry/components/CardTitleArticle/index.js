8d66789aa984365f7d6548256a3915ccf3d9577e
TobiahRex <tobiah.rex@gmail.com>
2017-09-08 00:34:35 +0900

    modified CardTitleArticle.

--- src/containers/home/components/homepageMasonry/components/CardTitleArticle/index.js
+++ src/containers/home/components/homepageMasonry/components/CardTitleArticle/index.js
@@ -5,25 +5,25 @@ import { WebflowJs } from './assets/utils';
 const CardTitle = (props) => {
   WebflowJs();
   const {
+    link,
     header,
   } = props;
 
   return (
     <a
       className="masonry-column__news news--heading-link w-inline-block"
-      href="http://www.vapingpost.com/2017/05/03/air-sampling-confirms-secondhand-vapor-is-harmless/"
+      href={link}
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h3
-        className="content--header news__content"
-      >
+      <h3 className="content--header news__content" >
         {header}
       </h3>
     </a>
   );
 };
 CardTitle.propTypes = {
+  link: PropTypes.string.isRequired,
   header: PropTypes.string.isRequired,
 };
 
