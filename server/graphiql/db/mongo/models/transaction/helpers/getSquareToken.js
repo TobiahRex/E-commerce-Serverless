1de01f3cc3903490a3ed9310e66097ffb2127084
Lakshman <lakshmanTgld>
2017-09-08 00:40:29 +0900

    replicated the changes to server/

--- server/graphiql/db/mongo/models/transaction/helpers/getSquareToken.js
+++ server/graphiql/db/mongo/models/transaction/helpers/getSquareToken.js
@@ -1,19 +1,11 @@
 const {
-  SQUARE_ENV: squareEnv,
 
   US_SQUARE_ACCESS_TOKEN: usSquareAccessToken,
-  US_SQUARE_SANDBOX_ACCESS_TOKEN: usSquareSandboxAccessToken,
 
   JP_SQUARE_ACCESS_TOKEN: jpSquareAccessToken,
-  JP_SQUARE_SANDBOX_ACCESS_TOKEN: jpSquareSandboxAccessToken,
 } = process.env;
 
 export default function getSquareToken(country) {
-  if (country === 'US') {
-    if (squareEnv === 'development') return usSquareSandboxAccessToken;
-    return usSquareAccessToken;
-  }
-
-  if (squareEnv === 'development') return jpSquareSandboxAccessToken;
-  return jpSquareAccessToken;
+  if (country === 'JP') return jpSquareAccessToken;
+  return usSquareAccessToken;
 }
