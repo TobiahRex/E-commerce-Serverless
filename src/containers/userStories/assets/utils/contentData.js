f4475c36ed0b2d83dbbfbe5021874cbab955605b
TobiahRex <tobiah.rex@gmail.com>
2017-09-08 09:50:55 +0900

    end of day push.

--- src/containers/userStories/assets/utils/contentData.js
+++ src/containers/userStories/assets/utils/contentData.js
@@ -1,21 +1,46 @@
 export default {
-  english: [
-    {
-      component: 'UserCard',
-      props: {
-        CardImg: {
-          link: '/images/default-avatar.png',
-        },
-        CardHdr: {
-          header: 'User Story 1',
-        },
-        CardDate: {
-          date: 'Sep 8, 2017',
-        },
-        CardBlurb: {
-          blurb: 'The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.',
+  english: {
+    pageHeader: 'User Stories',
+    content: [
+      {
+        component: 'UserCard',
+        props: {
+          CardImg: {
+            link: '/images/default-avatar.png',
+          },
+          CardHdr: {
+            header: 'User Story 1',
+          },
+          CardDate: {
+            date: 'Sep 8, 2017',
+          },
+          CardBlurb: {
+            blurb: 'The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.The quick brown fox jumps over the lazy dog.',
+          },
         },
       },
-    },
-  ],
+    ],
+  },
+  japanese: {
+    pageHeader: 'ユーザーストーリー',
+    content: [
+      {
+        component: 'UserCard',
+        props: {
+          CardImg: {
+            link: '/images/default-avatar.png',
+          },
+          CardHdr: {
+            header: 'ユーザーストーリータイトル 1',
+          },
+          CardDate: {
+            date: '2017年9月8日',
+          },
+          CardBlurb: {
+            blurb: 'クイックブラウンキツネは怠惰な犬の上を飛びます。 クイックブラウンキツネは怠惰な犬の上を飛びます。 クイックブラウンキツネは怠惰な犬の上を飛びます。 クイックブラウンキツネは怠惰な犬の上を飛びます。 クイックブラウンキツネは怠惰な犬の上を飛びます。',
+          },
+        },
+      },
+    ],
+  },
 };
