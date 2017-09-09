cde76b97f5aed30327649b5174870b7c9143649b
TobiahRex <tobiah.rex@gmail.com>
2017-09-10 02:01:14 +0900

    updated and modified env check to include troubleshoot environment check.

--- server/envCheck.js
+++ server/envCheck.js
@@ -1,8 +1,11 @@
 import colors from 'colors';
 
-export default (nodeEnv) => {
+export default (nodeEnv, port) => {
   if (nodeEnv !== 'development') {
     console.log(colors.bold.underline.red('\n🚨  Local Server DENIED: \nYou are in a PRODUCTION environment  🚀 \n')); // eslint-disable-line no-console
+    throw new Error();
+  } else if (port === '') {
+    console.log(colors.bold.underline.red('\n🚨  Local Server DENIED: \nYou are in a PRODUCTION-TROUBLESHOOTING environment  🚀 \n')); // eslint-disable-line no-console
     throw new Error();
   } else {
     console.log(colors.bold.underline.white('\n🛠  You are in a DEVELOPMENT environment\n')); // eslint-disable-line no-console
