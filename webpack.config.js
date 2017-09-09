9e788faf6be9e56f3434f1d1045f3f6f039ef940
TobiahRex <tobiah.rex@gmail.com>
2017-09-10 00:36:47 +0900

    added userStores branch to production branch (master).

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-use-before-define, max-len, import/first */
-dotenv.load({ silent: true });
+dotenv.load({ silent: true }).config({ path: path.resolve('.env.prod') });
 
 import path from 'path';
 import dotenv from 'dotenv';
