6b555e052736e84de9abdec7d3d61ddf2db11652
TobiahRex <tobiah.rex@gmail.com>
2017-09-09 21:44:11 +0900

    changed the dispatched value for language changes from "english" to "en"

---
+++ src/redux/locale/initialState.js
@@ -0,0 +1,18 @@
+export default {
+  activeLanguage: 'en',
+  country: null,
+  translations: {
+    en: {
+      messages: {
+        'contactUs-BreadCrumb-Paths1': 'Home',
+        'contactUs-BreadCrumb-LastCrumb': 'Contact Us',
+      },
+    },
+    ja: {
+      messages: {
+        'contactUs-BreadCrumb-Paths1': '自宅',
+        'contactUs-BreadCrumb-LastCrumb': 'お問い合わせ',
+      },
+    },
+  },
+};
