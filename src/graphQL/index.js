ac7b27d20161e8bb044e2d63609cc3d6ab4877e4
TobiahRex <tobiah.rex@gmail.com>
2017-09-09 23:40:08 +0900

    changed graphql/index.js

--- src/graphQL/index.js
+++ src/graphQL/index.js
@@ -4,20 +4,11 @@ import {
 } from 'react-apollo';
 import logErrors from './errorLogging';
 
-const {
-  LAMBDA_ENV,
-  GRAPHQL_PORT,
-  CLIENT_GRAPHQL_URL,
-} = process.env;
-
-const uri = CLIENT_GRAPHQL_URL;
-
-
+const { CLIENT_GRAPHQL_URL: uri } = process.env;
+console.info('Apollo Netowrk URI = ', uri); // eslint-disable-line no-console
 if (!uri) throw new Error('Apollo Client network interface is not defined.');
-console.info('Apollo Netowrk URI = ', uri); //eslint-disable-line
 
 const networkInterface = createNetworkInterface({ uri });
-// networkInterface.use([storeQueriesMiddleware]);
 networkInterface.useAfter([logErrors]);
 const client = new ApolloClient({ networkInterface });
 
