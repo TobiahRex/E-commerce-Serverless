df7b39db6f521983aa0087c35511813f8fa637cc
TobiahRex <tobiah.rex@gmail.com>
2017-09-13 08:51:08 +0900

    finished re-formatting navbar file tree (for now).

--- src/containers/navbar/components/NavbarMobile/components/NavbarOptions/components/index.js
+++ src/containers/navbar/components/NavbarMobile/components/NavbarOptions/components/index.js
@@ -2,5 +2,5 @@ export NavbarLanguageBtn from './NavbarLanguageBtn';
 export NavbarLanguageDropdown from './NavbarLanguageDropdown';
 export NavbarLanguageBtnEnglish from './NavbarLanguageBtnEnglish';
 export NavbarLanguageBtnJapanese from './NavbarLanguageBtnJapanese';
-export NavbarLanguageDropdnEnglish from './NavbarLanguageDropdownEnglish';
-export NavbarLanguageDropdnJapanese from './NavbarLanguageDropdownJapanese';
+export NavbarLanguageDropdownEnglish from './NavbarLanguageDropdownEnglish';
+export NavbarLanguageDropdownJapanese from './NavbarLanguageDropdownJapanese';
