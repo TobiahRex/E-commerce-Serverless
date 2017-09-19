c3857e76cf2dd1ec117692a45c7604d04a738f9b
TobiahRex <tobiah.rex@gmail.com>
2017-09-19 11:01:44 +0900

    updated checkout address line1 shipping address line1 & line2.

--- src/containers/checkout/components/networkStatus.js
+++ src/containers/checkout/components/networkStatus.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import { withHandlers } from 'recompose';
+import { FormattedMessage as IntlMsg } from 'react-intl';
 
 function NetworkStatus({ routerPush, renderHelper }) {
   return (
@@ -14,9 +15,8 @@ function NetworkStatus({ routerPush, renderHelper }) {
           type="button"
           onClick={routerPush}
         >
-          <FontAwesome name="angle-double-left" />
-          {'\u00A0'}
-          Back To Homepage
+          <FontAwesome name="angle-double-left" />&nbsp;
+          <IntlMsg id="checkout.action-btn.back-to-home" />
         </button>
       </div>
     </div>
@@ -44,6 +44,7 @@ NetworkStatus.defaultProps = {
 const NetworkStatusWithHandlers = withHandlers({
   renderHelper: ({ errors, loading, success, toast }) => () => {
     let { message, hard } = errors;
+    // message = message[IntlLocale];
     const { soft } = errors;
     const showError = !!hard || !!soft || !!message.length || !!toast.message;
 
@@ -55,7 +56,9 @@ const NetworkStatusWithHandlers = withHandlers({
       <div className="checkout__error-hard">
         <div className="error-hard__title">
           <FontAwesome className="error-icon" name="times-circle" />
-          <h2>Error!</h2>
+          <h2>
+            <IntlMsg id="checkout.error.title" />
+          </h2>
         </div>
         <br />
         <p>{message}</p>
@@ -66,7 +69,7 @@ const NetworkStatusWithHandlers = withHandlers({
       <div className="checkout__error-soft">
         <div className="error-soft__title">
           <FontAwesome className="error-icon" name="exclamation-triangle" />
-          <h2>Error!</h2>
+          <h2><IntlMsg id="checkout.error.title" /></h2>
         </div>
         <br />
         <p>{message}</p>
@@ -77,7 +80,7 @@ const NetworkStatusWithHandlers = withHandlers({
       <div className="checkout__error-soft">
         <div className="error-soft__title">
           <FontAwesome className="error-icon" name="exclamation-triangle" />
-          <h2>Warning!</h2>
+          <h2><IntlMsg id="checkout.warning.title" /></h2>
         </div>
         <br />
         <p>{message}</p>
@@ -87,19 +90,22 @@ const NetworkStatusWithHandlers = withHandlers({
     const loadingMsg = (
       <div className="checkout__loading-icon">
         <FontAwesome className="spinner-icon" name="spinner" pulse />
-        <p>Please wait</p>
-        <p>while we submit your order...</p>
+        <p><IntlMsg id="checkout.loading.title" /></p>
+        <p><IntlMsg id="checkout.loading.subtitle" /></p>
       </div>
     );
 
     const successMsg = (
       <div className="checkout__successful-purchase">
         <div className="successful-purchase__title">
-          <FontAwesome className="success-icon" name="check-circle" />
-          <h2>Success!</h2>
+          <FontAwesome className="success-icon" name="check-circle" />&nbps;
+          <h2><IntlMsg id="checkout.submit.success.title" /></h2>
         </div>
         <br />
-        <p>Please wait one moment while we generate your Invoice...{'\u0020'}<FontAwesome className="success-spinner-icon" name="spinner" pulse /></p>
+        <p>
+          <IntlMsg id="checkout.submit.success.message" />&nbsp;
+          <FontAwesome className="success-spinner-icon" name="spinner" pulse />
+        </p>
       </div>
     );
 
