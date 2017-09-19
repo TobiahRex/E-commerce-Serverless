55be780ef1307d8ba9ecbf6d0f14b685f0089c56
TobiahRex <tobiah.rex@gmail.com>
2017-09-19 18:10:05 +0900

    CONTD - adding intl.messages to redux for shipping policy.

--- src/containers/legal/components/shippingPolicy/index.js
+++ src/containers/legal/components/shippingPolicy/index.js
@@ -28,28 +28,44 @@ function ShippingPolicy({ intl }) {
       <HdrPage header={header} />
       <br />
       <div className="main__body">
-        <h4>ORDER PROCESSING & SHIPMENT DELIVERY TIME</h4>
+        <h4>
+          <IntlMsg id="legal.policyy.shipping.order-process.title" />
+        </h4>
         <br />
         <p>
-          Orders will be processed and shipped at the next available business day.  Business days are between Monday (07:00 PST) - Friday (07:00 PST).  All Weekend orders (Friday after 07:00 PST, Saturday & Sunday) are processed first thing Monday morning at 07:00 PST.  This time frame usually results in your order being shipped within 1-3 business days after your full payment has been received. Shipment transit time will be between 2-7 business days depending on your physical address, accuracy of address information, availability of customers to answer questions (by contact number) from delivery personnel in the event of ambiguous shipping information, and/or any other unforseen circumstances.  We appreciate your understanding.
+          <IntlMsg id="legal.policyy.shipping.order-process.desc" />
         </p>
         <br />
         <br />
-        <h4>QUANTITY RESTRICTIONS</h4>
+        <h4>
+          <IntlMsg id="legal.policy.shipping.quantity-restrictions.title" />
+        </h4>
         <br />
         <p>
-          Japanese Ministry of Health, Labour & Wellfare have passed regulations on Nicotine e-juice imports.  A person(s) can import up to a maximum of 120 milliliters into the country at any one time, per person, per address, per month.  You can find more information at their website here: <a target="_blank" className="required" href="http://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iyakuhin/kojinyunyu/dl/kojinyunyu151130-04.pdf" rel="noopener noreferrer">Japanese Ministry of Health, Labour & Wellfare</a>.
+          <IntlMsg id="legal.policy.shipping.quantity-restrictions.blurb1" />
+          <a
+            target="_blank"
+            className="required"
+            href="http://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iyakuhin/kojinyunyu/dl/kojinyunyu151130-04.pdf"
+            rel="noopener noreferrer"
+          >
+            <IntlMsg id="legal.policy.shipping.quantity-restrictions.blurb2" />
+          </a>.
         </p>
         <br />
         <br />
-        <h4>CODs</h4>
+        <h4>
+          <IntlMsg id="legal.policy.shipping.cods.title" />
+        </h4>
         <br />
         <p>
-          We do not Accept {'COD\'s'} ({'"Cash On Delivery"'}).
+          <IntlMsg id="legal.policy.shipping.cods.desc" />
         </p>
         <br />
         <br />
-        <h4>IN A HURRY?</h4>
+        <h4>
+          IN A HURRY?
+        </h4>
         <br />
         <p>
           We offer priority handling and delivery on most items upon request. For special instructions, PLEASE ORDER BY PHONE.
