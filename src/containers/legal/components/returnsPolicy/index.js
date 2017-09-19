ec2824a52448501854a922117e3453ead8b3b700
TobiahRex <tobiah.rex@gmail.com>
2017-09-19 20:13:45 +0900

    finished i8ln for Returns Policy.

--- src/containers/legal/components/returnsPolicy/index.js
+++ src/containers/legal/components/returnsPolicy/index.js
@@ -27,18 +27,23 @@ function ReturnsPolicy({ intl }) {
         lastCrumb={lastCrumb}
       />
       <HdrPage header={header} />
+      <br />
       <div className="main__body">
-        <h4>REFUNDS</h4>
+        <h4>
+          <IntlMsg id="legal.policy.returns.refunds.title" />
+        </h4>
         <br />
         <p>
-          100% Money back guarantee for defective items. Defective items or missing parts must be reported within 15 days of delivery. Shipping damage(s) must be reported immediately (same day as delivery | UTC +9:00 TimeZone) upon receipt of package(s). Returns and exchanges for any other reason will not be accepted.
+          <IntlMsg id="legal.policy.returns.refunds.desc" />
         </p>
         <br />
         <br />
-        <h4>CANCELLATIONS</h4>
+        <h4>
+          <IntlMsg id="legal.policy.returns.cancellations.title" />
+        </h4>
         <br />
         <p>
-          Cancellations on orders can be requestd via email to support@nj2jp.com (include your order number located in your invoice email as well as provided on the website directly after successfully purchasing your order).  A Confirmation email from Nic Juice To Japan (NJ2JP) for cancellation(s) must be received by the customer before the close of business on the same day (Before 24:00 | UTC +9:00 TimeZone) of purchase for the cancellation to be valid.
+          <IntlMsg id="legal.policy.returns.cancellations.desc" />
         </p>
       </div>
     </div>
