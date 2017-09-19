28f7d8117965554ee1109f5534fe93df82499a2a
TobiahRex <tobiah.rex@gmail.com>
2017-09-19 19:51:18 +0900

    finished adding descs to i8ln for returns policy - refactoring JSX.

--- src/containers/legal/components/returnsPolicy/index.js
+++ src/containers/legal/components/returnsPolicy/index.js
@@ -9,29 +9,35 @@ import {
   HdrPage,
 } from './components';
 
-function ReturnsPolicy() {
+function ReturnsPolicy({ intl }) {
+  const {
+    messages: {
+      'legal.breadCrumb.paths1': bcPaths1,
+      'legal.breadCrumb.lastCrumb': lastCrumb,
+      'legal.policy.returns.header.title': header,
+    },
+  } = intl;
+
   return (
     <div className="returns-policy__main">
       <BreadCrumb
-        paths={[this.intl.bcPaths1]}
+        paths={[bcPaths1]}
         classes={['home']}
         destination={['']}
-        lastCrumb={this.intl.lastCrumb}
+        lastCrumb={lastCrumb}
       />
-      <HdrPage header={'user-stories.header.title'} />
-
-      <div className="main__title">
-        <h1>Returns Policy</h1>
-      </div>
+      <HdrPage header={header} />
       <div className="main__body">
+        <h4>REFUNDS</h4>
+        <br />
         <p>
-          <h4>REFUNDS</h4>
-          <br />
           100% Money back guarantee for defective items. Defective items or missing parts must be reported within 15 days of delivery. Shipping damage(s) must be reported immediately (same day as delivery | UTC +9:00 TimeZone) upon receipt of package(s). Returns and exchanges for any other reason will not be accepted.
-          <br />
-          <br />
-          <h4>CANCELLATIONS</h4>
-          <br />
+        </p>
+        <br />
+        <br />
+        <h4>CANCELLATIONS</h4>
+        <br />
+        <p>
           Cancellations on orders can be requestd via email to support@nj2jp.com (include your order number located in your invoice email as well as provided on the website directly after successfully purchasing your order).  A Confirmation email from Nic Juice To Japan (NJ2JP) for cancellation(s) must be received by the customer before the close of business on the same day (Before 24:00 | UTC +9:00 TimeZone) of purchase for the cancellation to be valid.
         </p>
       </div>
--- src/redux/locale/initialState.js
+++ src/redux/locale/initialState.js
@@ -383,6 +383,11 @@ export default {
         'legal.policy.shipping.tax.desc': 'We collect an exclusive sales tax when you place your order in compliance with tax requirements for the state of Washington. All shipments to Japan are subject to Washington State Sales Tax.',
         'legal.policy.shipping.customer-service.title': 'SALES TAX',
         'legal.policy.shipping.customer-service.desc': 'Our helpful customer service department is committed to assist you in every way. If you have a question or problem with your order, please call 1-855-587-8888 for Customer Service, Monday–Friday, 8am – 5pm (UTC +9). Or email us at support@vapeswitch.com 24 hours a day, 7 days a week. Special requests or adjustments need to be arranged with our Customer Service department before your order has shipped.',
+        'legal.policy.returns.header.title': 'Returns Policy',
+        'legal.policy.returns.refunds.title': 'REFUNDS',
+        'legal.policy.returns.refunds.desc': '100% Money back guarantee for defective items. Defective items or missing parts must be reported within 15 days of delivery. Shipping damage(s) must be reported immediately (same day as delivery | UTC +9:00 TimeZone) upon receipt of package(s). Returns and exchanges for any other reason will not be accepted.',
+        'legal.policy.returns.cancellations.title': 'CANCELLATIONS',
+        'legal.policy.returns.cancellations.desc': 'Cancellations on orders can be requestd via email to support@nj2jp.com (include your order number located in your invoice email as well as provided on the website directly after successfully purchasing your order).  A Confirmation email from Nic Juice To Japan (NJ2JP) for cancellation(s) must be received by the customer before the close of business on the same day (Before 24:00 | UTC +9:00 TimeZone) of purchase for the cancellation to be valid.',
       },
     },
     ja: {
