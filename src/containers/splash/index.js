c1e6730548d3fa2a87d35aa3f83248fb174cbea9
TobiahRex <tobiah.rex@gmail.com>
2017-09-20 19:20:52 +0900

    reformatting Splash Weblfow HTML into JSX.

--- src/containers/splash/index.js
+++ src/containers/splash/index.js
@@ -62,7 +62,7 @@ class Splash extends React.Component {
             <div className="splash-img">
               <div className="splash-img__header-container">
                 <div className="header-container__box" data-ix="fade-in-on-load">
-                  <div className="box__img--container"><img className="box__img" sizes="(max-width: 479px) 83vw, (max-width: 991px) 350px, 500px" src="images/nj2jp_oneLine_2-1.png" srcset="images/nj2jp_oneLine_2-1-p-500.png 500w, images/nj2jp_oneLine_2-1-p-800.png 800w, images/nj2jp_oneLine_2-1-p-1080.png 1080w, images/nj2jp_oneLine_2-1-p-1600.png 1600w, images/nj2jp_oneLine_2-1-p-2000.png 2000w, images/nj2jp_oneLine_2-1-p-2600.png 2600w, images/nj2jp_oneLine_2-1-p-3200.png 3200w, images/nj2jp_oneLine_2-1.png 6200w" width="514" />
+                  <div className="box__img--container"><img className="box__img" sizes="(max-width: 479px) 83vw, (max-width: 991px) 350px, 500px" src="images/nj2jp_oneLine_2-1.png" srcSet="images/nj2jp_oneLine_2-1-p-500.png 500w, images/nj2jp_oneLine_2-1-p-800.png 800w, images/nj2jp_oneLine_2-1-p-1080.png 1080w, images/nj2jp_oneLine_2-1-p-1600.png 1600w, images/nj2jp_oneLine_2-1-p-2000.png 2000w, images/nj2jp_oneLine_2-1-p-2600.png 2600w, images/nj2jp_oneLine_2-1-p-3200.png 3200w, images/nj2jp_oneLine_2-1.png 6200w" width="514" />
                   </div>
                   <div className="box__header-container">
                     <h1 className="header-container__header" data-ix="staggered-load">
@@ -216,7 +216,7 @@ class Splash extends React.Component {
                       No one can deliver Nicotine E-Juice to Japan faster than us.
                       <br />
                       <br />
-                      Once you shop with us and see how fast we are,&nbsp;we're confident you&nbsp;won't&nbsp;want to buy Nicotine vape juice from anywhere else.
+                      Once you shop with us and see how fast we are,&nbsp;{'we\'re'} confident you&nbsp;{'won\'t'}&nbsp;want to buy Nicotine vape juice from anywhere else.
                       <br />
                       <br />
                       Hard to believe? Try us now! You’ll be happy you did.
@@ -224,7 +224,7 @@ class Splash extends React.Component {
                   </div>
                 </div>
                 <div className="content__button-container" data-ix="fade-up">
-                  <a className="large__button w-inline-block" data-ix="large-button-hover" href="#">
+                  <a className="large__button w-inline-block" data-ix="large-button-hover">
                     <div className="large__button--alt" />
                     <p className="large-button__text">
                       Buy Now
@@ -232,13 +232,13 @@ class Splash extends React.Component {
                   </a>
                 </div>
               </div>
-              <div
-                className="blurb-container__img-container" data-ix="fastest-delivery-fade-in-right">
+              <div className="blurb-container__img-container" data-ix="fastest-delivery-fade-in-right">
                 <img
+                  alt="delivery-scan"
                   className="image-container__img"
                   sizes="(max-width: 767px) 100vw, (max-width: 991px) 375px, 49vw"
                   src="images/fastest-delivery-scan.jpg"
-                  srcset="images/fastest-delivery-scan-p-500.jpeg 500w, images/fastest-delivery-scan.jpg 648w"
+                  srcSet="images/fastest-delivery-scan-p-500.jpeg 500w, images/fastest-delivery-scan.jpg 648w"
                 />
               </div>
             </div>
@@ -258,16 +258,16 @@ class Splash extends React.Component {
             </div>
             <div className="how__curious-person">
               <img
+                alt="how"
                 className="curious-person__img"
                 sizes="(max-width: 479px) 195px, (max-width: 767px) 273px, (max-width: 991px) 389.859375px, 587.140625px"
                 src="images/girl-wondering.png"
-                srcset="images/girl-wondering-p-500.png 500w, images/girl-wondering.png 640w"
+                srcSet="images/girl-wondering-p-500.png 500w, images/girl-wondering.png 640w"
               />
               <div className="how__action-btn">
                 <a
                   className="show-me__button w-inline-block"
                   data-ix="show-me"
-                  href="#"
                 >
                   <div className="show-me__button--alt-1" />
                   <div className="show-me__button--alt-2" />
@@ -283,10 +283,11 @@ class Splash extends React.Component {
               <div className="how-steps__inner-container inner-container--white-background">
                 <div className="inner-container__left--container">
                   <img
+                    alt="couple-buying"
                     className="container__img"
                     sizes="(max-width: 767px) 100vw, 50vw"
                     src="images/nj2jp-homepage-carousel-couple-800x486.png"
-                    srcset="images/nj2jp-homepage-carousel-couple-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-couple-800x486.png 800w"
+                    srcSet="images/nj2jp-homepage-carousel-couple-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-couple-800x486.png 800w"
                   />
                 </div>
                 <div className="inner-container__blurb">
@@ -307,11 +308,13 @@ class Splash extends React.Component {
                 </div>
                 <div className="inner-container__right--container">
                   <img
+                    alt="distribution"
                     className="container__img"
                     data-ix="slide-on-scroll-right"
                     sizes="(max-width: 767px) 100vw, 50vw"
                     src="images/nj2jp-homepage-carousel-distribution-800x486.png"
-                    srcset="images/nj2jp-homepage-carousel-distribution-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-distribution-800x486.png 800w" />
+                    srcSet="images/nj2jp-homepage-carousel-distribution-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-distribution-800x486.png 800w"
+                  />
                 </div>
               </div>
             </div>
@@ -319,11 +322,13 @@ class Splash extends React.Component {
               <div className="how-steps__inner-container">
                 <div className="inner-container__left--container">
                   <img
+                    alt="warehouse"
                     className="container__img"
                     data-ix="slide-on-scroll-left"
                     sizes="(max-width: 767px) 100vw, 50vw"
                     src="images/nj2jp-homepage-carousel-warehouse-800x486.png"
-                    srcset="images/nj2jp-homepage-carousel-warehouse-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-warehouse-800x486.png 800w" />
+                    srcSet="images/nj2jp-homepage-carousel-warehouse-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-warehouse-800x486.png 800w"
+                  />
                 </div>
                 <div className="inner-container__blurb" data-ix="fade-on-scroll">
                   <p className="blurb__text">
@@ -343,11 +348,12 @@ class Splash extends React.Component {
                 </div>
                 <div className="inner-container__right--container">
                   <img
+                    alt="flight"
                     className="container__img"
                     data-ix="slide-on-scroll-right"
                     sizes="(max-width: 767px) 100vw, 50vw"
                     src="images/nj2jp-homepage-carousel-flight-800x486.png"
-                    srcset="images/nj2jp-homepage-carousel-flight-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-flight-800x486.png 800w"
+                    srcSet="images/nj2jp-homepage-carousel-flight-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-flight-800x486.png 800w"
                   />
                 </div>
               </div>
@@ -356,15 +362,19 @@ class Splash extends React.Component {
               <div className="how-steps__inner-container">
                 <div className="inner-container__left--container">
                   <img
+                    alt="delivery-truck"
                     className="container__img"
                     data-ix="slide-on-scroll-left"
                     sizes="(max-width: 767px) 100vw, 50vw"
                     src="images/nj2jp-homepage-carousel-truck-800x486.png"
-                    srcset="images/nj2jp-homepage-carousel-truck-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-truck-800x486.png 800w" />
+                    srcSet="images/nj2jp-homepage-carousel-truck-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-truck-800x486.png 800w"
+                  />
                 </div>
                 <div className="inner-container__blurb" data-ix="fade-on-scroll">
                   <p className="blurb__text">
-                    <strong className="text--number">5.</strong> Soon after landing, your package is on a delivery truck, on its way to your Japanese address.</p>
+                    <strong className="text--number">5.</strong>&nbsp;
+                    Soon after landing, your package is on a delivery truck, on its way to your Japanese address.
+                  </p>
                 </div>
               </div>
             </div>
@@ -372,40 +382,68 @@ class Splash extends React.Component {
               <div className="how-steps__inner-container inner-container--mobile">
                 <div className="inner-container__blurb section-blurb--grey" data-ix="fade-on-scroll">
                   <div className="blurb__text-container">
-                    <p className="blurb__text text--alt"><strong className="text--number">6.&nbsp;</strong>A few days later, you receive your package from Nic Juice 2 Japan. 😁</p>
+                    <p className="blurb__text text--alt">
+                      <strong className="text--number">6.</strong>&nbsp;
+                      A few days later, you receive your package from Nic Juice 2 Japan. 😁
+                    </p>
                   </div>
                   <div className="blurb__button--container" data-ix="fade-up-2">
-                                                              <a className="large__button w-inline-block" data-ix="large-button-hover" href="#">
-                                                                <div className="large__button--alt"></div>
-                                                                <p className="large-button__text text--phone-landscape">Buy Now</p>
-                                                              </a>
-                                                            </div>
-                                                          </div>
-                                                          <div className="inner-container__right--container"><img className="container__img" data-ix="slide-on-scroll-right" sizes="(max-width: 767px) 100vw, 50vw" src="images/nj2jp-homepage-carousel-delivery-800x486.png" srcset="images/nj2jp-homepage-carousel-delivery-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-delivery-800x486.png 800w"></div>
-                                                          </div>
-                                                        </div>
-                                                      </div>
+                    <a
+                      className="large__button w-inline-block"
+                      data-ix="large-button-hover"
+                      href="/juice/pina_colada"
+                    >
+                      <div className="large__button--alt" />
+                      <p className="large-button__text text--phone-landscape">
+                        Buy Now
+                      </p>
+                    </a>
+                  </div>
+                </div>
+                <div className="inner-container__right--container">
+                  <img
+                    alt="delivery"
+                    className="container__img"
+                    data-ix="slide-on-scroll-right"
+                    sizes="(max-width: 767px) 100vw, 50vw"
+                    src="images/nj2jp-homepage-carousel-delivery-800x486.png"
+                    srcSet="images/nj2jp-homepage-carousel-delivery-800x486-p-500.png 500w, images/nj2jp-homepage-carousel-delivery-800x486.png 800w"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="splash-news-reviews">
             <div className="news-reviews__header-container">
-              <h2 className="header__text" data-ix="flyout-footer">News &amp; Reviews</h2>
+              <h2 className="header__text" data-ix="flyout-footer">
+                News &amp; Reviews
+              </h2>
             </div>
             <div className="news-reviews__masonry-container">
-              <div className="place-holder-div__please-delete"></div>
+              <div className="place-holder-div__please-delete" />
             </div>
           </div>
           <div className="flyout__footer" data-ix="hide-on-load-2">
             <div className="footer__promotion-container">
               <div className="promotion-top">
                 <div className="non-member__title">
-                  <div className="tittle--header">Not a member?</div>
+                  <div className="tittle--header">
+                    Not a member?
+                  </div>
                 </div>
                 <div className="exit-btn__container" data-ix="close-popup">
-                  <div className="container__fa-text"></div>
+                  <div className="container__fa-text">
+                    
+                  </div>
                 </div>
               </div>
               <div className="promotion-bottom">
-                <div className="register-text">Register &amp; Save&nbsp;</div>
-                <div className="discount-text">10%</div>
+                <div className="register-text">
+                  Register &amp; Save
+                </div>&nbsp;
+                <div className="discount-text">
+                  10%
+                </div>
               </div>
             </div>
           </div>
