af11f3100f506abfb6b62242981f7c3f2147d751
TobiahRex <tobiah.rex@gmail.com>
2017-09-25 23:20:02 +0900

    updated reviews to try and initiate reviews carousel.

---
+++ src/redux/locale/initialState/japanese/legalReturnsPolicy.js
@@ -0,0 +1,7 @@
+export default {
+  'legal.policy.returns.header.title': '返品ポリシー',
+  'legal.policy.returns.refunds.title': '払い戻し',
+  'legal.policy.returns.refunds.desc': '不良品の100％返金保証。 不良品または不足している部品は、出荷後15日以内に報告する必要があります。 パッケージの受領後、すぐに配送損害（UTC + 9：00 TimeZoneと同じ日）を報告しなければなりません。 他の理由による返品および交換は受け付けられません。',
+  'legal.policy.returns.cancellations.title': 'キャンセル',
+  'legal.policy.returns.cancellations.desc': '注文のキャンセルは、support@nj2jp.comまで電子メールで請求することができます（請求書の電子メールに記載されている注文番号と、注文を正常に購入した後でウェブサイトに直接記載されています）。 同日（24：00〜UTC + 9：00 TimeZone前）の営業終了前に、Nic Juice To Japan（NJ2JP）からのキャンセルメールの確認メールをお受け取りください 有効である。',
+};
