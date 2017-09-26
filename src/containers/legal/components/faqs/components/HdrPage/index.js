f527d460a29bd753d33387c4bb08c03776df58de
TobiahRex <tobiah.rex@gmail.com>
2017-09-26 21:25:46 +0900

    adding lifecycle animations to FAQs.

--- src/containers/legal/components/faqs/components/HdrPage/index.js
+++ src/containers/legal/components/faqs/components/HdrPage/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './assets/styles/style.scss';
+// import {
+//   WebflowJs,
+//   WebflowAnimations,
+// } from '../../assets/utils';
 
-const HdrPage = props => (
-  <div className="main__title" data-ix="hdr-slide-from-top">
-    <h1 className="main__title title--header">
-      {props.header}
-    </h1>
-  </div>
-);
+const HdrPage = (props) => {
+  // WebflowJs();
+  // WebflowAnimations();
+  return (
+    <div className="main__title">
+      <h1 className="main__title title--header" data-ix="hdr-slide-from-top">
+        {props.header}
+      </h1>
+    </div>
+  );
+};
 
 HdrPage.propTypes = {
   header: PropTypes.string.isRequired,
