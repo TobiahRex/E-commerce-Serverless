5c70126298ce7433d80ba5d75243ec37cd658428
TobiahRex <tobiah.rex@gmail.com>
2017-09-26 20:05:50 +0900

    adding i8ln to FAQs .

---
+++ src/containers/legal/components/faqs/assets/utils/index.js
@@ -0,0 +1,4 @@
+export const WebflowJs = () => {
+  require('./webflow'); //eslint-disable-line
+};
+export WebflowAnimations from './webflow.animations';
---
+++ src/redux/locale/initialState/english/legalFaqs.js
@@ -0,0 +1,4 @@
+export default {
+  'legal.faqs.breadCrumb.paths1': 'Home',
+  'legal.faqs.breadCrumb.lastCrumb': 'FAQ\'s',
+};
