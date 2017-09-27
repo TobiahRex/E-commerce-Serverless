c44bcfedfdc15edd1d3b6b46b588947eb96d6bc1
TobiahRex <tobiah.rex@gmail.com>
2017-09-27 13:03:00 +0900

    refactored AboutUs.

--- src/containers/info/aboutUs/components/MemberCardTitle/index.js
+++ src/containers/info/aboutUs/components/MemberCardTitle/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage as IntlMsg } from 'react-intl';
-import { WebflowJs } from './assets/utils';
 
-const MemberCardTitle = (props) => {
-  WebflowJs(); //eslint-disable-line
+const MemberCardTitle = props => (
+  <h3 className={props.class}>
+    <IntlMsg id={props.title} />
+  </h3>
+);
 
-  return (
-    <div className="content--heading__container">
-      <h3 className="content--heading staff__content">
-        <IntlMsg id={props.header} />
-      </h3>
-      <h4 className="content--sub-heading staff__content">
-        <IntlMsg id={props.subHeader} />
-      </h4>
-    </div>
-  );
-};
 MemberCardTitle.propTypes = {
-  header: PropTypes.string.isRequired,
-  subHeader: PropTypes.string.isRequired,
+  class: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default MemberCardTitle;
