ca8d19a96602c0418a5748a5852b7a1f0c7ae9d7
TobiahRex <tobiah.rex@gmail.com>
2017-09-27 13:16:57 +0900

    refactored component assets for Aboutus.

--- src/containers/info/aboutUs/components/MemberSocialTwitter/index.js
+++ src/containers/info/aboutUs/components/MemberSocialTwitter/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
-import { WebflowJs } from './assets/utils';
 
-const MemberSocialTwitter = (props) => {
-  WebflowJs(); //eslint-disable-line
-
-  return (
-    <div className="staff__social-media">
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        className="social-media--link staff__social-media"
-        href={props.link}
-      >
-        <FontAwesome name="twitter" />
-      </a>
-    </div>
-  );
-};
+const MemberSocialTwitter = props => (
+  <div className="staff__social-media">
+    <a
+      rel="noopener noreferrer"
+      target="_blank"
+      className="social-media--link staff__social-media"
+      href={props.link}
+    >
+      <FontAwesome name="twitter" />
+    </a>
+  </div>
+);
 
 MemberSocialTwitter.propTypes = {
   link: PropTypes.string.isRequired,
--- src/containers/info/aboutUs/components/SubHdr/index.js
+++ src/containers/info/aboutUs/components/SubHdr/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 
 const SubHdr = () => (
-  <div class="about__container" data-ix="fade-in-2">
-    <h2 class="about-container__sub-heading">
+  <div className="about__container" data-ix="fade-in-2">
+    <h2 className="about-container__sub-heading">
       <IntlMsg id="aboutus.header.subtitle" />
     </h2>
   </div>
