d33b8c5450c636e9bc3fa499256f0a7f9664a389
TobiahRex <tobiah.rex@gmail.com>
2017-09-28 15:33:34 +0900

    updated WebflowAnimations for ShippingPOlicy.

---
+++ src/containers/info/legal/components/shippingPolicy/assets/utils/webflow.animations.js
@@ -0,0 +1,74 @@
+export default () => {
+  Webflow.require('ix').init([
+    {
+      slug: 'slide-from-top',
+      name: 'Slide from Top',
+      value: {
+        style: { x: '0px', y: '-100px', z: '0px' },
+        triggers: [
+          {
+            type: 'load',
+            stepsA: [{ transition: 'transform 450ms ease 0', x: '0px', y: '0px', z: '0px' }],
+            stepsB: [],
+          },
+        ],
+      },
+    },
+    {
+      slug: 'slide-from-left',
+      name: ' Slide From Left',
+      value: {
+        style: { opacity: 0, x: '-150px', y: '0px', z: '0px' },
+        triggers: [
+          {
+            type: 'scroll',
+            offsetTop: '10%',
+            offsetBot: '15%',
+            stepsA: [
+              { wait: '500ms' },
+              {
+                opacity: 1,
+                transition: 'transform 600ms ease-in-out 0, opacity 550ms ease 0',
+                x: '0px',
+                y: '0px',
+                z: '0px',
+              },
+            ],
+            stepsB: [
+              { wait: '500ms', opacity: 0, transition: 'opacity 500ms ease 0' },
+              { transition: 'transform 200 ease 0', x: '-150px', y: '0px', z: '0px' },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      slug: 'slide-from-right',
+      name: ' Slide From Right',
+      value: {
+        style: { opacity: 0, x: '150px', y: '0px', z: '0px' },
+        triggers: [
+          {
+            type: 'scroll',
+            offsetTop: '10%',
+            offsetBot: '15%',
+            stepsA: [
+              { wait: '500ms' },
+              {
+                opacity: 1,
+                transition: 'transform 600ms ease-in-out 0, opacity 550ms ease 0',
+                x: '0px',
+                y: '0px',
+                z: '0px',
+              },
+            ],
+            stepsB: [
+              { wait: '500ms', opacity: 0, transition: 'opacity 500ms ease 0' },
+              { transition: 'transform 200 ease 0', x: '150px', y: '0px', z: '0px' },
+            ],
+          },
+        ],
+      },
+    },
+  ]);
+};
--- src/containers/info/legal/components/shippingPolicy/index.js
+++ src/containers/info/legal/components/shippingPolicy/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { lifecycle } from 'recompose';
 import {
   injectIntl,
   intlShape,
@@ -8,6 +9,10 @@ import {
   HdrPage,
   BreadCrumb,
 } from './components';
+import {
+  WebflowAnimations,
+} from './assets/utils/';
+import './assets/styles/style.css';
 
 function ShippingPolicy({ intl }) {
   const {
@@ -40,57 +45,54 @@ function ShippingPolicy({ intl }) {
             </p>
           </div>
         </div>
-        <h4>
-          <IntlMsg id="legal.policy.shipping.quantity-restrictions.title" />
-        </h4>
-        <br />
-        <p>
-          <IntlMsg id="legal.policy.shipping.quantity-restrictions.blurb1" />
-          <a
-            target="_blank"
-            className="required"
-            href="http://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iyakuhin/kojinyunyu/dl/kojinyunyu151130-04.pdf"
-            rel="noopener noreferrer"
-          >
-            <IntlMsg id="legal.policy.shipping.quantity-restrictions.blurb2" />
-          </a>.
-        </p>
-        <br />
-        <br />
-        <h4>
-          <IntlMsg id="legal.policy.shipping.cods.title" />
-        </h4>
-        <br />
-        <p>
-          <IntlMsg id="legal.policy.shipping.cods.desc" />
-        </p>
-        <br />
-        <br />
-        <h4>
-          <IntlMsg id="legal.policy.shipping.hurry.title" />
-        </h4>
-        <br />
-        <p>
-          <IntlMsg id="legal.policy.shipping.hurry.desc" />
-        </p>
-        <br />
-        <br />
-        <h4>
-          <IntlMsg id="legal.policy.shipping.tax.title" />
-        </h4>
-        <br />
-        <p>
-          <IntlMsg id="legal.policy.shipping.tax.desc" />
-        </p>
-        <br />
-        <br />
-        <h4>
-          <IntlMsg id="legal.policy.shipping.customer-service.title" />
-        </h4>
-        <br />
-        <p>
-          <IntlMsg id="legal.policy.shipping.customer-service.desc" />
-        </p>
+        <div className="content-container__shipping-content">
+          <div className="shipping-content__hdr-container">
+            <h4 className="hdr-container__hdr-blurb" data-ix="slide-from-left">
+              <IntlMsg id="legal.policy.shipping.quantity-restrictions.title" />
+            </h4>
+          </div>
+          <div className="shipping-content__blurb-container">
+            <p className="blurb-container__blurb-text" data-ix="slide-from-right">
+              <IntlMsg id="legal.policy.shipping.quantity-restrictions.blurb1" />
+            </p>
+          </div>
+        </div>
+        <div className="content-container__shipping-content">
+          <div className="shipping-content__hdr-container">
+            <h4 className="hdr-container__hdr-blurb" data-ix="slide-from-left">
+              <IntlMsg id="legal.policy.shipping.cods.title" />
+            </h4>
+          </div>
+          <div className="shipping-content__blurb-container">
+            <p className="blurb-container__blurb-text" data-ix="slide-from-right">
+              <IntlMsg id="legal.policy.shipping.cods.desc" />
+            </p>
+          </div>
+        </div>
+        <div className="content-container__shipping-content">
+          <div className="shipping-content__hdr-container">
+            <h4 className="hdr-container__hdr-blurb" data-ix="slide-from-left">
+              <IntlMsg id="legal.policy.shipping.tax.title" />
+            </h4>
+          </div>
+          <div className="shipping-content__blurb-container">
+            <p className="blurb-container__blurb-text" data-ix="slide-from-right">
+              <IntlMsg id="legal.policy.shipping.tax.desc" />
+            </p>
+          </div>
+        </div>
+        <div className="content-container__shipping-content">
+          <div className="shipping-content__hdr-container">
+            <h4 className="hdr-container__hdr-blurb" data-ix="slide-from-left">
+              <IntlMsg id="legal.policy.shipping.customer-service.title" />
+            </h4>
+          </div>
+          <div className="shipping-content__blurb-container">
+            <p className="blurb-container__blurb-text" data-ix="slide-from-right">
+              <IntlMsg id="legal.policy.shipping.customer-service.desc" />
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -99,4 +101,9 @@ ShippingPolicy.propTypes = {
   intl: intlShape.isRequired,
 };
 const ShippingPolicyWithIntl = injectIntl(ShippingPolicy);
-export default ShippingPolicyWithIntl;
+const ShippingPolicyWithLifecycleAndIntl = lifecycle({
+  componentDidUpdate() {
+    WebflowAnimations();
+  },
+})(ShippingPolicyWithIntl);
+export default ShippingPolicyWithLifecycleAndIntl;
