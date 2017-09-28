4929323116ef980713d64b7dc9b7f19363a79a91
TobiahRex <tobiah.rex@gmail.com>
2017-09-28 22:00:27 +0900

    finished refactor of infoRoutes for non-codesplitting.

--- src/navigation/infoRoutes.js
+++ src/navigation/infoRoutes.js
@@ -6,13 +6,11 @@ import About from '../containers/info/aboutUs';
 import Faqs from '../containers/info/faqs';
 import Returns from '../containers/info/legal/components/returnsPolicy';
 import Shipping from '../containers/info/legal/components/shippingPolicy';
+import Privacy from '../containers/info/legal/components/privacyPolicy';
+import Terms from '../containers/info/legal/components/termsAndConditions';
+import Nicotine from '../containers/info/legal/components/nicotineDisclaimer';
 // import Wholesale from '../containers/info/legal/wholesale';
 // import Affiliates from '../containers/info/legal/affiliateProgram';
-
-const errorLoading = (error) => {
-  throw new Error(`Dynamic page loading failed ${error}`);
-};
-const loadRoute = cb => module => cb(null, module.default);
 
 const InfoRoutes = () => (
   <div>
@@ -34,27 +32,15 @@ const InfoRoutes = () => (
     />
     <Route
       path="/privacy_policy"
-      getComponent={(location, cb) => {
-        import('../containers/info/legal/components/privacyPolicy' /* webpackChunkName: "privacyPolicy" */)
-        .then(loadRoute(cb))
-        .catch(errorLoading);
-      }}
+      component={Privacy}
     />
     <Route
       path="/terms_and_conditions"
-      getComponent={(location, cb) => {
-        import('../containers/info/legal/components/termsAndConditions' /* webpackChunkName: "termsAndConditions" */)
-        .then(loadRoute(cb))
-        .catch(errorLoading);
-      }}
+      component={Terms}
     />
     <Route
       path="/nicotine_disclaimer"
-      getComponent={(location, cb) => {
-        import('../containers/info/legal/components/nicotineDisclaimer' /* webpackChunkName: "nicotineDisclaimer" */)
-        .then(loadRoute(cb))
-        .catch(errorLoading);
-      }}
+      component={Nicotine}
     />
     {/* <Route path="/wholesale" component={Wholesale} /> */}
     {/* <Route path="/affiliate_program" component={Affiliates} /> */}
