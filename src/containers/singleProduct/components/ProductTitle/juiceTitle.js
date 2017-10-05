6e2d0e1f3d9eef1a7e72d091367548235e31b314
TobiahRex <tobiah.rex@gmail.com>
2017-10-05 12:54:45 -0700

    renamed ProductTitle to ProductPageHdr.

---
+++ src/containers/singleProduct/components/ProductTitle/juiceTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function JuiceTitle({ title }) {
+  return (
+    <div className="desc__title">
+      <h1>{title[IntlLocale]}</h1>
+    </div>
+  );
+}
+const { string, shape } = PropTypes;
+JuiceTitle.propTypes = {
+  title: shape({
+    en: string,
+    ja: string,
+  }).isRequired,
+};
+export default JuiceTitle;
