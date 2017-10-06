7767c36c39a3be67b4f8d267222f64a53f260cb6
TobiahRex <tobiah.rex@gmail.com>
2017-10-06 09:01:52 -0700

    updated NicotineBtns to use new comp > OptionsNicotine.

---
+++ src/containers/singleProduct/components/OptionsNicotine/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { convertStrengthToNumber } from '../../assets/utils';
+
+function OptionsNicotine({
+  chosenStrength,
+  nicotineStrengths,
+  nicotineHandler,
+}) {
+  const style = {
+    active: {
+      backgroundColor: '#063A7A',
+    },
+    inactive: {
+      backgroundColor: '#FC2525',
+    },
+  };
+  nicotineStrengths = convertStrengthToNumber(nicotineStrengths);
+
+  return (
+    <div className="product-options__options-nicotine">
+      {
+        nicotineStrengths.map(({ _id, nicotineStrength }) => (
+          <div
+            key={_id}
+            className="options-nicotine__nicotine-button-container"
+            style={
+              nicotineStrength === chosenStrength ?
+              style.active : style.inactive
+            }
+          >
+            <button
+              data-nicotinestrength={nicotineStrength}
+              data-product={_id}
+              data-ix="new-interaction"
+              className="nicotine-button-container__nicotine-button w-inline-block"
+              onClick={nicotineHandler}
+            >
+              <div className="nicotine-button__text">
+                {`${nicotineStrength}mg`}
+              </div>
+            </button>
+
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+const { func, number, arrayOf, string, shape } = PropTypes;
+OptionsNicotine.propTypes = {
+  chosenStrength: number.isRequired,
+  nicotineHandler: func.isRequired,
+  nicotineStrengths: arrayOf(shape({
+    _id: string,
+    nicotineStrength: string,
+  })).isRequired,
+};
+export default OptionsNicotine;
