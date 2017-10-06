1a79d42685fdc1033981c504491ce5a018df5367
TobiahRex <tobiah.rex@gmail.com>
2017-10-06 16:00:04 -0700

    finished building QtySection comp.

--- src/containers/singleProduct/components/QtySection/index.js
+++ src/containers/singleProduct/components/QtySection/index.js
@@ -4,26 +4,9 @@ import FontAwesome from 'react-fontawesome';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 
 function QtySection({
-  chosenStrength,
-  nicotineStrengths,
-  nicotineHandler,
-  inStock,
   qtyHandler,
   quantity,
-  // added,
-  addToCartHandler,
-  error,
-  errorMsg,
 }) {
-  if (!inStock) {
-    return (
-      <ErrorMsg
-        error="true"
-        errorMsg="Out of stock"
-      />
-    );
-  }
-
   return (
     <div className="options-qty-cart__qty-container">
       <div className="qty-container__qty-blurb">
@@ -40,7 +23,7 @@ function QtySection({
         <div className="qty-buttons__button-container">
           <button
             data-tag="qty-plus"
-            className="button-container__qty-adjust" data-ix="new-interaction"
+            className="button-container__qty-adjust"
             onClick={qtyHandler}
           >
             <div className="qty-adj__text">
@@ -71,51 +54,3 @@ QtySection.propTypes = {
   qtyHandler: func.isRequired,
 };
 export default QtySection;
-
-/*
-<div className="desc__actions">
-{
-inStock ?
-(<div className="actions__btn-container">
-<div className="btn-container__qty--container">
-<ul className="qty__list">
-<li className="list--qty-title">
-<p><IntlMsg id="product.single.actions.quantity.title" /></p>
-</li>
-<li className="list--qty-readout">
-<p>{quantity}</p>
-</li>
-<li className="list--qty-adjust">
-
-<button
-data-tag="qty-plus"
-className="qty-adjust__plus sweep-right"
-onClick={qtyHandler}
-><FontAwesome name="plus" /></button>
-
-<button
-data-tag="qty-minus"
-className="qty-adjust__minus sweep-right"
-onClick={qtyHandler}
-><FontAwesome name="minus" /></button>
-
-</li>
-</ul>
-</div>
-<AddToCartBtn
-added={added}
-addToCart={addToCartHandler}
-/>
-</div>)
-:
-<ErrorMsg
-error="true"
-errorMsg="Out of stock"
-/>
-}
-<ErrorMsg
-error={error}
-errorMsg={errorMsg}
-/>
-</div>
-*/
