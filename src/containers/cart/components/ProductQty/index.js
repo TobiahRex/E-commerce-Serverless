e1503a47479b85b9e9e4a6197ea94e1e3e41d542
TobiahRex <tobiah.rex@gmail.com>
2017-10-10 15:18:57 -0700

    completed first draft of ProductQty.

--- src/containers/cart/components/ProductQty/index.js
+++ src/containers/cart/components/ProductQty/index.js
@@ -1,38 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
+import { ErrorMsg } from '../';
 
-function ProductUnitPrice({ productObj }) {
+function ProductQty({ productObj, qtyHandler }) {
   return (
     <div className="line-item__qty-container">
       <div className="qty-container__box-container">
-        <div className="qty-container__qty-text">100</div>
+        <div className="qty-container__qty-text">
+          {productObj.qty}
+        </div>
         <div className="box-container__qty-buttons">
-          <a className="qty-buttons__btn w-button" href="#">
+          <button
+            data-id={productObj._id}
+            data-tag="qty-plus"
+            className="qty-buttons__btn w-button"
+            onClick={qtyHandler}
+          >
             <FontAwesome name="plus" />
-          </a>
-          <a className="qty-buttons__btn w-button" href="#">
+          </button>
+
+          <button
+            data-id={productObj._id}
+            data-tag="qty-minus"
+            className="qty-buttons__btn w-button"
+            onClick={qtyHandler}
+          >
             <FontAwesome name="minus" />
-          </a>
+          </button>
         </div>
       </div>
       <div className="qty-container__error-msg">
         <p className="error-msg__blurb-container">
           Maximum of 4 bottles, per customer, per address. More info
           <em className="error-msg__link">here</em>.
-          <br>
-            Japanese Statute # 123123123.
-          </p>
-        </div>
+          <br />
+          Japanese Statute # 123123123.
+        </p>
       </div>
-      );
-      }
-const { shape, string } = PropTypes;
-ProductUnitPrice.propTypes = {
+      <ErrorMsg
+        error={productObj.error}
+        errorMsg={productObj.error.message[IntlLocale]}
+      />
+    </div>
+  );
+}
+const { shape, string, func, any } = PropTypes;
+ProductQty.propTypes = {
+  qtyHandler: func.isRequired,
   productObj: shape({
+    _id: string,
+    qty: any,
     product: shape({
-      price: string,
+      error: shape({
+        message: shape({
+          en: string,
+          ja: string,
+        }),
+      }),
     }),
   }).isRequired,
 };
-export default ProductUnitPrice;
+export default ProductQty;
