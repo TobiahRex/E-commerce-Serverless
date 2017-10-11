adf3d53106a67a8e2eeb15d0f2a37d170b8eeeb6
TobiahRex <tobiah.rex@gmail.com>
2017-10-11 16:45:39 -0700

    refactoring expressCheckout.js for use with assets/utils/index.js.

--- src/containers/checkout/assets/utils/index.js
+++ src/containers/checkout/assets/utils/index.js
@@ -1,1 +1,7 @@
+export orderActions from '../../../../redux/orders';
+export apiActions from '../../../../redux/api';
+export toasterActions from '../../../../redux/toaster';
+export userActions from '../../../../redux/user';
+export checkoutActions from '../../../../redux/checkout';
+
 export WebflowAnimations from './webflow.animations';
--- src/containers/checkout/redux.imports.js
+++
@@ -1,5 +0,0 @@
-export orderActions from '../../redux/orders';
-export apiActions from '../../redux/api';
-export toasterActions from '../../redux/toaster';
-export userActions from '../../redux/user';
-export checkoutActions from '../../redux/checkout';
