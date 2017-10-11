7651ac38223dc06481e008f67a1b2c01045b24ae
TobiahRex <tobiah.rex@gmail.com>
2017-10-11 16:48:45 -0700

    moved graphql assets from expressCheckout.js into assets/graphql.

--- src/containers/checkout/assets/utils/index.js
+++ src/containers/checkout/assets/utils/index.js
@@ -5,3 +5,15 @@ export userActions from '../../../../redux/user';
 export checkoutActions from '../../../../redux/checkout';
 
 export WebflowAnimations from './webflow.animations';
+
+export zipUserCart from '../../../../services/utils/zipUserCart';
+export zipArrays from '../../../../services/utils/zipArrays';
+export determineCartType from '../../../../services/utils/determineCartType';
+export nicotineStrengthConverter from '../../../../services/utils/nicotineStrengthConverter';
+export checkNewUser from '../../../../services/utils/checkNewUser';
+export arrayDeepEquality from '../../../../services/utils/arrayDeepEquality';
+export composeFinalTotal from '../../../../services/utils/composeFinalTotal';
+export squarePaymentForm from '../../../../services/utils/squarePaymentForm';
+export cleanOffTypename from '../../../../services/utils/cleanOffTypename';
+export checkForToast from '../../../../services/utils/checkForToast';
+export generateFinalForm from '../../../../services/utils/orderForm/generateFinalForm';
