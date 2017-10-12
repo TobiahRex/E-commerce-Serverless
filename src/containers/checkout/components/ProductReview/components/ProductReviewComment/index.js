228d75ad5fe6af6ec27a9602510eea916d251aff
TobiahRex <tobiah.rex@gmail.com>
2017-10-11 17:33:34 -0700

    refactored ProductReviewComments for ExpressCheckout.

--- src/containers/checkout/components/ProductReview/components/ProductReviewComment/index.js
+++ src/containers/checkout/components/ProductReview/components/ProductReviewComment/index.js
@@ -2,31 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from 'react-intl';
 
-class ProductReviewComments extends React.PureComponent {
-
-  handleOnChange = e => this.props.handleOnChange(e);
-
-  render() {
-    return (
-      <div className="checkout__comments">
-        <textarea
-          name="prComments"
-          cols="40"
-          rows="5"
-          placeholder={
-            this.props.intl.messages[
-              'checkout.product-review.table.comments.placeholder'
-            ]
-          }
-          value={this.props.comments}
-          onChange={this.handleOnChange}
-        />
-      </div>
-    );
-  }
+function ProductReviewComments({ handleOnChange }) {
+  return (
+    <div className="checkout__comments">
+      <textarea
+        name="prComments"
+        cols="40"
+        rows="5"
+        placeholder={
+          this.props.intl.messages[
+            'checkout.product-review.table.comments.placeholder'
+          ]
+        }
+        value={this.props.comments}
+        onChange={handleOnChange}
+      />
+    </div>
+  );
 }
+const ProductReviewCommentsWithIntl = injectIntl(ProductReviewComments);
 const { string, func } = PropTypes;
-
 ProductReviewComments.propTypes = {
   intl: intlShape.isRequired,
   comments: string,
@@ -35,5 +30,5 @@ ProductReviewComments.propTypes = {
 ProductReviewComments.defaultProps = {
   comments: '',
 };
-const ProductReviewCommentsWithIntl = injectIntl(ProductReviewComments);
+
 export default ProductReviewCommentsWithIntl;
