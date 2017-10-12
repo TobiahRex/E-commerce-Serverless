4d3d9980ee5db59568d504318a43b4db3ed62a0b
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 14:47:42 -0700

    optimized "Prefecture" with recompose for pure render.

--- src/containers/checkout/components/ShippingAddress/components/index.js
+++ src/containers/checkout/components/ShippingAddress/components/index.js
@@ -3,3 +3,6 @@ export FamilyName from './FamilyName';
 export Email from './Email';
 export PostalCode from './PostalCode';
 export AddressLine from './AddressLine';
+export Country from './Country';
+export City from './City';
+export PhoneNumber from './PhoneNumber';
