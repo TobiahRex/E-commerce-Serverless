a2ffc41e7726b2a003e3184bafb298d4bec58f05
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 14:52:23 -0700

    optimized "PhoneNumber" with recompose for pure render.

--- src/containers/checkout/components/ShippingAddress/components/PhoneNumber/index.js
+++ src/containers/checkout/components/ShippingAddress/components/PhoneNumber/index.js
@@ -2,44 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Validation from 'react-validation';
 import { FormattedMessage as IntlMsg } from 'react-intl';
+import _ from 'lodash';
+import { lifecycle } from 'recompose';
 
-class PhoneNumber extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      phoneNumber: props.phoneNumber,
-    };
-  }
-
-  handleOnChange = e => this.props.handleOnChange(e)
-
-  render() {
-    return (
-      <div className="input__row">
-        <div className="input__row--phone">
-          <p>
-            <IntlMsg id="checkout.shipping-address.phone" />&nbsp;
-            <span className="required">*</span>
-          </p>
-          <Validation.components.Input
-            errorClassName="is-invalid-input"
-            type="string"
-            containerClassName=""
-            name={`${this.props.type}PhoneNumber`}
-            validations={['required', 'numeric', 'phone-startWithZero', 'phone-japanLength']}
-            onChange={this.handleOnChange}
-            value={this.props.phoneNumber}
-          />
-        </div>
+function PhoneNumber({
+  type,
+  phoneNumber,
+  handleOnChange,
+}) {
+  return (
+    <div className="input__row">
+      <div className="input__row--phone">
+        <p>
+          <IntlMsg id="checkout.shipping-address.phone" />&nbsp;
+          <span className="required">*</span>
+        </p>
+        <Validation.components.Input
+          errorClassName="is-invalid-input"
+          type="string"
+          containerClassName=""
+          name={`${type}PhoneNumber`}
+          validations={['required', 'numeric', 'phone-startWithZero', 'phone-japanLength']}
+          onChange={handleOnChange}
+          value={phoneNumber}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
+const PhoneNumberWithLifecycle = lifecycle({
+  shouldComponentUpdate(nextProps) {
+    if (!_.isEqual(nextProps, this.props)) return true;
+    return false;
+  },
+})(PhoneNumber);
+
 const { string, func } = PropTypes;
 PhoneNumber.propTypes = {
   type: string.isRequired,
   phoneNumber: string.isRequired,
   handleOnChange: func.isRequired,
 };
-export default PhoneNumber;
+export default PhoneNumberWithLifecycle;
