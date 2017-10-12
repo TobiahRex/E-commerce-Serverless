e660cb56b9a5c4afc4f86881e3d42699f470e8ca
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 16:32:11 -0700

    optimized "CreditCardNumber" with recompose for pure render.

--- src/containers/checkout/components/CreditCardInfo/components/NameOnCard/index.js
+++ src/containers/checkout/components/CreditCardInfo/components/NameOnCard/index.js
@@ -3,50 +3,45 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import Validation from 'react-validation';
 import { FormattedMessage as IntlMsg } from 'react-intl';
+import { lifecycle } from 'recompose';
+
+function NameOnCard({
+  ccNameOnCard,
+  handleOnChange,
+}) {
+  return (
+    <div className="input__row" >
+      <div className="input__row--name-on-card">
+        <p>
+          <IntlMsg id="checkout.credit-card.name-on-card" />&nbsp;
+          <span className="required">*</span>
+        </p>
+        <Validation.components.Input
+          errorClassName="is-invalid-input"
+          type="text"
+          containerClassName=""
+          name="ccNameOnCard"
+          validations={['required', 'ccName', 'ccName-firstLast']}
+          onChange={handleOnChange}
+          value={ccNameOnCard}
+        />
+      </div>
+    </div>
+  );
+}
+const NameOnCardWithLifecycle = lifecycle({
+  shouldComponentUpdate(nextProps) {
+    const npCopy = _.cloneDeep(nextProps);
+    const tpCopy = _.cloneDeep(this.props);
+
+    if (!_.isEqual(npCopy, tpCopy)) return true;
+    return false;
+  },
+})(NameOnCard);
 
 const { func, string } = PropTypes;
-
-class NameOnCard extends React.PureComponent {
-  static propTypes = {
-    ccNameOnCard: string.isRequired,
-    handleOnChange: func.isRequired,
-  }
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      ccNameOnCard: '',
-    };
-  }
-  componentWillReceiveProps(nextProps) {
-    const nextPropsCopy = Object.assign({}, nextProps);
-    delete nextPropsCopy.handleOnChange;
-
-    if (!_.isEqual(nextProps, this.props)) this.setState({ ...nextPropsCopy });
-  }
-
-  handleOnChange = e => this.props.handleOnChange(e)
-
-  render() {
-    return (
-      <div className="input__row" >
-        <div className="input__row--name-on-card">
-          <p>
-            <IntlMsg id="checkout.credit-card.name-on-card" />&nbsp;
-            <span className="required">*</span>
-          </p>
-          <Validation.components.Input
-            errorClassName="is-invalid-input"
-            type="text"
-            containerClassName=""
-            name="ccNameOnCard"
-            validations={['required', 'ccName', 'ccName-firstLast']}
-            onChange={this.handleOnChange}
-            value={this.state.ccNameOnCard}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-export default NameOnCard;
+NameOnCard.propTypes = {
+  ccNameOnCard: string.isRequired,
+  handleOnChange: func.isRequired,
+};
+export default NameOnCardWithLifecycle;
