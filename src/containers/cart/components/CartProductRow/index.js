db70123058bb21a86f32925caea3bc5f5b20ae13
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 13:33:14 -0700

    updated how the errors are rendered in Cart.

--- src/containers/cart/components/CartProductRow/index.js
+++ src/containers/cart/components/CartProductRow/index.js
@@ -9,14 +9,13 @@ import {
 } from '../';
 
 function CartProductRow({
-  key,
   productObj,
   qtyHandler,
   deleteFromCart,
 }) {
   return (
     <div
-      key={key}
+      key={`shopping-cart-table-row-${productObj._id}`}
       className="product-list_line-item" data-ix="shopping-list-line-item-slide"
     >
       <div className="line-item__juice-container">
@@ -41,38 +40,11 @@ function CartProductRow({
 const {
   any,
   func,
-  string,
   objectOf,
 } = PropTypes;
 CartProductRow.propTypes = {
-  key: string.isRequired,
   productObj: objectOf(any).isRequired,
   qtyHandler: func.isRequired,
   deleteFromCart: func.isRequired,
 };
 export default CartProductRow;
-
-/* <div className="shopping-cart-action-btns-parent">
-  <div className="btns__container">
-    <button
-      data-slug="express_checkout"
-      className="shopping-cart-action-btn-checkout sweep-right"
-      onClick={routerPush}
-    >
-      <span className="btn-flex-parent">
-        <FontAwesome name="credit-card-alt" />&nbsp;
-        <IntlMsg id="cart.action-btn.express-checkout" />
-      </span>
-    </button>
-    <button className="shopping-cart-action-btn-clear sweep-right" onClick={emptyCart}>
-      <span className="btn-flex-parent">
-        <IntlMsg id="cart.action-btn.clear-cart" />
-      </span>
-    </button>
-  </div>
-  </div>
-  <div className="shopping-cart-back-parent">
-  <button data-slug="/" className="shopping-cart-back sweep-right" onClick={routerPush}>
-    <IntlMsg id="cart.action-btn.back-home" />
-  </button>
-</div> */
