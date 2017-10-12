d42727a624faac3ee0dd87391aefcbda61902b35
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 14:39:43 -0700

    optimized "Country" with recompose for pure render.

--- src/containers/checkout/components/ShippingAddress/components/index.js
+++ src/containers/checkout/components/ShippingAddress/components/index.js
@@ -2,3 +2,4 @@ export GivenName from './GivenName';
 export FamilyName from './FamilyName';
 export Email from './Email';
 export PostalCode from './PostalCode';
+export AddressLine from './AddressLine';
