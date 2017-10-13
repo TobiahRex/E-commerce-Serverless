f2cf038892ed327ad2797069643abbcc1d064719
TobiahRex <tobiah.rex@gmail.com>
2017-10-13 16:07:32 -0700

    completed refactor of "CreditCardExpiration" with WF assets.

--- src/containers/checkout/components/CreditCardInfo/components/CreditCardExpire/index.js
+++ src/containers/checkout/components/CreditCardInfo/components/CreditCardExpire/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 import _ from 'lodash';
 import { lifecycle } from 'recompose';
+import './assets/styles/style.css';
 
 function CreditCardExpire() {
   return (
-    <div className="input__row">
-      <div className="input__row--exp-date">
-
-        <div className="input__container--exp-month">
-          <p>
-            <IntlMsg id="checkout.credit-card.expiration" />&nbsp;
-            <span className="required">*</span>
-          </p>
-
-          <div id="sq-expiration-date" />
-        </div>
-      </div>
+    <div className="cc-expire-section__container">
+      <label className="form__label" htmlFor="cc-number">
+        <IntlMsg id="checkout.credit-card.expiration" />&nbsp;
+        <em className="label__asterisk">*</em>
+      </label>
+      <div id="sq-expiration-date" />
     </div>
   );
 }
