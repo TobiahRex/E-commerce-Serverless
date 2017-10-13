310e42df8b343d52296e1553693d3133b82adb70
TobiahRex <tobiah.rex@gmail.com>
2017-10-13 11:56:01 -0700

    properly rendering revised "GivenName" components.

--- src/containers/checkout/components/shippingAddress/index.js
+++ src/containers/checkout/components/shippingAddress/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage as IntlMsg } from 'react-intl';
+import { HdrBox } from './components';
+import './assets/styles/style.css';
 
 function ShippingAddress({ children }) {
   return (
-    <div className="checkout__shipping">
-      <div className="title">
-        <h3><IntlMsg id="checkout.shipping-address.title" /></h3>
-      </div>
+    <div className="main-section__shipping">
+      <HdrBox />
       {children}
     </div>
   );
