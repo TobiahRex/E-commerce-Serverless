27f290e432853f160891ed4ae6d74db0db5e2a12
TobiahRex <tobiah.rex@gmail.com>
2017-10-12 19:04:20 -0700

    refactored "NewUserDiscount" with WF asset.

--- src/containers/checkout/components/ProductReview/components/NewUserDiscount/index.js
+++ src/containers/checkout/components/ProductReview/components/NewUserDiscount/index.js
@@ -4,32 +4,32 @@ import { FormattedMessage as IntlMsg } from 'react-intl';
 
 function NewUserDiscountOffer({ routerPush }) {
   return (
-    <div>
-      <div className="input__row">
-        <div className="input__row--guest-warning">
-          <p>
-            <span className="warning-bold">
-              <IntlMsg id="checkout.product-review.new-user.discount.warning.title" />&nbsp;
-            </span>
-            <IntlMsg id="checkout.product-review.new-user.discount.warning.description" /><span className="required">*&nbsp;</span>
-          </p>
-        </div>
-      </div>
-
-      <div className="input__row">
-        <div className="input__row--guest-register">
-          <button
-            className="guest-register sweep-right"
-            data-slug="login"
-            onClick={routerPush}
-          ><IntlMsg id="checkout.product-review.new-user.discount.register" /></button>
-        </div>
-        <br />
-        <p>
-          <span className="required">*&nbsp;</span>
-          &nbsp;
-          <IntlMsg id="checkout.product-review.new-user.discount.credit-card" />
+    <div className="product-review__logged-out">
+      <div className="logged-out__warning">
+        <p className="warning__blurb">
+          <strong className="blurb__red-text">
+            <IntlMsg id="checkout.product-review.new-user.discount.warning.title" />&nbsp;&nbsp;
+          </strong>
+          <IntlMsg id="checkout.product-review.new-user.discount.warning.description" />&nbsp;
+          <strong className="blurb__red-text">*</strong>&nbsp;
         </p>
+      </div>
+      <div className="logged-out__register">
+        <button
+          className="register__button w-inline-block"
+          data-slug="login"
+          onClick={routerPush}
+        >
+          <div className="button__blurb">
+            <IntlMsg id="checkout.product-review.new-user.discount.register" />
+          </div>
+        </button>
+      </div>
+      <div className="logged-out__disclaimer">
+        <div className="disclaimer__blurb">
+          <em className="blurb__red-text--alt">*</em>&nbsp;
+          <IntlMsg id="checkout.product-review.new-user.discount.credit-card" />
+        </div>
       </div>
     </div>
   );
