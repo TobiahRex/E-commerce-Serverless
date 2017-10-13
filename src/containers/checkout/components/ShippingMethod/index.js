e81fb247480565fe0059015286c3ff5173564b28
TobiahRex <tobiah.rex@gmail.com>
2017-10-13 11:09:02 -0700

    refactored "ShippingMethod" @ ExpressCheckout.

--- src/containers/checkout/components/ShippingMethod/index.js
+++ src/containers/checkout/components/ShippingMethod/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import { FormattedMessage as IntlMsg } from 'react-intl';
 import _ from 'lodash';
 import { lifecycle } from 'recompose';
+import {
+  HdrBox,
+  IntlShippingForm,
+} from './components';
+import './assets/styles/style.css';
 
 function ShippingMethod() {
   return (
-    <div className="checkout__shipping-method">
-      <div className="title">
-        <h3>
-          <IntlMsg id="checkout.shipping-method.title" />
-        </h3>
-      </div>
-      <div className="input__row">
-        <div className="input__row--free-shipping">
-          <input
-            type="checkbox"
-            checked
-            onChange={e => console.log(e.target.value)}
-          />
-          <p>
-            <IntlMsg id="checkout.shipping-method.free-shipping" />
-          </p>
-        </div>
-      </div>
+    <div className="main-section__shipping-method">
+      <HdrBox />
+      <IntlShippingForm />
     </div>
   );
 }
