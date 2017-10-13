4c910b0da39832c3c28c648600caf38d432dcf64
TobiahRex <tobiah.rex@gmail.com>
2017-10-13 16:13:22 -0700

    rebuilding CvnAndZip.

--- src/containers/checkout/components/CreditCardInfo/components/CvnAndZip/index.js
+++ src/containers/checkout/components/CreditCardInfo/components/CvnAndZip/index.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 import { lifecycle } from 'recompose';
+import './assets/styles/style.css';
 
 function CvnAndZip({
   toggleModal,
 }) {
   return (
-    <div className="input__row cvn">
-      <div className="input__row--cvn-number">
+    <div className="cvn-zip-section__container">
+      {/* <div className="input__row--cvn-number">
         <div className="cvn-number--wrapper">
           <p>CVV
             <span className="required">*</span>
@@ -23,13 +24,28 @@ function CvnAndZip({
             <IntlMsg id="checkout.credit-card.cvn.whats-this" />
           </button>
         </div>
+      <div id="sq-cvv" /> */}
+      <div className="cvn-section__container">
+        <label className="form__label" htmlFor="CVV">
+          CVV
+          <em className="label__asterisk">*</em> &nbsp;
+          <button
+            type="button"
+            data-modal="showCvnModal"
+            className="button--cvn-modal"
+            onClick={toggleModal}
+          >
+            <IntlMsg id="checkout.credit-card.cvn.whats-this" />
+          </button>
+        </label>
         <div id="sq-cvv" />
       </div>
-      <div className="input__row--zip-code">
-        <p>
+
+      <div className="zip-section__container">
+        <label className="form__label" htmlFor="CVV">
           <IntlMsg id="checkout.credit-card.postal-code" />&nbsp;
-          <span className="required">*</span>
-        </p>
+          <em className="label__asterisk">*</em> &nbsp;
+        </label>
         <div id="sq-postal-code" />
       </div>
     </div>
