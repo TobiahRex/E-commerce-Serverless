7242e46a7fc15e320ec407347ec40e1bbf0e8f50
TobiahRex <tobiah.rex@gmail.com>
2017-10-13 21:13:53 -0700

    checkpoint for end of day purchase.

--- src/containers/checkout/components/NetworkStatus/components/SuccessMsg/index.js
+++ src/containers/checkout/components/NetworkStatus/components/SuccessMsg/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 import _ from 'lodash';
 import { lifecycle } from 'recompose';
+import './assets/styles/style.css';
 
-function ErrorMsg({ message }) {
+function PleaseWait() {
   return (
-    <div className="main-section__error">
-      <div className="error__hdr-section">
-        <div className="hdr-section__hdr-container">
-          <h2 className="hdr-container__error-blurb">
-            <em className="error-blurb__fa-text">
-              <FontAwesome className="error-icon" name="times-circle" />&nbsp;
-            </em>
-            <IntlMsg id="checkout.error.title" />
+    <div className="main-section__success">
+      <div className="success__hdr-section">
+        <div className="hd-section__hdr-container">
+          <h2 className="hdr-container__success-blurb"><em className="success-blurb__fa-text">
+            <FontAwesome className="success-icon" name="check-circle" />&nbsp;
+          </em>&nbsp;
+            <IntlMsg id="checkout.submit.success.title" />
           </h2>
         </div>
       </div>
-      <div className="error__blurb-section">
-        <div className="blurb-section__blurb">{message}</div>
+      <div className="success__blurb-section">
+        <div className="blurb-section__success-blurb">
+          <IntlMsg id="checkout.submit.success.message" />&nbsp;
+          <em className="success-blurb__fa-spinner">
+            <IntlMsg id="checkout.submit.success.message" />&nbsp;
+          </em>
+        </div>
       </div>
     </div>
   );
 }
-const ErrorMsgWithLifecycle = lifecycle({
+const PleaseWaitWithLifecycle = lifecycle({
   shouldComponentUpdate(nextProps) {
     const npCopy = _.cloneDeep(nextProps);
     const tpCopy = _.cloneDeep(this.props);
@@ -32,9 +36,5 @@ const ErrorMsgWithLifecycle = lifecycle({
     if (!_.isEqual(npCopy, tpCopy)) return true;
     return false;
   },
-})(ErrorMsg);
-
-ErrorMsg.propTypes = {
-  message: PropTypes.string.isRequired,
-};
-export default ErrorMsgWithLifecycle;
+})(PleaseWait);
+export default PleaseWaitWithLifecycle;
--- src/containers/checkout/components/submitOrder/index.js
+++ src/containers/checkout/components/submitOrder/index.js
@@ -5,42 +5,84 @@ import Validation from 'react-validation';
 import FontAwesome from 'react-fontawesome';
 import { withHandlers, lifecycle } from 'recompose';
 import { FormattedMessage as IntlMsg } from 'react-intl';
-
 import {
   squarePaymentForm as SqrPaymentForm,
 } from '../../assets/utils';
+import './assets/styles/style.css';
 
 function SubmitOrder({ enable, requestCardNonce, loading }) {
+  console.log('%cenable', 'background:purple;', enable);
   if (enable) {
     return (
-      <div className="checkout__purchase-btn" style={{ display: loading ? 'none' : '' }} >
-        <Validation.components.Button
-          className="button"
-          errorClassName="asd"
-          onClick={requestCardNonce}
-        >
-          <span className="btn-flex-parent">
-            <p>
-              <IntlMsg id="checkout.submit1" />
-            </p>
-            &nbsp;
-            <FontAwesome name="opencart" />
-          </span>
-        </Validation.components.Button>
+      // <div className="checkout__purchase-btn" style={{ display: loading ? 'none' : '' }} >
+      //   <Validation.components.Button
+      //     className="button"
+      //     errorClassName="asd"
+      //     onClick={requestCardNonce}
+      //   >
+      //     <span className="btn-flex-parent">
+      //       <p>
+      //         <IntlMsg id="checkout.submit1" />
+      //       </p>
+      //       &nbsp;
+      //       <FontAwesome name="opencart" />
+      //     </span>
+      //   </Validation.components.Button>
+      // </div>
+
+      <div
+        className="main-section__checkout-btn"
+        style={{ display: loading ? 'none' : '' }}
+      >
+        <div className="checkout-btn__container">
+          <Validation.components.Button
+            className="container__btn"
+            // errorClassName="asd"
+            onClick={requestCardNonce}
+          >
+            <IntlMsg id="checkout.submit1" />&nbsp;
+            <strong className="important-text">
+              <FontAwesome name="opencart" />
+            </strong>
+          </Validation.components.Button>
+          {/* <a className="container__btn w-button" href="#">
+            <IntlMsg id="checkout.submit1" />
+            <strong className="important-text">
+              <FontAwesome name="opencart" />
+            </strong>
+          </a> */}
+        </div>
       </div>
     );
   }
   return (
-    <div className="checkout__purchase-btn">
-      <button className="button" disabled>
-        <span className="btn-flex-parent">
-          <p>
-            <IntlMsg id="checkout.submit1" />
-          </p>
-          &nbsp;
-          <FontAwesome name="opencart" />
-        </span>
-      </button>
+    // <div className="checkout__purchase-btn">
+    //   <button className="button" disabled>
+    //     <span className="btn-flex-parent">
+    //       <p>
+    //         <IntlMsg id="checkout.submit1" />
+    //       </p>
+    //       &nbsp;
+    //       <FontAwesome name="opencart" />
+    //     </span>
+    //   </button>
+    // </div>
+
+    <div className="main-section__checkout-btn" >
+      <div className="checkout-btn__container">
+        <button className="container__btn" disabled >
+          <IntlMsg id="checkout.submit1" />
+          <strong className="important-text">
+            <FontAwesome name="opencart" />
+          </strong>
+        </button>
+        {/* <a className="container__btn w-button" href="#">
+          <IntlMsg id="checkout.submit1" />
+          <strong className="important-text">
+            <FontAwesome name="opencart" />
+          </strong>
+        </a> */}
+      </div>
     </div>
   );
 }
@@ -56,6 +98,7 @@ const SubmitOrderWithHandlers = withHandlers({
 })(SubmitOrder);
 const SubmitOrderWithHandlersAndLifecycle = lifecycle({
   shouldComponentUpdate(nextProps) {
+    console.log('%cnextProps', 'background:pink;', nextProps);
     const npCopy = _.cloneDeep(nextProps);
     const tpCopy = _.cloneDeep(this.props);
 
