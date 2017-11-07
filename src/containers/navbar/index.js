29817949040571092168c8d3d7ef8533f4531804
TobiahRex <tobiah.rex@gmail.com>
2017-11-06 22:38:53 -0700

    finished optimizing the new mini Navbar for smoothed font.

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {
   NavbarWeb,
   NavbarMobile,
+  NavbarWebMini,
 } from './components';
 
 const Navbar = () => (
   <header className="navbar-comp-container">
     <NavbarWeb />
+    <NavbarWebMini />
     <NavbarMobile />
   </header>
 );
