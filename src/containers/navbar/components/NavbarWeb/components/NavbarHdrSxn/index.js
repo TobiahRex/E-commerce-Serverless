1645f320efa305b00922fced59c0bc63a92de64d
TobiahRex <tobiah.rex@gmail.com>
2017-11-08 15:57:45 -0700

    optimized Navbar Navs for either Link or Div elements.

--- src/containers/navbar/components/NavbarWeb/components/NavbarHdrSxn/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarHdrSxn/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
-import { FormattedMsg as IntlMsg } from 'react-intl';
+import { FormattedMessage as IntlMsg } from 'react-intl';
 
-function NavbarHdrSxn({ header }) {
+function NavbarHdrSxn({
+  ix,
+  header,
+  link = '',
+}) {
+  if (link) {
+    return (
+      <Link
+        className="nav-container__hdr-section w-inline-block"
+        data-ix={ix}
+        to={link}
+      >
+        <div className="hdr-section__option--title">
+          <IntlMsg id={header} />
+        </div>
+      </Link>
+    );
+  }
+
   return (
-    <Link className="nav-container__hdr-section w-inline-block" data-ix="nav-b-juice-hover" to="#">
+    <div
+      className="nav-container__hdr-section w-inline-block"
+      data-ix={ix}
+    >
       <div className="hdr-section__option--title">
         <IntlMsg id={header} />
       </div>
-    </Link>
+    </div>
   );
 }
 NavbarHdrSxn.propTypes = {
+  link: PropTypes.string.isRequired,
   header: PropTypes.string.isRequired,
+  ix: PropTypes.string.isRequired,
 };
 export default NavbarHdrSxn;
