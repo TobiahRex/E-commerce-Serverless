0ea1eb8820a4f950b8aef67f47995235ccf75864
TobiahRex <tobiah.rex@gmail.com>
2017-11-08 21:29:41 -0700

    building out dynamic login/logout part of Navbar.

--- src/containers/navbar/components/NavbarWeb/components/NavbarAuthSxn/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarAuthSxn/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
-import { FormattedMessage as IntlMsg } from 'react-intl';
+import { connect } from 'react-redux';
+import {
+  NavbarAuthLogin,
+  NavbarAuthLogout,
+} from './';
 
-function NavbarAuthSxn() {
+function NavbarAuthSxn({ activeUser }) {
   return (
-    <div className="right-side__logged-section" data-ix="nav-b-logged-section-load">
-      <Link className="logged-section__login-prompt" to="/login">
-        <div className="logged-section__login-prompt">
-          <p className="login-prompt__nav-s-login">
-            <IntlMsg id="navbar.login.title" />
-          </p>
-        </div>
-      </Link>
-      <div className="logged-section__usr-icon">
-        <img alt="" className="usr-icon__nav-s-img" src="/images/default-avatar-150px.png" />
-      </div>
+    <div>
+      {
+        activeUser ?
+          <NavbarAuthLogout /> :
+          <NavbarAuthLogin />
+      }
     </div>
   );
 }
+const { objectOf, any } = PropTypes;
 NavbarAuthSxn.propTypes = {
-  link: PropTypes.string.isRequired,
-  header: PropTypes.string.isRequired,
-  ix: PropTypes.string.isRequired,
+  activeUser: objectOf(any),
 };
-export default NavbarAuthSxn;
+NavbarAuthSxn.defaultProps = {
+  activeUser: {},
+};
+export default connect(({ user }) => ({
+  activeUser: user.profile,
+}), null)(NavbarAuthSxn);
