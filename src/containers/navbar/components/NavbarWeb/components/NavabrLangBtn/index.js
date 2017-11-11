b119c578eadb96848f374223b1a591ed04aeed2f
TobiahRex <tobiah.rex@gmail.com>
2017-11-10 18:08:12 -0700

    building out NavbarLangBtn.

---
+++ src/containers/navbar/components/NavbarWeb/components/NavabrLangBtn/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
+import { FormattedMessage as IntlMsg } from 'react-intl';
+
+function NavbarLangBtn({ activeLanguage }) {
+  const englishLang = (
+    <div>
+      <div className="language-switcher__img-container">
+        <img
+          alt={activeLanguage}
+          className="img-container__navbar-language-img"
+          src={`/images/${activeLanguage}-flag.png`}
+        />
+      </div>
+      <div className="language-switcher__blurb-container">
+        <p className="blurb-container__navbar-language-blurb">
+          <IntlMsg id={`navbar.language.${activeLanguage}`} />
+        </p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="left-side__language-switcher w-inline-block" to="#">
+      {englishLang}
+      <div className="language-switcher__arrow-container">
+        <p className="arrow-container__nav-language-arrow" data-ix="nav-b-language-arrow-bounce">
+          <FontAwesome name="angle-down" />
+        </p>
+      </div>
+    </div>
+  );
+}
+NavbarLangBtn.propTypes = {
+  activeLanguage: PropTypes.string.isRequired,
+};
+export default NavbarLangBtn;
