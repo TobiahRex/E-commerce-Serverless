035218ba59d30eda7f983c31736217d3b38971d0
TobiahRex <tobiah.rex@gmail.com>
2017-11-11 19:26:54 -0700

    updated css style sheet for Navbar Dropdown.

--- src/containers/navbar/components/NavbarWeb/components/NavbarCart/components/MyCartDropdown/components/MyCartLinks/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarCart/components/MyCartDropdown/components/MyCartLinks/index.js
@@ -9,7 +9,7 @@ export default function MyCartLinks() {
       <Link className="btn-section__checkout-btn w-button" to="/cart">
         <IntlMsg id="navbar.cart.link.view-cart" />
       </Link>
-      <Link className={`btn-section__view-cart-btn w-button sweep-right ${IntLocale}`} to="/express_checkout">
+      <Link className={`btn-section__view-cart-btn w-button sweep-right ${IntlLocale}`} to="/express_checkout">
         <IntlMsg id="navbar.cart.link.checkout" />
       </Link>
     </div>
