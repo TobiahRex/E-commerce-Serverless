ec54b51309a1c97b190b2f822371c8d6583c9093
TobiahRex <tobiah.rex@gmail.com>
2017-11-14 12:32:58 -0700

    added data-w-id attribute to language selector.

--- src/containers/navbar/components/NavbarWeb/components/NavbarLanguage/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarLanguage/index.js
@@ -7,8 +7,7 @@ import {
 
 function NavbarLanguage({ handleLangChange, activeLanguage }) {
   return (
-    <div className="nav-action-top__left-side">
-
+    <div className="nav-action-top__left-side" data-w-id="f0db6205-47f1-21c7-784b-18b8d37a443f">
       {
         activeLanguage === 'en' ?
           <NavbarLangBtn activeLanguage="english" /> :
