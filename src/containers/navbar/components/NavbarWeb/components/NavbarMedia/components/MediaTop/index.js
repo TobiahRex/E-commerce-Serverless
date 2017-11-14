02e45a9963fe4227917477246de8484867c397cb
TobiahRex <tobiah.rex@gmail.com>
2017-11-13 19:45:51 -0700

    created "MediaTop" component.

--- src/containers/navbar/components/NavbarWeb/components/NavbarMedia/components/MediaTop/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarMedia/components/MediaTop/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { FormattedMessage as IntlMsg } from 'react-intl';
 
-export default function ProductsTop() {
+export default function MediaTop() {
   return (
-    <div className="floating-juice-container__top-section">
-      <div className="top-section__juice-hdr">
-        <h2 className="juice-hdr__hdr-blurb">
-          <IntlMsg id="navbar.nav.juices.description.title" />
-        </h2>
+    <div className="floating-media-container__top-section">
+      <Link className="top-section__media-hdr w-inline-block" to="/contact_us">
+        <h3 className="media-hdr__section-text section-text--contact-us">
+          <IntlMsg id="navbar.nav.contact-us.title" />
+        </h3>
+      </Link>
+      <Link className="top-section__media-hdr w-inline-block" to="/reviews">
+        <h3 className="media-hdr__section-text section-text--juice-reviews">
+          <IntlMsg id="navbar.nav.juice-reviews.title" />
+        </h3>
+      </Link>
+      <div className="top-section__media-hdr" data-ix="nav-s-media-review-hdr-hover-2">
+        <Link className="top-section__media-hdr w-inline-block" to="/vape_news">
+          <h3 className="media-hdr__section-text section-text--vape-news">
+            <IntlMsg id="navbar.nav.vape-news.title" />
+          </h3>
+        </Link>
       </div>
-      <div className="top-section__juice-blurb">
-        <p className="paragraph">
-          <IntlMsg id="navbar.nav.juices.description" />
-        </p>
-      </div>
+      <Link className="top-section__media-hdr w-inline-block" to="/user_stories">
+        <h3 className="media-hdr__section-text section-text--usr-stories">
+          <IntlMsg id="navbar.nav.user-stories.title" />
+        </h3>
+      </Link>
     </div>
   );
 }
