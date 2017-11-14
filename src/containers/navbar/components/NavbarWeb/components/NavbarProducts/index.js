e94b26a6b6f63fe50c8173c927973a26079f89d2
TobiahRex <tobiah.rex@gmail.com>
2017-11-14 15:09:59 -0700

    rewriting NavbarProducts as a functional component with recompose.

--- src/containers/navbar/components/NavbarWeb/components/NavbarProducts/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarProducts/index.js
@@ -1,40 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import { withHandlers } from 'recompose';
 import {
   ProductsTop,
   ProductsMid,
   ProductsLow,
 } from './components';
 
-class NavbarProducts extends React.Component {
-  // shouldComponentUpdate(nextProps) {
-  //   if (!_.isEqual(nextProps, this.props)) return true;
-  //   return false;
-  // }
-
-  routerPush = (e) => {
-    const location = e.target.dataset.slug || e.target.parentNode.dataset.slug || e.target.parentNode.parentNode.dataset.slug;
-    this.props.push(location);
-  }
-
-  render() {
-    return (
-      <div className="navbar-big__juice-dropdown">
-        <div className="juice-dropdown__floating-juice-container" data-ix="nav-b-juice-hover">
-          <ProductsTop />
-          <ProductsMid
-            popularProducts={this.props.popularProducts}
-            routerPush={this.routerPush}
-          />
-          <ProductsLow routerPush={this.routerPush} />
-          <div className="floating-juice-container__nav-b-menu-box" />
-        </div>
+function NavbarProducts({ popularProducts, routerPush }) {
+  return (
+    <div className="navbar-big__juice-dropdown">
+      <div className="juice-dropdown__floating-juice-container" data-ix="nav-b-juice-hover">
+        <ProductsTop />
+        <ProductsMid
+          popularProducts={popularProducts}
+          routerPush={routerPush}
+        />
+        <ProductsLow routerPush={routerPush} />
+        <div className="floating-juice-container__nav-b-menu-box" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 const { shape, string, arrayOf, func } = PropTypes;
 NavbarProducts.propTypes = {
@@ -52,7 +40,7 @@ NavbarProducts.propTypes = {
       }),
     }),
   ),
-  push: func.isRequired,
+  routerPush: func.isRequired,
 };
 NavbarProducts.defaultProps = {
   popularProducts: [],
@@ -62,4 +50,10 @@ const NavbarProductsWithProps = connect(({ products }) => ({
 }), dispatch => ({
   push: location => dispatch(push(location)),
 }))(NavbarProducts);
-export default NavbarProductsWithProps;
+const NavbarProductsWithPropsAndHandlers = withHandlers({
+  routerPush: (e) => {
+    const location = e.target.dataset.slug || e.target.parentNode.dataset.slug || e.target.parentNode.parentNode.dataset.slug;
+    this.props.push(location);
+  },
+})(NavbarProductsWithProps);
+export default NavbarProductsWithPropsAndHandlers;
