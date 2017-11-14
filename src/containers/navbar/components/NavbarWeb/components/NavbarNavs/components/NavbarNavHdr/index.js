84d7b640dc78e9547094be93ad71cb41384ed8ac
TobiahRex <tobiah.rex@gmail.com>
2017-11-14 13:06:29 -0700

    distributed "webflow ix" ids throughout.

--- src/containers/navbar/components/NavbarWeb/components/NavbarNavs/components/NavbarNavHdr/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarNavs/components/NavbarNavHdr/index.js
@@ -5,6 +5,7 @@ import { FormattedMessage as IntlMsg } from 'react-intl';
 
 function NavbarNavHdr({
   ix,
+  wid,
   header,
   link = '',
 }) {
@@ -13,6 +14,7 @@ function NavbarNavHdr({
       <Link
         className="nav-container__hdr-section w-inline-block"
         data-ix={ix}
+        data-w-id={wid}
         to={link}
       >
         <div className="hdr-section__option--title">
@@ -33,10 +35,12 @@ function NavbarNavHdr({
     </div>
   );
 }
+const { string } = PropTypes;
 NavbarNavHdr.propTypes = {
-  link: PropTypes.string,
-  header: PropTypes.string.isRequired,
-  ix: PropTypes.string.isRequired,
+  ix: string.isRequired,
+  wid: string.isRequired,
+  link: string,
+  header: string.isRequired,
 };
 NavbarNavHdr.defaultProps = {
   link: '',
