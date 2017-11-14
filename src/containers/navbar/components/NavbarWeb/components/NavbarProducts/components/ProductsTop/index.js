86876f7d911aaa584dc46b65df9a746234666da0
TobiahRex <tobiah.rex@gmail.com>
2017-11-13 20:03:26 -0700

    modularizing NavbarProducts.

--- src/containers/navbar/components/NavbarWeb/components/NavbarProducts/components/ProductsTop/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarProducts/components/ProductsTop/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { FormattedMessage as IntlMsg } from 'react-intl';
+import {
+  ProductTopHdr,
+  MissionStatement,
+} from './components';
 
 export default function ProductsTop() {
   return (
     <div className="floating-juice-container__top-section">
-      <div className="top-section__juice-hdr">
-        <h2 className="juice-hdr__hdr-blurb">
-          <IntlMsg id="navbar.nav.juices.description.title" />
-        </h2>
-      </div>
-      <div className="top-section__juice-blurb">
-        <p className="paragraph">
-          <IntlMsg id="navbar.nav.juices.description" />
-        </p>
-      </div>
+      <ProductTopHdr />
+      <MissionStatement />
     </div>
   );
 }
