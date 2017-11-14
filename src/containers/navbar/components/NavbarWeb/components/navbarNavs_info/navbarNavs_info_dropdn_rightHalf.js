6a91a80116065aac33d2a458addbebfcd348f172
TobiahRex <tobiah.rex@gmail.com>
2017-11-13 19:55:25 -0700

    created NavInfo component.

---
+++ src/containers/navbar/components/NavbarWeb/components/navbarNavs_info/navbarNavs_info_dropdn_rightHalf.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { FormattedMessage as IntlMsg } from 'react-intl';
+
+export default function NavbarNavsInfoDropdnRighthalf() {
+  return (
+    <div className="info-dropdown-content-innerContainer-right">
+      <ul
+        className="info-dropdown-content-innerContainer-right-list"
+      >
+        <li className="sweep-right">
+          <Link to={'/shipping_policy'}>
+            <p>
+              <IntlMsg id="navbar.nav.info.links.shipping-policy" />
+            </p>
+          </Link>
+        </li>
+        <li className="sweep-right">
+          <Link to={'/return_policy'}>
+            <p>
+              <IntlMsg id="navbar.nav.info.links.return-policy" />
+            </p>
+          </Link>
+        </li>
+        <li className="sweep-right">
+          <Link to={'/privacy_policy'}>
+            <p>
+              <IntlMsg id="navbar.nav.info.links.privacy-policy" />
+            </p>
+          </Link>
+        </li>
+        <li className="sweep-right">
+          <Link to={'/terms_and_conditions'}>
+            <p>
+              <IntlMsg id="navbar.nav.info.links.terms-and-conditions" />
+            </p>
+          </Link>
+        </li>
+        <li className="sweep-right">
+          <Link to={'/nicotine_disclaimer'}>
+            <p>
+              <IntlMsg id="navbar.nav.info.links.nicotine-disclaimer" />
+            </p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
