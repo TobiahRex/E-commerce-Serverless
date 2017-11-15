730710cf6bca91020384cfa14a5221232fe749e8
TobiahRex <tobiah.rex@gmail.com>
2017-11-14 18:53:57 -0700

    migrated MyCart dropdown functionality into NavbarWeb (higher container component) than from lower container.

--- src/containers/navbar/components/NavbarWeb/assets/utils/index.js
+++ src/containers/navbar/components/NavbarWeb/assets/utils/index.js
@@ -4,3 +4,13 @@ export function WebflowJs() {
 export WebflowAnimations from './webflow.animations';
 export WebflowAnimations2 from './webflow.animations2';
 export localeActions from '../../../../../../redux/locale';
+export zipUserCart from '../../../../../../services/utils/zipUserCart';
+export determineCartType from '../../../../../../services/utils/determineCartType';
+export orderActions from '../../../../../../redux/orders/';
+export userActions from '../../../../../../redux/user/';
+export { DeleteFromMemberCart } from '../../../../../../graphql/mutations';
+export {
+  FetchMultipleProducts,
+  FetchMultipleProductsOptions,
+} from '../../../../../../graphql/queries';
+export convertStrengthToNumber from '../../../../../../services/utils/convertStrengthToNumber';
