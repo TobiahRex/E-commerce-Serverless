a3aea36c36dc0ac99a3926caaeb1c3629f98576f
TobiahRex <tobiah.rex@gmail.com>
2017-11-19 14:55:22 -0700

    Navbar is finally and successfully implemented.

--- src/containers/navbar/components/NavbarWeb/components/NavbarLanguage/components/NavbarLangBtn/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarLanguage/components/NavbarLangBtn/index.js
@@ -8,19 +8,17 @@ function NavbarLangBtn({ activeLanguage }) {
 
   return (
     <div className="left-side__language-switcher w-inline-block" data-w-id="09dc2a28-68cd-fc3e-d475-3c752121f228">
-      <div className="language-switcher__displayed">
-        <div className="language-switcher__img-container">
-          <img
-            alt={language}
-            className="img-container__navbar-language-img"
-            src={`/images/${language}-flag.png`}
-          />
-        </div>
-        <div className="language-switcher__blurb-container">
-          <p className="blurb-container__navbar-language-blurb">
-            <IntlMsg id={`navbar.language.${language}.title`} />
-          </p>
-        </div>
+      <div className="language-switcher__img-container">
+        <img
+          alt={language}
+          className="img-container__navbar-language-img"
+          src={`/images/${language}-flag.png`}
+        />
+      </div>
+      <div className="language-switcher__blurb-container">
+        <p className="blurb-container__navbar-language-blurb">
+          <IntlMsg id={`navbar.language.${language}.title`} />
+        </p>
       </div>
       <div className="language-switcher__arrow-container">
         <p className="arrow-container__nav-language-arrow" data-ix="nav-b-language-arrow-bounce">
