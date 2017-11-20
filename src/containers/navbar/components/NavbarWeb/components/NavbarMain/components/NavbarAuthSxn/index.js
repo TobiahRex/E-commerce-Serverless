fbaddfda97b5594735f4ea1cfbf1a19d867617d9
TobiahRex <tobiah.rex@gmail.com>
2017-11-20 01:28:55 -0700

    making "NavbarAuthSxn" a pure component.

--- src/containers/navbar/components/NavbarWeb/components/NavbarMain/components/NavbarAuthSxn/index.js
+++ src/containers/navbar/components/NavbarWeb/components/NavbarMain/components/NavbarAuthSxn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { lifecycle } from 'recompose';
 import {
   NavbarAuthLogin,
   NavbarAuthLogout,
